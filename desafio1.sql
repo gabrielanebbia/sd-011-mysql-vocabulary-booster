df20afa2d610dac5a3ea7803a3ffa535fc35e94e
marlon307 <marlon-ramosb@hotmail.com>
2021-08-27 16:03:22 -0300

    Desafio 1 update

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,1 @@
-SELECT COUNTRY_NAME AS 'País', REGION_ID AS 'Status Inclusão',
-CASE
-	WHEN REGION_ID = 1 THEN 'incluído'
-    ELSE 'não incluído'
-END
-FROM hr.countries;
+SELECT COUNTRY_NAME AS 'País', CASE WHEN REGION_ID = 1 THEN 'incluído' ELSE 'não incluído' END AS 'Status Inclusão' FROM hr.countries;
