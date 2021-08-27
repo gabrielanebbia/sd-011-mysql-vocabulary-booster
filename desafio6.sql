a7d3626e9ee7c7cbc35d804b40077987f38ef3e2
Marco Galindo <marco1408@gmail.com>
2021-08-27 16:10:09 -0300

    requisito 6 corrigido

--- desafio6.sql
+++ desafio6.sql
@@ -3,10 +3,10 @@ t2.job_title as Cargo,
 t3.start_date as `Data de início do cargo`,
 t4.department_name as Departamento
 from employees as t1
-inner join jobs as t2
-on t1.job_id = t2.job_id
 inner join job_history as t3
 on t3.employee_id = t1.employee_id
+inner join jobs as t2
+on t3.job_id = t2.job_id
 inner join departments as t4
 on t4.department_id = t3.department_id
 order by concat(t1.first_name, ' ', t1.last_name) desc;
