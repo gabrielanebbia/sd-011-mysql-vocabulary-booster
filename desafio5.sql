2b4ccc49074f4206775e3391ebf1a6132ab303c0
Guilherme Nunes <guiicnunes@gmail.com>
2021-08-27 15:14:05 -0300

    FIX: Nome errado da tabela 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,7 @@
 SELECT j.JOB_TITLE AS 'Cargo',
 (MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
 ROUND(MIN_SALARY/12, 2) AS 'Média mínima mensal',
-ROUND(MAX_SALARY/12, 2) AS 'Média máxima mesal'
+ROUND(MAX_SALARY/12, 2) AS 'Média máxima mensal'
 FROM
 hr.employees AS e
 INNER JOIN
