0d844bdf74804995157c5323ae9720804c4ecb62
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-08-27 18:49:29 -0300

    requisitos 4, 5 e 6

--- desafio4.sql
+++ desafio4.sql
@@ -1,5 +1,5 @@
 SELECT 
-j.JOB_TITLE ,
+j.JOB_TITLE AS 'Cargo',
 ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
 CASE
 WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
@@ -11,4 +11,4 @@ FROM hr.jobs AS j
 INNER JOIN
 hr.employees e ON j.JOB_ID = e.JOB_ID
 GROUP BY j.JOB_ID
-ORDER BY `Média salarial`, j.JOB_TITLE; 
+ORDER BY `Média salarial`, `Cargo`; 
