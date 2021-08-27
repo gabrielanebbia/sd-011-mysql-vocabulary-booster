79910cd68a24944bdf76ce55cb15f8419760a7b6
Marco Galindo <marco1408@gmail.com>
2021-08-27 17:05:33 -0300

    corrigindo desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,8 +1,10 @@
 select t1.contactname as `Nome de contato`,
-t2.shipperid as `Empresa que fez o envio`,
+t3.shippername as `Empresa que fez o envio`,
 t2.orderdate as `Data do pedido`
 from customers as t1
 inner join orders as t2
 on t1.customerid = t2.customerid
+inner join shippers as t3
+on t3.shipperid = t2.shipperid
 where t2.shipperid between 1 and 2
 order by t1.contactname, t2.shipperid, t2.orderdate;
