b8cb39f072400d7bc394bb332482ed725689bb38
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-08-27 18:08:36 -0300

    refatora requisito 05

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,8 @@
 SELECT
 job_title AS 'Cargo',
 (max_salary - min_salary) AS 'Variação Salarial',
-TRUNCATE(CAST((min_salary / 12) AS DECIMAL(10, 2)), 2) AS 'Média mínima mensal',
-TRUNCATE(CAST((max_salary / 12) AS DECIMAL(10, 2)), 2) AS 'Média máxima mensal'
+CAST((min_salary / 12) AS DECIMAL(10, 2)) AS 'Média mínima mensal',
+CAST((max_salary / 12) AS DECIMAL(10, 2)) AS 'Média máxima mensal'
 FROM hr.jobs
 ORDER BY
 (max_salary - min_salary),
