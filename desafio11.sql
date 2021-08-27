eee9436daa573b2c7062c33cb3275cd72406d641
KaioHPs <kaiohpsalternativo@gmail.com>
2021-08-27 19:29:12 -0300

    Correçoes de Estilo

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,9 @@
 SELECT c1.ContactName AS 'Nome',
 c1.Country AS 'País',
 (
-	SELECT COUNT(*)
-	FROM w3schools.customers AS c2
-	WHERE c2.Country = c1.Country AND c2.CustomerID != c1.CustomerID
+SELECT COUNT(*)
+FROM w3schools.customers AS c2
+WHERE c2.Country = c1.Country AND c2.CustomerID != c1.CustomerID
 ) AS `Número de compatriotas`
 FROM w3schools.customers AS c1
 HAVING `Número de compatriotas` > 0
--- desafio15.sql
+++ desafio15.sql
@@ -3,11 +3,11 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN selectedJob VARCHAR(50))
 BEGIN
-    SELECT ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
-	FROM hr.jobs AS j
-	INNER JOIN hr.employees AS e
-	ON j.JOB_ID = e.JOB_ID
-	WHERE j.JOB_TITLE = selectedJob;
+SELECT ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
+FROM hr.jobs AS j
+INNER JOIN hr.employees AS e
+ON j.JOB_ID = e.JOB_ID
+WHERE j.JOB_TITLE = selectedJob;
 END $$
 
 DELIMITER ;
