131c833e67189dea1b51bbeecac860957c34baca
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-08-27 20:34:26 -0300

    req1 e req3

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,6 @@
-SELECT COUNTRY_NAME AS País, IF (REGION_ID LIKE 1, 'incluído', 'não incluído') AS 'Status Inclusão'
+SELECT COUNTRY_NAME AS País,
+IF (REGION_ID = 1,
+'incluído',
+'não incluído') AS 'Status Inclusão'
 FROM hr.countries
-ORDER BY País ASC;
+ORDER BY COUNTRY_NAME;
