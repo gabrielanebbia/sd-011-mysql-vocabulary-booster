455c6b6ada7c5abf6b128551b23132428001df4a
Alberto Candido <albertocandido98@gmail.com>
2021-08-27 19:15:02 -0300

    Corrige requisito 12

--- desafio11.sql
+++ desafio11.sql
@@ -3,4 +3,6 @@ t1.Country AS 'País',
 (SELECT COUNT(*) FROM w3schools.customers t2
 WHERE t1.Country = t2.Country AND t1.CustomerID <> t2.CustomerID) AS 'Número de compatriotas'
 FROM w3schools.customers t1
+WHERE (SELECT COUNT(*) FROM w3schools.customers t2
+WHERE t1.Country = t2.Country AND t1.CustomerID <> t2.CustomerID) <> 0
 ORDER BY t1.ContactName;
