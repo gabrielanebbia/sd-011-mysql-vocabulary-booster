4d412e7a6fb244a05f8650b5a503c890447fed1b
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-08-27 19:10:23 -0300

    corrige req 15

--- desafio15.sql
+++ desafio15.sql
@@ -1,12 +1,10 @@
 DELIMITER $$
-CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100), OUT media NUMERIC)
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
 BEGIN
-SET media = (
 SELECT ROUND(AVG(funcionarios.SALARY), 2) AS 'Média salarial'
 FROM hr.employees AS funcionarios
 INNER JOIN hr.jobs AS cargos ON cargos.JOB_ID = funcionarios.JOB_ID
-WHERE cargo = cargos.JOB_TITLE
-    );
+WHERE cargo = cargos.JOB_TITLE;
 END $$
 
 DELIMITER ;
