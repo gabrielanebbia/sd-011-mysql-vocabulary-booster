9edca6d511cdcd0410c0b006ef698fa318203302
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-08-27 15:20:50 -0300

    desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,12 @@
+SELECT
+  CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
+  DATE(jh.START_DATE) AS 'Data de início',
+  e.SALARY AS 'Salário'
+FROM
+  hr.job_history AS jh
+  INNER JOIN hr.employees AS e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE
+  MONTH(jh.START_DATE) IN ('01', '02', '03')
+ORDER BY
+  CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME),
+  DATE(jh.START_DATE);
