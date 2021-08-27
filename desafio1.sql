459b8ef927059a09f00d4b165234a5f4bdaa2b2d
MatheusAraujoDev <matheusaraujo.dev@gmail.com>
2021-08-27 15:22:58 -0300

    Ajusta indentação req 01

--- desafio1.sql
+++ desafio1.sql
@@ -1,8 +1,8 @@
 SELECT 
-    COUNTRY_NAME AS País,
-    IF(REGION_ID = 1,
-        'incluído',
-        'não incluído') AS 'Status inclusão'
+COUNTRY_NAME AS País,
+IF(REGION_ID = 1,
+'incluído',
+'não incluído') AS 'Status inclusão'
 FROM
-    hr.countries
+hr.countries
 ORDER BY COUNTRY_NAME;
