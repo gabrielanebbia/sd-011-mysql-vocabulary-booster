2840829747191a8fcc1fe5120fa9f99082bfe414
Hugosomers <hugosomersla@gmail.com>
2021-08-27 17:51:13 -0300

    Done desafio13

---
+++ desafio13.sql
@@ -0,0 +1,6 @@
+SELECT prod.ProductName AS 'Produto',
+prod.Price AS 'Preço'
+FROM w3schools.products AS prod
+INNER JOIN w3schools.order_details AS details
+ON prod.ProductID = details.ProductID AND details.Quantity > 80
+ORDER BY ProductName;
