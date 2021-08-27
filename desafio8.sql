c41262db204d1f5428abc5e1ce88255f2d7c1fee
Diego Ribeiro <dihrm2001@gmail.com>
2021-08-27 17:02:36 -0300

    Desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,14 @@
+SELECT (
+  SELECT CustomerName
+  FROM w3schools.customers AS c
+  WHERE o.CustomerID = c.CustomerID
+) AS `Nome de contato`,
+(
+  SELECT ShipperName
+  FROM w3schools.shippers AS s
+  WHERE o.ShipperID = s.ShipperID
+) AS `Empresa que fez o envio`,
+OrderDate AS `Data do pedido`
+FROM w3schools.orders AS o
+WHERE o.ShipperID IN (1, 2)
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
