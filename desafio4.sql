27d307b5819704917a0817ee3c55ff3481595534
Daniel Leon <daniel.radialista@gmail.com>
2021-08-27 15:42:53 -0300

    refatorado requisitos de 1 a 4, com nomes das tabelas todos em minúsculos

--- desafio4.sql
+++ desafio4.sql
@@ -1,4 +1,4 @@
-SELECT j.JOB_TITLE AS 'Cargo', ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
+SELECT j.job_title AS 'Cargo', ROUND(AVG(e.salary), 2) AS 'Média salarial',
 CASE
 WHEN AVG(e.salary) BETWEEN 2000 AND 5800 THEN 'Júnior'
 WHEN AVG(e.salary) BETWEEN 5801 AND 7500 THEN 'Pleno'
@@ -7,6 +7,6 @@ WHEN AVG(e.salary) > 10500 THEN 'CEO'
 END AS 'Senioridade'
 FROM hr.employees AS e
 INNER JOIN hr.jobs AS j
-ON e.JOB_ID = j.JOB_ID
-GROUP BY j.JOB_TITLE
+ON e.job_id = j.job_id
+GROUP BY j.job_title
 ORDER BY `Média salarial`, 'Cargo';
