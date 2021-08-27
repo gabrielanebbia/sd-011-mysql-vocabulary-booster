1d4c306b3fcec7b02547de123bea021ac207c666
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-08-27 18:14:08 -0300

    [Natalia de Souza Ribeiro] Iniciando o Projeto

---
+++ desafio1.sql
@@ -0,0 +1,6 @@
+SELECT * FROM hr.countries;
+SELECT P.COUNTRY_NAME AS País, IF(R.REGION_NAME = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries AS P
+INNER JOIN hr.regions AS R
+ON P.REGION_ID = R.REGION_ID
+ORDER BY COUNTRY_NAME;
