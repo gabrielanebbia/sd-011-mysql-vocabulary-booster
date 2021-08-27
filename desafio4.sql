0ff32f38c3f28c5885675de92d63ba06d7b4c055
Matheus Gaspar <msouzagaspar@gmail.com>
2021-08-27 23:17:04 +0200

    correção desafio 4

--- desafio4.sql
+++ desafio4.sql
@@ -5,7 +5,7 @@ WHEN AVG(t1.SALARY) > 10500 THEN 'CEO'
 WHEN AVG(t1.SALARY) > 7500 THEN 'Sênior'
 WHEN AVG(t1.SALARY) > 5800 THEN 'Pleno'
 ELSE 'Júnior'
-AS 'Senioridade'
+END AS 'Senioridade'
 FROM hr.employees AS t1
 INNER JOIN hr.jobs AS t2
 ON t1.job_id = t2.job_id
