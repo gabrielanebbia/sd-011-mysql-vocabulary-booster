73706fed49b345277301571b0b8f538afe932477
Josan Johnata <josanjohnata@gmail.com>
2021-08-27 19:08:31 -0300

    desafio 17

---
+++ desafio17.sql
@@ -0,0 +1,12 @@
+USE w3schools;
+
+DELIMITER $$
+
+CREATE TRIGGER trigger_OrderDate_insert
+    BEFORE INSERT ON w3schools.orders
+    FOR EACH ROW
+BEGIN
+    SET NEW.OrderDate = DATE(NOW());
+END $$
+
+DELIMITER ;
