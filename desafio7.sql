278cb915811aca955d60cda1e4e551b7a3f42cef
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-08-27 15:54:59 -0300

    Desafio 07

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+SELECT
+UCASE(CONCAT(e.FIRST_NAME, " ", e.LAST_NAME)) AS "Nome completo",
+e.HIRE_DATE AS "Data de início",
+e.SALARY AS "Salário"
+FROM
+hr.employees e
+WHERE
+MONTH(e.HIRE_DATE) BETWEEN 1 AND 3
+ORDER BY CONCAT(e.FIRST_NAME, " ", e.LAST_NAME) ASC, e.HIRE_DATE ASC;
