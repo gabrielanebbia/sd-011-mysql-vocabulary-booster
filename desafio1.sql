5baba4702d807e8724fc50cce13e33245b86b020
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-08-27 15:33:00 -0300

    Desafio 4 e correcoes de tabulacao

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,6 @@
 SELECT 
-	c.COUNTRY_NAME AS País,
-    CASE r.REGION_NAME WHEN "Europe" THEN "incluído" ELSE "não incluído" END AS "Status Inclusão"
+c.COUNTRY_NAME AS País,
+CASE r.REGION_NAME WHEN "Europe" THEN "incluído" ELSE "não incluído" END AS "Status Inclusão"
 FROM hr.countries c
 INNER JOIN hr.regions r ON c.REGION_ID = r.REGION_ID
 ORDER BY c.COUNTRY_NAME ASC;
