8e2af3960bf5349de945a8da132fd649e02a7f79
Filizzola <juliofilizzola@hotmail.com>
2021-08-27 17:21:34 -0300

    desafio 18  ref fixed

--- desafio18.sql
+++ desafio18.sql
@@ -1,6 +1,8 @@
 SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
 DATE_FORMAT(jh.START_DATE,  '%d/%m/%Y') AS 'Data de início',
 DATE_FORMAT(jh.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
-YEAR(jh.END_DATE) - YEAR(jh.START_DATE) AS 'Anos trabalhados'
+ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE)/365.25, 2) AS 'Anos trabalhados'
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID ORDER BY `Nome completo`;
+
+-- DATE_FORMAT E DATEDIFF FOI O THYAGO PESSOAL DA TURMA 11
