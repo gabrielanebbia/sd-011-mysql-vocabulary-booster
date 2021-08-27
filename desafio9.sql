45169b191672e35cd758d507349734549f7d5aee
Gisele Costa <jikinha@gmail.com>
2021-08-27 17:03:06 -0300

    desafio 9

--- desafio9.sql
+++ desafio9.sql
@@ -2,10 +2,11 @@ SELECT CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
 COUNT(o.OrderID) AS 'Total de pedidos'
 FROM w3schools.orders o
 INNER JOIN w3schools.employees e ON e.EmployeeID = o.EmployeeID
-INNER JOIN w3schools.customers c ON c.CustomerID = o.CustomerID
-WHERE CONCAT(e.FirstName, ' ', e.LastName) = c.ContactName
+WHERE e.EmployeeID = o.EmployeeID
 GROUP BY CONCAT(e.FirstName, ' ', e.LastName)
 ORDER BY COUNT(o.OrderID);
+
+
 
 Exibe todos as pessoas funcionárias que já realizaram algum pedido, mostrando também seu total de pedidos feitos
 Exiba todas as pessoas funcionárias que já realizaram algum pedido e o total dos pedidos feitos.
