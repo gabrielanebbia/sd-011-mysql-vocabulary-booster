4ae5eb2493eb4d02ae42e5c01ec38be127eab9dd
Gisele Costa <jikinha@gmail.com>
2021-08-27 17:03:12 -0300

    desafio 9

--- desafio9.sql
+++ desafio9.sql
@@ -5,20 +5,3 @@ INNER JOIN w3schools.employees e ON e.EmployeeID = o.EmployeeID
 WHERE e.EmployeeID = o.EmployeeID
 GROUP BY CONCAT(e.FirstName, ' ', e.LastName)
 ORDER BY COUNT(o.OrderID);
-
-
-
-Exibe todos as pessoas funcionárias que já realizaram algum pedido, mostrando também seu total de pedidos feitos
-Exiba todas as pessoas funcionárias que já realizaram algum pedido e o total dos pedidos feitos.
-
-Use o banco w3schools como referência
-
-Monte uma query que exiba 02 colunas:
-
-A primeira deve possuir o alias "Nome completo" e exibir o nome completo da pessoa funcionária (não se esqueça do espaço entre o nome e o sobrenome).
-
-A segunda deve possuir o alias "Total de pedidos" e exibir a quantidade total de pedidos feitos pela pessoa.
-
-Ordene seus resultados pelo total de pedidos em ordem crescente.
-
--- esse não entendi
