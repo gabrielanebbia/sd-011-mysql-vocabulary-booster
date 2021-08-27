b02e374f8c3162e4003435e602caf15f1725e2bc
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-08-27 15:25:52 -0300

    adiciona requisito 02

---
+++ desafio2.sql
@@ -0,0 +1,18 @@
+SELECT
+job_title AS 'Cargo',
+(
+IF(
+max_salary < 10000,
+'Baixo',
+IF(
+max_salary < 20000,
+'Médio',
+IF(
+max_salary < 30000,
+'Alto',
+'Altíssimo'
+)
+)
+)
+) AS 'Nível'
+FROM hr.jobs;
