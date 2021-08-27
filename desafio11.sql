a83bd0afbac8ac58786b75ff561a4522037d485b
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-08-27 20:59:14 -0300

    Remove last select(select for tests)

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,7 @@
-SELECT c1.CustomerName AS 'Nome',
+SELECT c1.ContactName AS 'Nome',
 c1.Country AS 'País',
 COUNT(*) - 1 AS 'Número de compatriotas'
 FROM w3schools.customers c1, w3schools.customers c2
 WHERE c1.Country = c2.Country
-GROUP BY c1.CustomerName, c1.Country
-ORDER BY c1.CustomerName;
-
-SELECT * FROM w3schools.customers;
+GROUP BY c1.ContactName, c1.Country
+ORDER BY c1.ContactName;
