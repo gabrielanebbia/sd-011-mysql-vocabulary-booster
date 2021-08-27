4ad0381071a67373d85c6e1b5877201330c9b8b5
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-08-27 15:49:57 -0300

    adiciona requisito 04

---
+++ desafio4.sql
@@ -0,0 +1,23 @@
+SELECT
+jobs.job_title AS 'Cargo',
+AVG(employees.salary) AS 'Média salarial',
+(
+IF(
+AVG(employees.salary) < 5800,
+'Júnior',
+IF(
+AVG(employees.salary) < 7500,
+'Pleno',
+IF(
+AVG(employees.salary) < 10500,
+'Sênior',
+'CEO'
+)
+)
+)
+) AS 'Senioridade'
+FROM hr.employees AS employees
+INNER JOIN hr.jobs AS jobs
+ON employees.job_id = jobs.job_id
+GROUP BY employees.job_id
+ORDER BY AVG(employees.salary), jobs.job_title;
