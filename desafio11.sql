32b8539773f9918c3bbce355c0e0b4f41f78562d
Filizzola <juliofilizzola@hotmail.com>
2021-08-27 16:44:41 -0300

    refdesafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,4 +1,6 @@
-SELECT c.ContactName AS 'Nome', c.Country AS 'País', COUNT(*) AS 'Números de compatriotas' 
+SELECT c.ContactName AS 'Nome',
+c.Country AS 'País',
+COUNT(*) AS 'Número de compatriotas' 
 FROM w3schools.customers AS c, w3schools.customers AS c2
 WHERE c.Country = c2.Country and c.ContactName <> c2.ContactName
 GROUP BY `Nome`, `País`
