e68c327464307f993b924f02602a81da2be63969
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-08-27 20:35:37 -0300

    Add a second order priority of 'desafio 10'

--- desafio10.sql
+++ desafio10.sql
@@ -6,4 +6,4 @@ FROM w3schools.products p
 INNER JOIN w3schools.order_details od ON p.ProductID = od.ProductID
 GROUP BY p.ProductName
 HAVING AVG(od.Quantity) > 20
-ORDER BY AVG(od.Quantity);
+ORDER BY AVG(od.Quantity), p.ProductName;
