0d032208420252ba2c9143b3be4f0bfac7419c5c
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-08-27 20:32:32 -0300

    Collumn 'Nome completo' values in upper case

--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,8 @@
-SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-jb.START_DATE AS 'Data de início',
+SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+jh.START_DATE AS 'Data de início',
 e.SALARY AS 'Salário'
-FROM hr.job_history AS jb
-INNER JOIN hr.employees AS e
-ON jb.EMPLOYEE_ID = e.EMPLOYEE_ID
-WHERE MONTH(jb.START_DATE) <= 3
-ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME), jb.START_DATE;
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS jh
+ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE MONTH(jh.START_DATE) <= 3
+ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME), jh.START_DATE;
