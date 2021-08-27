577fad6013a29d20ccf0dd1e02eeb2c256a71770
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-08-27 15:16:40 -0300

    add req.2

---
+++ desafio2.sql
@@ -0,0 +1,12 @@
+SELECT jobs.JOB_TITLE AS 'Cargo',
+CASE
+WHEN jobs.MAX_SALARY >= 5000 AND jobs.MAX_SALARY <= 10000 THEN 'Baixo'
+WHEN jobs.MAX_SALARY >= 10001 AND jobs.MAX_SALARY <= 20000 THEN 'Médio'
+WHEN jobs.MAX_SALARY >= 20001 AND jobs.MAX_SALARY <= 30000 THEN 'Alto'
+WHEN jobs.MAX_SALARY >= 30001 THEN 'Altíssimo'
+ELSE 'Nao encontrado'
+END 
+AS 'Nível'
+FROM jobs
+ORDER BY Cargo
+ASC;
