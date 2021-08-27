30b0125c142b9e5830e11acf98e783351d0de183
Alberto Candido <albertocandido98@gmail.com>
2021-08-27 17:20:55 -0300

    Corrige requisitos 4 e 6 novamente

--- desafio6.sql
+++ desafio6.sql
@@ -1,16 +1,16 @@
 SELECT * FROM hr.job_history;
 
 SELECT 
-CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS 'Nome completo', -- a
-j.JOB_TITLE AS 'Cargo', -- b
-jh.START_DATE AS 'Data de início do cargo', -- c
-d.DEPARTMENT_NAME AS 'Departamento' -- d
+CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS 'Nome completo', 
+j.JOB_TITLE AS 'Cargo', 
+jh.START_DATE AS 'Data de início do cargo', 
+d.DEPARTMENT_NAME AS 'Departamento' 
 FROM hr.employees emp 
-INNER JOIN hr.departments d
-ON d.DEPARTMENT_ID = emp.DEPARTMENT_ID
-INNER JOIN hr.job_history jh
-ON jh.EMPLOYEE_ID = emp.EMPLOYEE_ID
-INNER JOIN hr.jobs j
-ON jh.JOB_ID = j.JOB_ID
-ORDER BY CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) DESC,
+INNER JOIN hr.job_history jh 
+ON jh.EMPLOYEE_ID = emp.EMPLOYEE_ID 
+INNER JOIN hr.jobs j 
+ON jh.JOB_ID = j.JOB_ID 
+INNER JOIN hr.departments d 
+ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID 
+ORDER BY CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) DESC, 
 j.JOB_TITLE;
