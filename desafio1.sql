4884736dc8638ce2b6e8702d08d9b2234ce9b570
OldRoge <rogeriomrjr@icloud.com>
2021-08-27 16:40:52 -0300

    Fixing code climate errors

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
 SELECT countries.COUNTRY_NAME AS País,
-		IF(regions.REGION_ID = 1, 'incluído', 'não incluído') AS "Status Inclusão"
+IF(regions.REGION_ID = 1, 'incluído', 'não incluído') AS "Status Inclusão"
 FROM hr.countries AS countries
 INNER JOIN hr.regions AS regions
 ON countries.REGION_ID = regions.REGION_ID
