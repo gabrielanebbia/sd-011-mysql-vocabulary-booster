9aa099fba0840abd53d93189c9426a7588ffefd0
Guilherme Nunes <guiicnunes@gmail.com>
2021-08-27 14:42:15 -0300

    FEAT: Desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,12 @@
+SELECT JOB_TITLE AS 'Cargo',
+(MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
+ROUND(MIN_SALARY/12, 2) AS 'Média mínima mensal',
+ROUND(MAX_SALARY/12, 2) AS 'Média máxima mesal'
+FROM
+hr.employees AS e
+INNER JOIN
+hr.jobs AS j
+ON
+e.JOB_ID = j.JOB_ID
+GROUP BY JOB_TITLE
+ORDER BY (MAX_SALARY - MIN_SALARY), JOB_TITLE;
