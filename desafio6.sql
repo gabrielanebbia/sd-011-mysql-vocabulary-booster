fc3191407aa1407dd6f2d1b870ba9fe69f5f52a5
Bruno Duarte <brunoarduarte@gmail.com>
2021-08-27 18:31:36 -0300

    Desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,9 @@
+SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
+J.JOB_TITLE AS `Cargo`,
+JH.START_DATE AS `Data de início do cargo`,
+D.DEPARTMENT_NAME AS `Departamento`
+FROM employees AS E
+INNER JOIN jobs AS J ON E.JOB_ID = J.JOB_ID
+INNER JOIN job_history AS JH ON E.EMPLOYEE_ID= JH.EMPLOYEE_ID
+INNER JOIN departments AS D ON E.DEPARTMENT_ID = D.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC, `Cargo`;
