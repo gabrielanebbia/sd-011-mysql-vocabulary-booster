56ea807e8be0bbe9b317929b277419331e3e4be9
Marco Galindo <marco1408@gmail.com>
2021-08-27 18:22:34 -0300

    desafio 11 corrigido

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,7 @@
 select t1.contactname as Nome,
 t1.country as País,
 count(t1.country = t2.country) as `Número de compatriotas`
-from customers as t1, customers as t2
+from w3schools.customers as t1, w3schools.customers as t2
 where t1.country = t2.country and t1.contactname <> t2.contactname
 group by t1.contactname
 order by t1.contactname;
