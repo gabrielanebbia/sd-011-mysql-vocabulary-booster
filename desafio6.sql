6e4b37a3898f27a36cb4377a51eac036c8806561
Gisele Costa <jikinha@gmail.com>
2021-08-27 15:44:36 -0300

    desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -2,8 +2,8 @@ SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
 j.job_title AS 'Cargo',
 jh.start_date AS 'Data de início do cargo',
 d.department_name AS 'Departamento'
-FROM hr.employees e
-INNER JOIN hr.jobs j ON e.job_id = j.job_id
-INNER JOIN hr.job_history jh ON e.employee_id = jh.employee_id
+FROM hr.job_history jh
+INNER JOIN hr.jobs j ON jh.job_id = j.job_id
+INNER JOIN hr.employees e ON e.employee_id = jh.employee_id
 INNER JOIN hr.departments d ON jh.department_id = d.department_id
 ORDER BY CONCAT(e.first_name, ' ', e.last_name) DESC, j.job_title;
