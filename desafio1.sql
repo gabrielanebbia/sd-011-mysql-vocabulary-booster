a582a0e703532766c972efa93c5662dd9f05cdbd
MatheusAraujoDev <matheusaraujo.dev@gmail.com>
2021-08-27 15:41:02 -0300

    Altera letra maiuscula req 01

--- desafio1.sql
+++ desafio1.sql
@@ -2,7 +2,7 @@ SELECT
 COUNTRY_NAME AS País,
 IF(REGION_ID = 1,
 'incluído',
-'não incluído') AS 'Status inclusão'
+'não incluído') AS 'Status Inclusão'
 FROM
 hr.countries
-ORDER BY País;
+ORDER BY COUNTRY_NAME;
