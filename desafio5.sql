eefbd8c8d1a379eac79cd871bbe3ede66dfcfb46
Gustavo Mendes <gusm217@gmail.com>
2021-08-27 19:46:37 -0300

    Corrige o 5"

--- desafio5.sql
+++ desafio5.sql
@@ -5,4 +5,4 @@ ROUND((MIN_SALARY / 12), 2) AS "Média mínima mensal",
 ROUND((MAX_SALARY / 12), 2) AS "Média máxima mensal"
 FROM hr.jobs
 GROUP BY job_title
-ORDER BY 2 ASC, job_title;
+ORDER BY "Variação Salarial" ASC, job_title;
