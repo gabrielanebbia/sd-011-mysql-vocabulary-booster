34f4e36dd64f752e5701d0a5f12f02c9f1f107ca
Amandha W. Barok <amandha.barok@gmail.com>
2021-08-27 15:42:04 -0700

    Finishing sixth requiriment

---
+++ desafio6.sql
@@ -0,0 +1,14 @@
+SELECT * FROM job_history;
+
+
+SELECT 
+	CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
+    j.JOB_TITLE AS 'Cargo',
+    e.HIRE_DATE AS 'Data de início do cargo',
+    d.DEPARTMENT_NAME AS 'Departamento'
+    
+FROM hr.job_history AS jh
+INNER JOIN hr.employees AS e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+INNER JOIN hr.departments AS d ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
+INNER JOIN hr.jobs AS j ON e.JOB_ID = j.JOB_ID
+ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC, JOB_TITLE;
