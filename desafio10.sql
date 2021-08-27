40fc09b372168187c3918917985211f22863010b
Diego Ribeiro <dihrm2001@gmail.com>
2021-08-27 17:46:18 -0300

    Desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT p.ProductName AS `Produto`,
+MIN(Quantity) AS `Mínima`,
+MAX(Quantity) AS `Máxima`,
+ROUND(AVG(Quantity), 2) AS `Média`
+FROM w3schools.order_details AS o
+INNER JOIN w3schools.products AS p
+ON o.ProductID = p.ProductID
+GROUP BY o.ProductID
+HAVING ROUND(AVG(Quantity), 2) > 20
+ORDER BY `Média`, `Produto`;
