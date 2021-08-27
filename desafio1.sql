f0fd11dc334acf1d847626d549e99adc8d6b097d
Diego Ribeiro <dihrm2001@gmail.com>
2021-08-27 15:16:22 -0300

    Desafio 2

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,7 @@
-SELECT COUNTRY_NAME AS 'País',
+SELECT COUNTRY_NAME AS `País`,
 IF(
 (SELECT REGION_NAME
-FROM regions
+FROM hr.regions
 WHERE REGION_ID = c.REGION_ID) = 'Europe',
-'incluído', 'não incluído') AS 'Status Inclusão'
+'incluído', 'não incluído') AS `Status Inclusão`
 FROM hr.countries AS c;
