cafcca299f6437765cf94a3f5f08111157dd4e9b
Guilherme Nunes <guiicnunes@gmail.com>
2021-08-27 17:35:18 -0300

    FIX: Alias 11 e 12

--- desafio11.sql
+++ desafio11.sql
@@ -3,6 +3,6 @@ t1.Country AS 'País',
 (
 SELECT COUNT(*) - 1 FROM w3schools.customers AS t2
 WHERE t2.Country = t1.Country 
-)
+) AS 'Número de compatriotas'
 FROM w3schools.customers AS t1
 ORDER BY t1.ContactName;
