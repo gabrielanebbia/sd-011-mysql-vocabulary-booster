81ce424ae8c54579328682948070141943359b65
gabiazevedo <gabiazevedoms@gmail.com>
2021-08-27 17:28:21 -0300

    Refatoração requisitos 2, 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,4 @@
-SELECT DISTINCT JOB_TITLE AS Cargo, MAX_SALARY - MIN_SALARY
+SELECT JOB_TITLE AS Cargo, MAX_SALARY - MIN_SALARY
 AS `Diferença entre salários máximo e mínimo`
-FROM hr.employees AS em 
-INNER JOIN hr.jobs AS jo
-ON em.JOB_ID = jo.JOB_ID
+FROM hr.jobs
 ORDER BY `Diferença entre salários máximo e mínimo`;
