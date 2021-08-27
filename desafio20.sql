810b0b9f58cca1173f6727dc081360bdff0a08e6
Filizzola <juliofilizzola@hotmail.com>
2021-08-27 17:23:01 -0300

    desafio 20

---
+++ desafio20.sql
@@ -0,0 +1,21 @@
+USE hr;
+DELIMITER $$ 
+
+CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(20))
+BEGIN 
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo', 
+d.DEPARTMENT_NAME AS 'Departamento',
+j.JOB_TITLE AS 'Cargo'
+FROM hr.employees AS e
+INNER JOIN hr.jobs AS j
+INNER JOIN hr.departments AS d
+INNER JOIN hr.job_history AS jh
+ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+AND d.DEPARTMENT_ID = jh.DEPARTMENT_ID
+AND j.JOB_ID = jh.JOB_ID
+WHERE e.EMAIL = EMAIL;
+END $$ 
+
+DELIMITER ;
+
+CALL exibir_historico_completo_por_funcionario("NKOCHHAR");
