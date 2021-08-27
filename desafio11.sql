84f99e58236f153edac92b57fbd21f0684c856aa
Matheus Alexandre <alexandremhm@gmail.com>
2021-08-27 19:02:17 -0300

    Fix 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
 SELECT a.contactName AS "Nome", 
 a.Country AS "País", 
-(SELECT COUNT(*) FROM customers AS b WHERE a.country = b.country AND a.ContactName <> b.ContactName)
+(SELECT COUNT(*) FROM customers AS b WHERE a.country = b.country AND a.ContactName <> b.ContactName) AS "Número de compatriotas"
 FROM customers AS a
 ORDER BY a.ContactName;
