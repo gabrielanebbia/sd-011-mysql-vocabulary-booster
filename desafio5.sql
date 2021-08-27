cbeac26e3c38a84bb67c7bf1f1f6b42b4e82f8e9
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-08-27 15:37:13 -0300

    Desafio 05

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+SELECT
+j.JOB_TITLE AS Cargo,
+j.MAX_SALARY - j.MIN_SALARY AS "Variação Salarial",
+ROUND(MIN(e.SALARY), 2) AS "Média mínima mensal",
+ROUND(MAX(e.SALARY), 2) AS "Média máxima mensal"
+FROM hr.jobs j
+LEFT JOIN hr.employees e ON j.JOB_ID = e.JOB_ID
+GROUP BY j.JOB_TITLE
+ORDER BY j.MAX_SALARY - j.MIN_SALARY ASC, j.JOB_TITLE ASC;
