551f467601ede288e7e576db5ab3b1e301cedde1
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-08-27 19:12:15 -0300

    ajusta

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,9 @@
 SELECT
   C.ContactName AS `Nome`,
   C.Country AS `País`,
-  COUNT(CC.Country) AS `Número de compatriotas`
+  COUNT(C.Country) AS `Número de compatriotas`
   FROM customers AS C, customers AS CC
-WHERE C.Country = CC.Country AND C.ContactName != CC.ContactName
-GROUP BY CC.ContactName, CC.Country
+WHERE CC.Country = C.Country AND CC.ContactName != C.ContactName
+GROUP BY C.ContactName, C.Country
 HAVING `Número de compatriotas` > 0
-ORDER BY CC.ContactName;
+ORDER BY C.ContactName;
