85078f384e93d20475ef20b5c8a7c8adaf753706
Thiagofox <thiagofox00@gmail.com>
2021-08-27 17:17:32 -0300

    desafio 5 e 6

--- desafio5.sql
+++ desafio5.sql
@@ -26,5 +26,3 @@ ROUND(J.MIN_SALARY / 12, 2)  AS `Média mínima mensal`,
 ROUND(J.MAX_SALARY / 12, 2)  AS `Média máxima mensal`
 FROM hr.jobs AS J
 ORDER BY (J.MAX_SALARY - J.MIN_SALARY), J.JOB_TITLE;
-
-
