6dd3fedc3f6dd279e346ede67159a325adfa5c66
Renan <renan.vamo@gmail.com>
2021-08-27 18:54:10 -0300

    Refatora CC

---
+++ desafio6.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) 'Nome Completo', j.JOB_TITLE 'Cargo', e.HIRE_DATE 'Data de início do cargo', d.DEPARTMENT_NAME 'Departamento'
+FROM hr.employees e
+INNER JOIN hr.jobs j
+ON j.JOB_ID = e.JOB_ID
+INNER JOIN hr.departments d
+ON d.DEPARTMENT_ID = e.DEPARTMENT_ID
+ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME), j.JOB_TITLE;
