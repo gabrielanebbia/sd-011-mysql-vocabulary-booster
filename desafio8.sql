242c311fb850f2b6de30d9ddd14dbc50579e8600
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-08-27 16:03:21 -0300

    Desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -6,4 +6,5 @@ INNER JOIN w3schools.orders AS orders
 ON orders.CustomerID = C.CustomerID
 INNER JOIN w3schools.shippers AS ship
 ON ship.ShipperID = orders.ShipperID
+WHERE ship.ShipperID IN(1, 2)
 ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;
