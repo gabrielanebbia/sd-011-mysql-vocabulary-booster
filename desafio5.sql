a03404f1114f2ada871489ad92fa7a2610b39c92
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-08-27 19:40:13 -0300

    Correções

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,8 @@
 SELECT
 j.JOB_TITLE AS Cargo,
-j.MAX_SALARY - j.MIN_SALARY AS "Variação Salarial",
-ROUND(j.MIN_SALARY/12, 2) AS "Média mínima mensal",
-ROUND(j.MAX_SALARY/12, 2) AS "Média máxima mensal"
+ROUND(AVG(j.MAX_SALARY - j.MIN_SALARY)) AS "Variação Salarial",
+ROUND(AVG(j.MIN_SALARY)/12, 2) AS "Média mínima mensal",
+ROUND(AVG(j.MAX_SALARY)/12, 2) AS "Média máxima mensal"
 FROM hr.jobs j
 GROUP BY j.JOB_TITLE
-ORDER BY j.MAX_SALARY - j.MIN_SALARY ASC, j.JOB_TITLE ASC;
+ORDER BY ROUND(AVG(j.MAX_SALARY - j.MIN_SALARY)) ASC, Cargo ASC;
