c82d1bd1e853e6def125ab84e6adea400a86c713
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-08-27 16:42:05 -0300

    Desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,6 @@
 SELECT
 UCASE(CONCAT(E.FIRST_NAME, " ", E.LAST_NAME)) AS "Nome completo",
-H.START_DATE AS "Data de início do cargo",
+H.START_DATE AS "Data de início",
 E.SALARY AS "Salário"
 FROM hr.job_history AS H
 INNER JOIN hr.employees AS E
