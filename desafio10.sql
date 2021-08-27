d1d1248da312ae266b19a66470444cca507987ea
Gisele Costa <jikinha@gmail.com>
2021-08-27 15:17:34 -0300

    desafio 8 resolvido, teste desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -4,5 +4,5 @@ MAX(od.Quantity) AS 'Máxima',
 ROUND(AVG(od.Quantity), 2) AS 'Média'
 FROM w3schools.products p
 INNER JOIN w3schools.order_details od ON od.ProductID = p.ProductID
-GROUP BY p.ProductID
+WHERE AVG(od.Quantity) > 20
 ORDER BY AVG(od.Quantity), p.ProductName;
