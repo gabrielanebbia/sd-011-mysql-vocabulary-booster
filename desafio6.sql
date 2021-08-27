fa9797cd39c97d8fc4fb25f5896af5222260b1a3
Diego Ribeiro <dihrm2001@gmail.com>
2021-08-27 16:03:13 -0300

    Desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,14 @@
+SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome Completo`,
+(
+  SELECT JOB_TITLE
+  FROM hr.jobs
+  WHERE e.JOB_ID = JOB_ID
+) AS `Cargo`,
+HIRE_DATE AS `Data de início do cargo`,
+(
+  SELECT DEPARTMENT_NAME
+  FROM hr.departments
+  WHERE e.DEPARTMENT_ID = DEPARTMENT_ID
+) AS `Departamento`
+FROM hr.employees AS e
+ORDER BY `Nome Completo`, `Cargo`;
