856646289b9d0950837d30c05cebc1180a237b2d
OldRoge <rogeriomrjr@icloud.com>
2021-08-27 16:38:05 -0300

    Done requirement 1

---
+++ desafio1.sql
@@ -0,0 +1,6 @@
+SELECT countries.COUNTRY_NAME AS País,
+		IF(regions.REGION_ID = 1, 'incluído', 'não incluído') AS "Status Inclusão"
+FROM hr.countries AS countries
+INNER JOIN hr.regions AS regions
+ON countries.REGION_ID = regions.REGION_ID
+ORDER BY COUNTRY_NAME ASC;
