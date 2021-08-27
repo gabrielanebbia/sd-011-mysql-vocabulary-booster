674e4942f764524b43125f46a1f5bc18e101c008
Josan Johnata <josanjohnata@gmail.com>
2021-08-27 15:43:43 -0300

    desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,2 @@
-SELECT COUNTRY_NAME AS 'Counrty', IF(REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
-FROM hr.countries ORDER BY 'Country';
+SELECT COUNTRY_NAME AS 'País', IF(REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries ORDER BY 'País';
