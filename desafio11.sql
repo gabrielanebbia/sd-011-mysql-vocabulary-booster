52d773e0fde04a83b8a01db3e54d50a71ff72cb8
Diego Ribeiro <dihrm2001@gmail.com>
2021-08-27 19:06:35 -0300

    Desafio 12

--- desafio11.sql
+++ desafio11.sql
@@ -6,4 +6,9 @@ Country AS `País`,
   WHERE c1.Country = c2.Country
 ) AS `Número de compatriotas`
 FROM w3schools.customers AS c1
+WHERE (
+  SELECT COUNT(*) - 1
+  FROM w3schools.customers AS c2
+  WHERE c1.Country = c2.Country
+) > 0
 ORDER BY `Nome`;
