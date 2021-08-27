ff5190f63835fd9ef4aac017602a626f8d7db0ae
Gisele Costa <jikinha@gmail.com>
2021-08-27 15:35:01 -0300

    code climate

--- desafio14*.sql
+++ desafio14*.sql
@@ -8,10 +8,9 @@ LIMIT 5;
 
 -- DÁ ERRO NO MOMENTO QUE COLOCO O ORDER BY, MAS ESTOU FAZENDO DA MANEIRA ENSINADA NO CURSO, TENTEI COLOCAR OUTRO SELECT MAS NÃO FUNCIONOU
 
- Considerando o conjunto formado pelas pessoas consumidoras e empresas fornecedoras de produtos, queremos saber quais são os cinco primeiros países distintos, em ordem alfabética, presentes nesse conjunto
+Considerando o conjunto formado pelas pessoas consumidoras e empresas fornecedoras de produtos, queremos saber quais são os cinco primeiros países distintos, em ordem alfabética, presentes nesse conjunto
 Exiba os 05 primeiros países distintos dentre as pessoas consumidoras e as empresas fornecedoras, em ordem alfabética.
 
 Use o banco w3schools como referência
 
 Monte uma query que exiba 01 coluna com o alias "País" contendo o nome do país.
-
--- desafio19*.sql
+++ desafio19*.sql
@@ -1,7 +1,5 @@
 
-
 Crie uma função chamada exibir_quantidade_pessoas_contratadas_por_mes_e_ano no banco de dados hr que, dados o mês e ano como parâmetros nessa ordem, retorna a quantidade de pessoas funcionárias que foram contratadas nesse mês e ano
 Use o banco hr como referência
 
 Crie uma função chamada exibir_quantidade_pessoas_contratadas_por_mes_e_ano no banco de dados hr que, dados o mês e ano como parâmetros nessa ordem, retorna a quantidade de pessoas funcionárias que foram contratadas nesse mês e ano. Confirme que sua função retorna o valor 14 ao ser chamada passando os números 6 e 1987 para mês e ano, respectivamente.
-
