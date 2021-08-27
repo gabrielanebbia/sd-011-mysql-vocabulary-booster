17ab00b8e1eb3e6079935136aa479635f5aee056
Hamander <1hamander@gmail.com>
2021-08-27 14:10:01 -0300

    desafio 10 fix

--- desafio10.sql
+++ desafio10.sql
@@ -7,4 +7,5 @@ FROM w3schools.products pr
 INNER JOIN w3schools.order_details ord
 ON pr.ProductID = ord.ProductID
 GROUP BY pr.ProductID
-HAVING `Média` > 20.00;
+HAVING `Média` > 20.00
+ORDER BY `Média`, `Produto`;
