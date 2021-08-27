8b83fd68e2c61da089b8ff95670bd9df2e1c7108
Amandha W. Barok <amandha.barok@gmail.com>
2021-08-27 15:56:56 -0700

    Fixing errors

--- desafio6.sql
+++ desafio6.sql
@@ -1,12 +1,8 @@
-SELECT * FROM job_history;
-
-
 SELECT 
-	CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-    j.JOB_TITLE AS 'Cargo',
-    e.HIRE_DATE AS 'Data de início do cargo',
-    d.DEPARTMENT_NAME AS 'Departamento'
-    
+CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
+j.JOB_TITLE AS 'Cargo',
+e.HIRE_DATE AS 'Data de início do cargo',
+d.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.job_history AS jh
 INNER JOIN hr.employees AS e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
 INNER JOIN hr.departments AS d ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
