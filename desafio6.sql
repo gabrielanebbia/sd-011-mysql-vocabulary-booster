6bfe70ef43e8b721eb8630c211be25c65c7e8997
Marco Galindo <marco1408@gmail.com>
2021-08-27 16:03:28 -0300

    desafio 6 finalizado

---
+++ desafio6.sql
@@ -0,0 +1,12 @@
+select concat(t1.first_name, ' ', t1.last_name) as `Nome completo`,
+t2.job_title as Cargo,
+t3.start_date as `Data de início do cargo`,
+t4.department_name as Departamento
+from employees as t1
+inner join jobs as t2
+on t1.job_id = t2.job_id
+inner join job_history as t3
+on t3.employee_id = t1.employee_id
+inner join departments as t4
+on t4.department_id = t3.department_id
+order by concat(t1.first_name, ' ', t1.last_name) desc;
