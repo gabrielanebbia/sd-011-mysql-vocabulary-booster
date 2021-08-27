8a46101d51846cc17f9c8f1978f73387c295adef
Filizzola <juliofilizzola@hotmail.com>
2021-08-27 15:50:29 -0300

    desafio 5 fixed

--- desafio5.sql
+++ desafio5.sql
@@ -2,4 +2,4 @@ SELECT JOB_TITLE AS Cargo,
 MAX_SALARY - MIN_SALARY AS 'Variação Salarial',
 ROUND(MIN_SALARY / 12, 2) AS 'Média mínima mensal',
 ROUND(MAX_SALARY / 12, 2) AS 'Média máxima mensal'
-FROM jobs GROUP BY JOB_ID ORDER BY `Variação Salarial`, Cargo;
+FROM hr.jobs GROUP BY JOB_ID ORDER BY `Variação Salarial`, Cargo;
