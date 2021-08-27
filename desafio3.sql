99684147a1185fb4df0c6af3c8ce5305048440fc
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-08-27 15:09:58 -0300

    testando formatacao

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,8 @@
 SELECT
-j.JOB_TITLE AS 'Cargo',
-(j.MAX_SALARY - j.MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
-FROM hr.jobs AS j
-ORDER BY (j.MAX_SALARY - j.MIN_SALARY),
-j.JOB_TITLE;
+  j.JOB_TITLE AS 'Cargo',
+  (j.MAX_SALARY - j.MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
+FROM
+  hr.jobs AS j
+ORDER BY
+  (j.MAX_SALARY - j.MIN_SALARY),
+  j.JOB_TITLE;
