732c64583ee00aae3c00b5e4c9979c34eb1f313e
Leandro Lopes <leandro_carlos@uol.com.br>
2021-08-27 19:32:11 -0300

    desafio4 corrigindo S

--- desafio4.sql
+++ desafio4.sql
@@ -1,11 +1,11 @@
-SELECT jobs.JOB_TITLE AS Cargo,
-round(AVG(e.salary), 2) AS 'Média Salarial',
+SELECT jobs.JOB_TITLE AS 'Cargo',
+round(AVG(e.salary), 2) AS 'Média salarial',
 CASE
 WHEN AVG(e.salary) >= 2000 AND AVG(e.salary) <= 5800 THEN 'Júnior'
 WHEN AVG(e.salary) >= 5801 AND AVG(e.salary) <= 7500 THEN 'Pleno'
 WHEN AVG(e.salary) >= 7501 AND AVG(e.salary) <= 10500 THEN 'Sênior'
 ELSE 'CEO'
-END AS Senioridade 
+END AS 'Senioridade' 
 FROM hr.jobs AS jobs
 INNER JOIN hr.employees AS e ON jobs.JOB_ID = e.JOB_ID
 GROUP BY e.JOB_ID
