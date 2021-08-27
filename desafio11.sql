0907f681332df58e0f29a434611c810f168c78d0
LeoFuna <leo.funa@gmail.com>
2021-08-27 17:05:58 -0300

    acerto requisito 6

--- desafio11.sql
+++ desafio11.sql
@@ -2,4 +2,5 @@ SELECT contactName AS 'Nome',
 country AS 'País', 
 (SELECT COUNT(country) - 1 FROM w3schools.customers GROUP BY country HAVING country = `País`) AS 'Número de compatriotas'
 FROM w3schools.customers
+HAVING `Número de compatriotas` <> 0
 ORDER BY `Nome`;
