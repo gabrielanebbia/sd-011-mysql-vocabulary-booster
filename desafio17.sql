6e48572e844304bdfa02b4168e8f58ac153d787f
Filizzola <juliofilizzola@hotmail.com>
2021-08-27 17:14:20 -0300

    desafio 17

--- desafio17.sql
+++ desafio17.sql
@@ -1,10 +1,10 @@
 USE w3schools;
 DELIMITER $$
 CREATE TRIGGER trigger_OrderDate_insert
-    BEFORE INSERT ON w3schools.orders
-    FOR EACH ROW
+BEFORE INSERT ON w3schools.orders
+FOR EACH ROW
 BEGIN
-  SET NEW.OrderDate = Date( NOW());
+SET NEW.OrderDate = Date( NOW());
 END $$
 
 DELIMITER ;
