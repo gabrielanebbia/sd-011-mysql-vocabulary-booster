95582842fbdf346a3701046b78e8baf3f0d3e8eb
Filizzola <juliofilizzola@hotmail.com>
2021-08-27 15:49:01 -0300

    desafio 5 ref

--- desafio3.sql
+++ desafio3.sql
@@ -1,2 +1,4 @@
-SELECT JOB_TITLE AS 'Cargo', MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máaximo e minimo' FROM hr.jobs 
-GROUP BY JOB_ID ORDER BY `Diferença entre salários máaximo e minimo` ASC, Cargo ASC;
+SELECT JOB_TITLE AS 'Cargo',
+MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máximo e minimo'
+FROM hr.jobs 
+GROUP BY JOB_ID ORDER BY `Diferença entre salários máximo e minimo`, Cargo;
