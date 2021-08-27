680c77239736f00218172a47cde900ad21ab9248
Hamander <1hamander@gmail.com>
2021-08-27 14:08:26 -0300

    desafio 10 completo

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT 
+pr.ProductName AS 'Produto',
+MIN(ord.Quantity) AS 'Mínima',
+MAX(ord.Quantity) AS 'Máxima',
+ROUND(AVG(ord.Quantity), 2) AS 'Média'
+FROM w3schools.products pr
+INNER JOIN w3schools.order_details ord
+ON pr.ProductID = ord.ProductID
+GROUP BY pr.ProductID
+HAVING `Média` > 20.00;
