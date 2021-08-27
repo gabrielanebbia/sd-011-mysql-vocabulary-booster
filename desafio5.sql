6e69fd4b92e300e4eb4edef936bfadca62b2c37f
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-08-27 17:16:07 -0300

    v

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,6 @@
-SELECT t2.job_title AS "Cargo",
-t2.max_salary - t2.min_salary AS "Variação Salarial",
-ROUND(AVG(t2.min_salary / 12), 2) AS "Média mínima mensal",
-ROUND(AVG(t2.max_salary / 12), 2) AS "Média máxima mensal"
-FROM employees AS t1
-INNER JOIN jobs AS t2
-ON t1.job_id = t2.job_id
-GROUP BY t1.job_id
-ORDER BY `Variação Salarial`, t2.job_title;
+SELECT job_title AS "Cargo",
+max_salary - min_salary AS "Variação Salarial",
+ROUND(AVG(min_salary / 12), 2) AS "Média mínima mensal",
+ROUND(AVG(max_salary / 12), 2) AS "Média máxima mensal"
+FROM jobs
+ORDER BY `Variação Salarial`, job_title;
