a5bbde8021544998d0e81fa7daa9ff13d3839cdd
anaclarabck <ackyotoku@gmail.com>
2021-08-27 19:04:53 -0300

    [ADD] answers 12~15

---
+++ desafio13.sql
@@ -0,0 +1,7 @@
+SELECT products.ProductName AS 'Produto',
+products.Price AS 'Preço'
+FROM w3schools.order_details AS orderDetails
+INNER JOIN w3schools.products AS products
+ON orderDetails.ProductID = products.ProductID
+WHERE orderDetails.Quantity > 80
+ORDER BY products.ProductName ASC;
---
+++ desafio15.sql
@@ -0,0 +1,16 @@
+USE hr;
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(
+    IN cargo VARCHAR(30)
+)
+BEGIN
+	SELECT ROUND(AVG(SALARY),2) AS 'Média salarial'
+	FROM hr.employees AS e
+    INNER JOIN hr.jobs AS j
+    ON j.JOB_ID = e.JOB_ID
+    WHERE j.JOB_TITLE = cargo
+	GROUP BY j.JOB_ID;
+END $$
+
+DELIMITER ;
