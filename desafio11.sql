91ffab38bac3faa408773c2e5dd2258fc9bf02e0
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-08-27 16:33:17 -0300

    desafio 11 remove valores com 0 compatriotas

--- desafio11.sql
+++ desafio11.sql
@@ -10,8 +10,19 @@ SELECT
       WHERE
         c2.Country = c1.Country
     ) - 1
-  ) AS 'Número de compatriotas'
+  ) AS `Número de compatriotas`
 FROM
   w3schools.customers AS c1
+WHERE
+  (
+    (
+      SELECT
+        COUNT(*)
+      FROM
+        w3schools.customers AS c2
+      WHERE
+        c2.Country = c1.Country
+    ) - 1
+  ) > 0
 ORDER BY
   c1.ContactName;
