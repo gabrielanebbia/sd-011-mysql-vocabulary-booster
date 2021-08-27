5551ee1ff00359d6fc7a9281d636f8d97a604c92
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-08-27 15:15:52 -0300

    adiciona requisito 01

---
+++ desafio01.sql
@@ -0,0 +1,10 @@
+SELECT
+countries.country_name,
+(
+IF(regions.region_name = 'Europe', 'incluído', 'não incluído')
+) AS `Status Inclusão`
+FROM
+hr.countries AS countries
+INNER JOIN
+hr.regions AS regions
+ON countries.region_id = regions.region_id;
