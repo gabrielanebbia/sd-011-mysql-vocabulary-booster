9d6130ed25e2636e6c054099be81c589b0302a9d
Gisele Costa <jikinha@gmail.com>
2021-08-27 15:24:58 -0300

    DESAFIO 7

--- desafio7.sql
+++ desafio7.sql
@@ -3,7 +3,5 @@ jh.start_date AS 'Data de início',
 e.salary AS 'Salário'
 FROM hr.employees e
 INNER JOIN hr.job_history jh ON e.employee_id = jh.employee_id
+WHERE MONTH(jh.start_date) IN (01, 02, 03)
 ORDER BY UCASE(CONCAT(e.first_name, ' ', e.last_name)), jh.start_date;
-
--- pode ser que não seja jh.start_date e sim e.hire_date
--- aí não precisaria de inner join
