cdaa296b8f12889268c953438489e3ef2ee312de
Reginara Gomes <refagomes@gmail.com>
2021-08-27 17:55:13 -0300

    Adiciona requisito 10

---
+++ desafio10.sql
@@ -0,0 +1,13 @@
+SELECT * FROM w3schools.products;
+
+SELECT
+details_table.ProductID 'Produto',
+MIN(details_table.Quantity) AS 'Mínima',
+MAX(details_table.Quantity) AS 'Máxima',
+ROUND(AVG(details_table.Quantity), 2) AS 'Média'
+FROM
+w3schools.order_details AS details_table
+INNER JOIN
+w3schools.products AS products_table ON details_table.ProductID = products_table.ProductID
+GROUP BY `Produto`HAVING `Média` > 20.00
+ORDER BY `Média` ASC, `Produto` ASC;
