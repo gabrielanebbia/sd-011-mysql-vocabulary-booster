85fa0c3a7fb15aa97249b52002e6b798f3257aae
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-27 15:24:03 -0300

    Desafios 3 a 6

---
+++ desafio6.sql
@@ -0,0 +1,12 @@
+SELECT CONCAT(A.first_name, ' ', A.last_name) AS 'Nome completo',
+B.JOB_TITLE AS Cargo,
+C.START_DATE AS 'Data de início do cargo',
+D.DEPARTMENT_NAME AS 'Departamento'
+FROM hr.employees AS A
+INNER JOIN hr.jobs AS B
+ON A.JOB_ID = B.JOB_ID
+INNER JOIN hr.job_history AS C
+ON A.EMPLOYEE_ID = C.EMPLOYEE_ID
+INNER JOIN hr.departments AS D
+ON C.DEPARTMENT_ID = D.DEPARTMENT_ID
+ORDER BY 'Nome completo' DESC, Cargo;
