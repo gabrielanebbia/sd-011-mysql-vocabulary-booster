268fbdc9f98c64898c8b74bda6f2e2dc06263b7a
LeoLacana <wilson17414@gmail.com>
2021-08-27 19:21:54 -0300

    requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+SELECT
+UCASE(CONCAT(e.first_name, ' ', e.last_name)) AS 'Nome completo',
+h.start_date AS 'Data de início',
+e.salary AS 'Salário'
+FROM hr.job_history h
+INNER JOIN hr.employees e
+ON h.employee_id = e.employee_id
+WHERE MONTH(h.start_date) IN(1, 2, 3)
+ORDER BY `Nome completo`, h.start_date;
