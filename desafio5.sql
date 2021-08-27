a193a0187bbfd561cb45dd2aa1ea9971a3966276
Pedro Piquini <piquini.pedro@gmail.com>
2021-08-27 18:41:48 -0300

    ajustes nos desafios 5 e 6, desafio 7 completo

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,7 @@
 SELECT j.JOB_TITLE AS 'Cargo',
 (MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
-(MIN_SALARY / 12) AS 'Média mínima mensal',
-(MAX_SALARY / 12) AS 'Média máxima mensal'
+ROUND((MIN_SALARY / 12), 2) AS 'Média mínima mensal',
+ROUND((MAX_SALARY / 12), 2) AS 'Média máxima mensal'
 FROM jobs AS j
 INNER JOIN employees AS e
 ON e.JOB_ID = j.JOB_ID
--- desafio6.sql
+++ desafio6.sql
@@ -9,4 +9,4 @@ INNER JOIN jobs AS j
 ON j.JOB_ID = h.JOB_ID
 INNER JOIN departments AS d
 ON d.DEPARTMENT_ID = h.DEPARTMENT_ID
-ORDER BY `Nome completo` DESC, `Cargo
+ORDER BY `Nome completo` DESC, `Cargo`;
