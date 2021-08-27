d1632d2d55ce7026ef76ed991ad06527c641be14
Thyago Pessoa <flash_thyago@hotmail.com>
2021-08-27 15:33:54 -0300

    Desafio 3

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,1 @@
-SELECT COUNTRY_NAME AS País, IF(REGION_ID = 1, 'Incluído', 'Não Incluído') AS 'Status Inclusão' FROM hr.countries ORDER BY `País`;
+SELECT COUNTRY_NAME AS País, IF(REGION_ID = 1, 'Incluído', 'Não Incluído') AS 'Status Inclusão' FROM hr.countries ORDER BY COUNTRY_NAME;
