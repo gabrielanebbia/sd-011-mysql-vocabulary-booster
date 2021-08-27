097415ea37e376b4a397735dc2e93d5f27eb08c2
Matheus Gaspar <msouzagaspar@gmail.com>
2021-08-27 22:17:12 +0200

    correção desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,4 @@
 SELECT country_name as 'País',
-IF(region_id = 1, 'incluído', 'não incluído') as 'Status Inclusão' FROM countries;
+IF(region_id = 1, 'incluído', 'não incluído') as 'Status Inclusão' 
+FROM countries 
+ORDER BY country_name;
