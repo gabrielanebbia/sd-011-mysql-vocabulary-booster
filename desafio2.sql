75458bfb035c524a064203c559d3c0add887da94
rubia braga <rubiaemilly29@gmail.com>
2021-08-27 16:58:21 -0300

    Requisito-2

---
+++ desafio2.sql
@@ -0,0 +1,8 @@
+SELECT JOB_TITLE AS 'Cargo', 
+CASE WHEN hr.jobs.MAX_SALARY >= 5000 AND hr.jobs.MAX_SALARY <= 10000 THEN 'Baixo'
+WHEN hr.jobs.MAX_SALARY >= 10001 AND hr.jobs.MAX_SALARY <= 20000 THEN 'Médio'
+WHEN hr.jobs.MAX_SALARY >= 20001 AND hr.jobs.MAX_SALARY <= 30000 THEN 'Alto'
+ELSE 'Altíssimo'
+END AS 'Nível'
+FROM hr.jobs
+ORDER BY JOB_TITLE;
