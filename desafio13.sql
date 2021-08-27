6827826bd4660dd94a45cbb2e01a555effafa938
Marco Galindo <marco1408@gmail.com>
2021-08-27 18:08:52 -0300

    adicionando banco de dados aos 13

--- desafio13.sql
+++ desafio13.sql
@@ -1,7 +1,7 @@
 select t1.productname as Produto,
 t1.price as Preço
-from products as t1
-inner join order_details as t2
+from w3schools.products as t1
+inner join w3schools.order_details as t2
 on t1.productid = t2.productid
 where t2.quantity > 80
 order by Produto;
