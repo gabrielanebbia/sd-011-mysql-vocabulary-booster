e39881ff32ca293eec5357d15e6ef0dd59b619a1
Matheus Alexandre <alexandremhm@gmail.com>
2021-08-27 17:37:55 -0300

    Add requisito 9

---
+++ desafio9.sql
@@ -0,0 +1,7 @@
+SELECT UCASE(CONCAT(a.FirstName, ' ', a.LastName)) AS `Nome completo`, 
+COUNT(b.OrderId) AS "Total de pedidos" 
+FROM employees AS a
+INNER JOIN orders AS b
+ON a.EmployeeID = b.EmployeeID
+GROUP BY UCASE(CONCAT(a.FirstName, ' ', a.LastName))
+ORDER BY COUNT(b.OrderId);
