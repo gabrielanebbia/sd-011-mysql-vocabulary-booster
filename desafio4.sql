46f4f761c4e662e82c0d662edaa699c20c7117f8
KaioHPs <kaiohpsalternativo@gmail.com>
2021-08-27 16:23:44 -0300

    Alteraçoes de Estilo

--- desafio4.sql
+++ desafio4.sql
@@ -1,11 +1,11 @@
 SELECT j.JOB_TITLE AS 'Cargo', ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
-	CASE
-        WHEN ROUND(AVG(e.SALARY), 2) >= 2000 AND ROUND(AVG(e.SALARY), 2) <= 5800 THEN 'Júnior'
-        WHEN ROUND(AVG(e.SALARY), 2) >= 5801 AND ROUND(AVG(e.SALARY), 2) <= 7500 THEN 'Pleno'
-        WHEN ROUND(AVG(e.SALARY), 2) >= 7501 AND ROUND(AVG(e.SALARY), 2) <= 10500 THEN 'Sênior'
-        WHEN ROUND(AVG(e.SALARY), 2) > 10500 THEN 'CEO'
-        ELSE 'Não Registrado'
-    END AS 'Nível'
+CASE
+    WHEN ROUND(AVG(e.SALARY), 2) >= 2000 AND ROUND(AVG(e.SALARY), 2) <= 5800 THEN 'Júnior'
+    WHEN ROUND(AVG(e.SALARY), 2) >= 5801 AND ROUND(AVG(e.SALARY), 2) <= 7500 THEN 'Pleno'
+    WHEN ROUND(AVG(e.SALARY), 2) >= 7501 AND ROUND(AVG(e.SALARY), 2) <= 10500 THEN 'Sênior'
+    WHEN ROUND(AVG(e.SALARY), 2) > 10500 THEN 'CEO'
+    ELSE 'Não Registrado'
+END AS 'Nível'
 FROM hr.employees AS e
 LEFT JOIN hr.jobs AS j
 ON j.JOB_ID = e.JOB_ID
