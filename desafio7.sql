8d018d9a89397a9b5830f8e9ce61105dae3ce528
Amandha W. Barok <amandha.barok@gmail.com>
2021-08-27 16:45:31 -0700

    Finishing the seventh requiriment

---
+++ desafio7.sql
@@ -0,0 +1,10 @@
+SELECT * FROM job_history;
+SELECT * FROM employees;
+
+SELECT 
+UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+jh.START_DATE AS 'Data de início',
+e.SALARY AS 'Salário'
+FROM hr.job_history AS jh
+INNER JOIN hr.employees AS e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+ORDER BY UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)), jh.START_DATE;
