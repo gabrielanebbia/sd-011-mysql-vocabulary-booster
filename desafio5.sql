fa23f2dbd71236a05eb04d1a5e12760dce7143f2
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-08-27 18:00:24 -0300

    Desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,5 @@
 select j.JOB_TITLE AS 'Cargo',
-j.MAX_SALARY- j.MIN_SALARY AS 'Variação salarial',
+j.MAX_SALARY- j.MIN_SALARY AS 'Variação Salarial',
 ROUND(j.MIN_SALARY / 12, 2) AS 'Média mínima mensal',
 ROUND(j.MAX_SALARY / 12, 2) AS 'Média máxima mensal'
 FROM hr.employees AS e
