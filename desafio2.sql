c9dbcd460c10ee6071c055a18febd59f4698464b
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-08-27 18:26:20 -0300

    task 2

---
+++ desafio2.sql
@@ -0,0 +1,10 @@
+SELECT job_title as 'Cargo',
+CASE WHEN max_salary > 30000 THEN 'Altíssimo'
+WHEN max_salary >= 20001 THEN 'Alto'
+WHEN max_salary >= 10001 THEN 'Médio'
+ELSE 'Baixo'
+END as 'Nível'
+FROM hr.jobs
+ORDER BY Cargo ASC;
+
+-- Programmer = Baixo :(
