f8da00ef75aec1a25f5ddba87192bbda5f3d056e
Reginara Gomes <refagomes@gmail.com>
2021-08-27 16:19:22 -0300

    Corrige requisito 8, não sabia que tava errando nisso

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,5 @@
 SELECT
-customers_table.ContactName AS 'Nome do Contato',
+customers_table.ContactName AS 'Nome de Contato',
 shippers_table.ShipperName AS 'Empresa que fez o envio',
 orders_table.OrderDate AS 'Data do pedido'
 FROM
@@ -10,4 +10,4 @@ INNER JOIN
 w3schools.shippers AS shippers_table ON orders_table.ShipperID = shippers_table.ShipperID
 WHERE
 shippers_table.ShipperID IN (1, 2)
-ORDER BY `Nome do Contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;
+ORDER BY `Nome de Contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;
