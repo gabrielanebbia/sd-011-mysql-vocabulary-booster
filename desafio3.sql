a61e55ea46f53619a75a39a84a6902e2fe9190c7
Filizzola <juliofilizzola@hotmail.com>
2021-08-27 15:44:44 -0300

    desafio 6 e desafio 3 ref

--- desafio3.sql
+++ desafio3.sql
@@ -1,2 +1,2 @@
 SELECT JOB_TITLE AS 'Cargo', MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máaximo e minimo' FROM hr.jobs 
-GROUP BY JOB_ID ORDER BY `Diferença entre salários máaximo e minimo`, Cargo;
+GROUP BY JOB_ID ORDER BY `Diferença entre salários máaximo e minimo` ASC, Cargo ASC;
---
+++ desafio6.sql
@@ -0,0 +1,10 @@
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo', j.JOB_TITLE AS Cargo, jh
+.START_DATE AS 'Data de início do cargo', d.DEPARTMENT_NAME AS Departamento 
+FROM employees AS e
+INNER JOIN hr.job_history AS jh
+ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+INNER JOIN hr.jobs AS j
+ON jh.JOB_ID = jh.JOB_ID
+INNER JOIN hr.departments AS d
+ON e.DEPARTMENT_ID = jh.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC, Cargo ASC;
