7ce3851bbb98ed7eb33406336bea2e5678d4d23e
Maran <maranmorimoto@gmail.com>
2021-08-27 20:52:59 -0300

    requirement 08 done

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+SELECT 
+c.ContactName AS 'Nome do contato',
+s.ShipperName AS 'Empresa que fez o envio',
+o.OrderDate AS 'Data do pedido'
+FROM w3schools.orders AS o
+INNER JOIN w3schools.customers c
+ON o.CustomerID = c.CustomerID
+INNER JOIN w3schools.shippers s
+ON o.ShipperID = s.ShipperID
+ORDER BY c.ContactName, s.ShipperName, o.OrderDate;
