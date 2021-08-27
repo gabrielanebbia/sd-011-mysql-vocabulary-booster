68d0975c640dd2cd1753962173f400720136cbd7
Diogo Augusto <diogotrescastro@gmail.com>
2021-08-27 17:55:44 -0300

    requisitos 8 e 9

---
+++ desafio9.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(E.FirstName, ' ', E.LastName) AS 'Nome completo',
+COUNT(O.OrderID) AS 'Total de pedidos'
+FROM w3schools.orders AS O
+INNER JOIN w3schools.employees AS E
+ON O.EmployeeID = E.EmployeeID
+GROUP BY `Nome Completo`
+ORDER BY `Total de Pedidos`;
