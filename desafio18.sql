9aa9120f8233d6ea8cf47827116516202858950c
rodolforezende <rodolforrcont@gmail.com>
2021-08-27 17:33:03 -0300

    Desafio 18 - Corrigido

--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT(e.FIRST_NAME, ' ' , e.LAST_NAME) as 'Nome completo',
 DATE_FORMAT(jh.START_DATE, '%d/%m/%Y') AS 'Data de início',
 DATE_FORMAT(jh.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
-ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE)/365.25, 2) AS 'Anos trabalhados'
+ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE)/365, 2) AS 'Anos trabalhados'
 FROM hr.employees AS e,
 hr.job_history AS jh
 WHERE e.EMPLOYEE_ID = jh.EMPLOYEE_ID
