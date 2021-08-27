668d722098d2ebffd28e1a8304b65b4de63da90c
Tulio Sploradori <tuliospl@gmail.com>
2021-08-27 16:39:04 -0300

    Req 9

--- desafio10.sql
+++ desafio10.sql
@@ -1,6 +1,6 @@
 SELECT
 P.ProductName AS "Nome do produto",
-OD.Quantity
+COUNT(OD.EmployeeID) AS 'Total de pedidos'
 FROM w3schools.order_details AS OD
 INNER JOIN w3schools.products AS P
 ON OD.ProductID = P.ProductID
--- desafio9.sql
+++ desafio9.sql
@@ -1,6 +1,9 @@
 SELECT
-CONCAT(E.FirstName, " " , E.LastName) AS "Nome Completo"
+CONCAT(E.FirstName, " " , E.LastName) AS "Nome Completo",
+COUNT(O.EmployeeID) AS "Total de pedidos"
 FROM w3schools.orders AS O
 INNER JOIN w3schools.employees AS E
 ON E.EmployeeID = O.EmployeeID
-WHERE O.CustomerID = E.EmployeeID;
+WHERE O.CustomerID = E.EmployeeID
+GROUP BY O.EmployeeID
+ORDER BY "Total de pedidos";
