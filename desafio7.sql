43563f8ebb2dcd1b761482f91883216c9bd2eb7c
Víctor Faria <vitilevi@hotmail.com>
2021-08-27 17:25:23 -0300

    Feito requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+jh.START_DATE AS 'Data de início',
+e.SALARY AS 'Salário'
+FROM employees AS e
+INNER JOIN job_history AS jh
+ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE MONTH(jh.START_DATE) in (1, 2, 3)
+ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME), jh.START_DATE;
