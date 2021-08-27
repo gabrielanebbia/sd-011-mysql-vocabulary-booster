90717b63650100f2c3185ce22051419f3c33ef4f
Hamander <1hamander@gmail.com>
2021-08-27 12:28:28 -0300

    desafio5 adicionado

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+SELECT * FROM hr.jobs;
+SELECT JOB_TITLE AS 'Cargo', 
+MAX_SALARY - MIN_SALARY AS 'Variação Salarial',
+ROUND(AVG(MIN_SALARY) / 12, 2) AS 'Média mínima mensal',
+ROUND(AVG(MAX_SALARY) / 12, 2) AS 'Média máxima mensal'
+FROM hr.jobs
+GROUP BY `Cargo`
+ORDER BY `Variação Salarial`, JOB_TITLE;
