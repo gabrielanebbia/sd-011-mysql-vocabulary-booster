302cf6a4737c090f62f0148ccf9c4d95ebb74adb
LeoLacana <wilson17414@gmail.com>
2021-08-27 17:34:38 -0300

    requisito 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,12 +1,12 @@
-SELECT j.job_title AS 'Cargo', ROUND(AVG(e.salary), 2) AS 'Média salarial',
+SELECT j.job_title AS 'Cargo', ROUND(AVG(e.salary),2) AS 'Média salarial',
 CASE
-WHEN AVG(e.salary) <= 5000.00 THEN 'Júnior'
-WHEN AVG(e.salary) <= 7500.00 THEN 'Pleno'
-WHEN AVG(e.salary) <= 10500.00 THEN 'Sênior'
+WHEN AVG(e.salary) <= 5800 THEN 'Júnior'
+WHEN AVG(e.salary) <= 7500 THEN 'Pleno'
+WHEN AVG(e.salary) <= 10500 THEN 'Sênior'
 ELSE 'CEO'
-END AS 'Senioridade'
-FROM hr.jobs AS j
-INNER JOIN hr.employees AS e
-ON j.JOB_ID = e.JOB_ID
+END as 'Senioridade'
+FROM hr.jobs j
+INNER JOIN hr.employees e
+ON e.job_id = j.job_id
 GROUP BY job_title
 ORDER BY AVG(e.salary), j.job_title;
