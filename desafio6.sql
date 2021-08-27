4305ecfe34856112267e63447d4f7e75dd3d5c81
Bruno Duarte <brunoarduarte@gmail.com>
2021-08-27 19:04:47 -0300

    Desafio 7

--- desafio6.sql
+++ desafio6.sql
@@ -3,7 +3,7 @@ J.JOB_TITLE AS `Cargo`,
 JH.START_DATE AS `Data de início do cargo`,
 D.DEPARTMENT_NAME AS `Departamento`
 FROM employees AS E
-INNER JOIN jobs AS J ON E.JOB_ID = J.JOB_ID
 INNER JOIN job_history AS JH ON E.EMPLOYEE_ID= JH.EMPLOYEE_ID
 INNER JOIN departments AS D ON E.DEPARTMENT_ID = D.DEPARTMENT_ID
+INNER JOIN jobs AS J ON JH.JOB_ID = J.JOB_ID
 ORDER BY `Nome completo` DESC, `Cargo`;
