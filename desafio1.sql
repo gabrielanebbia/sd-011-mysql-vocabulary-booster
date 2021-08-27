c7373687da2e74ae99fda577b2f2c21587fd0108
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-08-27 18:06:10 -0300

    task 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,4 @@
--- initial task
+SELECT country_name as 'País',
+IF (region_id = 1, 'incluído', 'não incluído') as 'Status Inclusão'
+FROM hr.countries
+ORDER BY País ASC;
---
+++ tests.sql
@@ -0,0 +1,8 @@
+SELECT * FROM hr.countries
+ORDER BY region_id ASC, country_name ASC;
+
+-- region_id
+-- 1 = Europe
+-- 2 = America
+-- 3 = Asia/Oceania
+-- 4 = Africa
