d877585275a88360c78f8486a444a27ed10e8023
LeoFuna <leo.funa@gmail.com>
2021-08-27 17:01:37 -0300

    acerto no desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,4 @@
-SELECT * FROM hr.jobs;
-SELECT job_title AS Cargo, 
+SELECT job_title AS 'Cargo', 
 (max_salary - min_salary)  AS 'Variação Salarial', 
 ROUND((min_salary / 12), 2) AS 'Média mínima mensal',
 ROUND((max_salary / 12), 2) AS 'Média máxima mensal'
