4055ad1d1855b3ae32a7471a434a08dcc6e549ba
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-08-27 15:19:55 -0300

    desafio 6

--- desafio5.sql
+++ desafio5.sql
@@ -4,7 +4,7 @@ SELECT
   round(j.MIN_SALARY / 12) AS 'Média mínima mensal',
   Round(j.MAX_SALARY / 12, 2) AS 'Média máxima mensal'
 FROM
-  hr.jobs j
+  hr.jobs AS j
 ORDER BY
   (j.MAX_SALARY - j.MIN_SALARY),
   j.JOB_TITLE;
