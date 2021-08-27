9b508dcf2bdf007c1e1d3b5e7578f4aff03efdf2
Matheus Alexandre <alexandremhm@gmail.com>
2021-08-27 18:21:30 -0300

    Fix requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,8 @@
 SELECT A.ContactName AS "Nome",
 A.Country AS "País", 
-COUNT(A.Country) AS "Número de compatriotas"
+COUNT(A.Country) - 1 AS "Número de compatriotas"
 FROM customers AS A, customers AS B
 WHERE A.Country = B.Country
 GROUP BY A.ContactName
 ORDER BY A.ContactName;
+
