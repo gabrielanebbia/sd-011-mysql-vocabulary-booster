a166f764e94c939eb9cae73eac54a221ecd95fea
Arthur Hermann <arthurhermannc@gmail.com>
2021-08-27 15:38:11 -0300

    r14 r15 and r16

---
+++ desafio15.sql
@@ -0,0 +1,17 @@
+USE hr;
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(
+	IN cargo VARCHAR(200)
+)
+BEGIN
+SELECT 
+    ROUND(AVG(SALARY), 2)
+FROM
+    employees AS employees
+        INNER JOIN
+    jobs AS jobs ON jobs.JOB_ID = employees.JOB_ID
+        AND jobs.JOB_TITLE = cargo;
+END $$
+
+DELIMITER ;
