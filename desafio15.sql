8666305cc5d6cff3b267ae6e679a5e7e27ad18d1
anaclarabck <ackyotoku@gmail.com>
2021-08-27 19:06:10 -0300

    [UPDATE] indentation fixed

--- desafio15.sql
+++ desafio15.sql
@@ -1,16 +1,14 @@
 USE hr;
 DELIMITER $$
 
-CREATE PROCEDURE buscar_media_por_cargo(
-    IN cargo VARCHAR(30)
-)
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR 30))
 BEGIN
-	SELECT ROUND(AVG(SALARY),2) AS 'Média salarial'
-	FROM hr.employees AS e
-    INNER JOIN hr.jobs AS j
-    ON j.JOB_ID = e.JOB_ID
-    WHERE j.JOB_TITLE = cargo
-	GROUP BY j.JOB_ID;
+SELECT ROUND(AVG(SALARY),2) AS 'Média salarial'
+FROM hr.employees AS e
+INNER JOIN hr.jobs AS j
+ON j.JOB_ID = e.JOB_ID
+WHERE j.JOB_TITLE = cargo
+GROUP BY j.JOB_ID;
 END $$
 
 DELIMITER ;
