888fd40979002cfd56653551cc7456293e0e340f
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-08-27 17:11:23 -0300

    cc

--- desafio8.sql
+++ desafio8.sql
@@ -4,8 +4,8 @@
   O.OrderDate AS 'Data do pedido'
   FROM w3schools.customers AS C
   INNER JOIN w3schools.orders AS O
-   ON O.CustomerID = C.CustomerID
+  ON O.CustomerID = C.CustomerID
   INNER JOIN w3schools.shippers AS S
-   ON S.ShipperID = O.ShipperID
+  ON S.ShipperID = O.ShipperID
   WHERE S.ShipperName IN ('Speedy Express', 'United Package')
   ORDER BY 'Nome de contato' , 'Empresa que fez o envio', 'Data do pedido';
