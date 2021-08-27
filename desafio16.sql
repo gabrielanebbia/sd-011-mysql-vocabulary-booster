3448e6b1c76a1ec57889ae288bc27efc08aaa9d6
Filizzola <juliofilizzola@hotmail.com>
2021-08-27 17:09:15 -0300

    fixed desafio 16 e desafio 17

--- desafio16.sql
+++ desafio16.sql
@@ -3,13 +3,13 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(EMAIL VARCHAR(50))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE job INT;
-    SELECT COUNT(j.JOB_ID) FROM hr.employees AS e
-    INNER JOIN hr.job_history AS j
-    ON e.EMPLOYEE_ID = j.EMPLOYEE_ID
-    WHERE e.EMAIL = EMAIL
-    INTO job;
-    RETURN job;
+DECLARE job INT;
+SELECT COUNT(j.JOB_ID) FROM hr.employees AS e
+INNER JOIN hr.job_history AS j
+ON e.EMPLOYEE_ID = j.EMPLOYEE_ID
+WHERE e.EMAIL = EMAIL
+INTO job;
+RETURN job;
 END $$
 
 DELIMITER ;
---
+++ desafio17.sql
@@ -0,0 +1,10 @@
+USE w3schools;
+DELIMITER $$
+CREATE TRIGGER trigger_OrderDate_insert
+    BEFORE INSERT ON w3schools.orders
+    FOR EACH ROW
+BEGIN
+  SET NEW.OrderDate = Date( NOW());
+END $$
+
+DELIMITER ;
