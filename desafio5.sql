2d39918fc8a5272feb7983a545faf9be3c3e53f9
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-08-28 16:36:55 -0300

    requisito 5 concluido

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+SELECT j.job_title AS Cargo,
+j.max_salary - j.min_salary AS 'Variação Salarial',
+ROUND(MIN(e.salary), 2) AS 'Média mínima mensal',
+ROUND(MAX(e.salary), 2) AS 'Média máxima mensal'
+FROM hr.jobs AS j
+INNER JOIN hr.employees AS e
+ON j.JOB_ID = e.job_id
+GROUP BY job_title
+ORDER BY `Variação Salarial`, job_title;
