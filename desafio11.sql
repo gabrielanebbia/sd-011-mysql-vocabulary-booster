c28b9bd4d981c3deb09bdd38893b8be9692a8577
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-08-28 15:37:51 -0300

    Fixing unexpected tabs

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,8 @@
 SELECT C.ContactName AS 'Nome',
 C.Country AS 'País',
 (
-	SELECT COUNT(Country) - 1 FROM w3schools.customers
-    WHERE w3schools.customers.Country = C.Country
+SELECT COUNT(Country) - 1 FROM w3schools.customers
+WHERE w3schools.customers.Country = C.Country
 ) AS 'Número de compatriotas'
 FROM w3schools.customers AS C
 HAVING `Número de compatriotas` > 0
