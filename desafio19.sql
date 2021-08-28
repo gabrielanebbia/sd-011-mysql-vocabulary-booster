48e4b54e611c131fba1480bebfe89306d743a084
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-08-27 23:31:26 -0300

    desafio 19

---
+++ desafio19.sql
@@ -0,0 +1,10 @@
+DELIMITER $$
+
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(parametro1, parametro2, ..., parametroN)
+RETURNS tipo_de_dado tipo_de_retorno
+BEGIN
+    query_sql
+    RETURN resultado_a_ser_retornado;
+END $$
+
+DELIMITER ;
