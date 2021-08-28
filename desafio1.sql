77fe95e607b0cfe6779842f14468b458f87ded9f
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-08-28 12:22:07 -0300

    Desafio 1

---
+++ desafio1.sql
@@ -0,0 +1,12 @@
+#Crie uma query que mostre se o país pertence ou não à região "Europe".
+#Use o banco hr como referência
+#Seu relatório deve apresentar 02 colunas:
+#A primeira deve possuir o alias "País" e exibir o nome do país.
+#A segunda deve possuir o alias "Status Inclusão" e deve exibir "incluído" se o país em questão está incluso em "Europe", ou "não incluído", caso contrário.
+#Os resultados devem estar ordenados pelo nome do país em ordem alfabética.
+SELECT c.COUNTRY_NAME, ( 
+SELECT IF ((SELECT REGION_ID FROM hr.regions WHERE hr.regions.REGION_ID = r.REGION_ID) = 1, "incluído", "não incluído"))
+AS "Status Inclusão" FROM hr.countries AS c
+INNER JOIN hr.regions as r
+ON c.REGION_ID = r.REGION_ID
+ORDER BY c.COUNTRY_NAME ASC; 
