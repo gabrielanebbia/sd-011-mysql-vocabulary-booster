fdef068a675d8d79c6881e8fec99cf5780c8d9c8
Matheus Gaspar <msouzagaspar@gmail.com>
2021-08-28 10:49:52 +0200

    correção desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -6,4 +6,4 @@ FROM w3schools.orders AS t1
 INNER JOIN w3schools.customers AS t2 ON t1.CustomerID = t2.CustomerID
 INNER JOIN w3schools.shippers AS t3 ON t1.ShipperID = t3.ShipperID
 WHERE ShipperName IN ('Speedy Express', 'United Package')
-ORDER BY ContactName, OrderDate;
+ORDER BY ContactName, ShipperName, OrderDate;
