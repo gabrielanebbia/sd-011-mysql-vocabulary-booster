1af840bf848ca19ec7d01e54d92b928d5fb6c521
Marco Galindo <marco1408@gmail.com>
2021-08-28 12:37:36 -0300

    corrigindo requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,8 @@
 select t1.contactname as Nome,
 t1.country as País,
-count(t1.country = t2.country) as `Número de compatriotas`
-from w3schools.customers as t1, w3schools.customers as t2
-where t1.country = t2.country and t1.contactname <> t2.contactname
-group by t1.contactname
+(select count(t2.country) from w3schools.customers as t2
+where t1.country = t2.country and t1.contactname <> t2.contactname) as `Número de compatriotas`
+from w3schools.customers as t1
+where (select count(t2.country) from w3schools.customers as t2
+where t1.country = t2.country and t1.contactname <> t2.contactname) > 0
 order by t1.contactname;
