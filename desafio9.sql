2d3b9d2d9adde45c1a20520cb19e33b0d18829f8
Gustavo Mendes <gusm217@gmail.com>
2021-08-28 17:01:49 -0300

    Adiciona o 9

---
+++ desafio9.sql
@@ -0,0 +1,6 @@
+SELECT concat(e.FirstName, ' ' , e.LastName) AS 'Nome completo', count(o.EmployeeID) AS 'Total de pedidos'
+FROM w3schools.orders AS o
+INNER JOIN w3schools.employees AS e
+ON o.EmployeeID = e.EmployeeID
+GROUP BY o.EmployeeID 
+ORDER BY `Total de pedidos`;
