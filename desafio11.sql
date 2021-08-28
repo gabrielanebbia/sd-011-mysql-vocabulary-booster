232c987796c7d52f277b413f6635f15e39169d95
anaclarabck <ackyotoku@gmail.com>
2021-08-28 04:27:53 -0300

    [UPDATE] refactored answer 11

--- desafio11.sql
+++ desafio11.sql
@@ -4,8 +4,5 @@ FROM w3schools.customers AS t1
 GROUP BY Country
 HAVING t1.Country = t2.Country) AS 'Número de compatriotas'
 FROM w3schools.customers AS t2
-WHERE (SELECT COUNT(*)-1
-FROM w3schools.customers AS t1
-GROUP BY Country
-HAVING t1.Country = t2.Country) > 0
+HAVING `Número de compatriotas` > 0
 ORDER BY ContactName ASC;
