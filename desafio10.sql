4d1d062ee5172b34b9e3a8668fabc749622fe7d1
menezesrenan7 <renan_menezes07@hotmail.com>
2021-08-28 17:02:05 -0300

    correções

--- desafio10.sql
+++ desafio10.sql
@@ -1,1 +1,1 @@
-SELECT t1.CustomerName AS Nome, t1.Country AS País, COUNT(t1.Country) - 1 AS `Número de compatriotas` FROM w3schools.customers AS t1 GROUP BY t1.CustomerName ORDER BY Nome ASC;
+SELECT P.ProductName AS Produto, MIN(OD.Quantity) AS Mínima, MAX(OD.Quantity) AS Máxima, ROUND(AVG(OD.Quantity), 2) AS Média FROM w3schools.order_details AS OD INNER JOIN w3schools.products AS P ON P.ProductID = OD.ProductID GROUP BY Produto HAVING Média > 20.00 ORDER BY Média ASC, Produto;
--- desafio13.sql
+++ desafio13.sql
@@ -1,1 +1,1 @@
-SELECT P.ProductName AS Produto, P.Price AS Preço, FROM w3schools.products P INNER JOIN w3schools.order_details O ON O.Quantity > 80 ORDER BY Produto;
+SELECT P.ProductName AS Produto, P.Price AS Preço FROM w3schools.products P INNER JOIN w3schools.order_details O ON O.Quantity > 80 ORDER BY Produto;
