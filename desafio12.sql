16d03c5d025369a89e35f99296348dc6316ed92a
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-08-28 15:21:12 -0300

    Desafio 12

--- desafio12.sql
+++ desafio12.sql
@@ -6,5 +6,5 @@ CONCAT(t2.FIRST_NAME, " ", t2.LAST_NAME) AS "Nome completo funcionário 2",
 t2.SALARY AS "Salário funcionário 2",
 t2.PHONE_NUMBER AS "Telefone funcionário 2"
 FROM hr.employees AS t1, hr.employees AS t2
-WHERE t1.JOB_ID = t2.JOB_ID AND t1.FIRST_NAME <> t2.FIRST_NAME
+WHERE t1.JOB_ID = t2.JOB_ID AND t1.EMPLOYEE_ID <> t2.EMPLOYEE_ID
 ORDER BY CONCAT(t1.FIRST_NAME, " ", t1.LAST_NAME), CONCAT(t2.FIRST_NAME, " ", t2.LAST_NAME);
