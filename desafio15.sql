082992a7a40e21e22b915ddf166b983e7c87e70e
Maran <maranmorimoto@gmail.com>
2021-08-28 18:56:36 -0300

    requirement 15 done

---
+++ desafio15.sql
@@ -0,0 +1,12 @@
+USE hr;
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo (IN role VARCHAR(20))
+BEGIN
+SELECT 
+ROUND(AVG(e.Salary), 2) AS 'Média Salarial'
+FROM hr.employees e
+INNER JOIN hr.jobs j
+ON j.job_id = e.job_id
+WHERE j.job_title = role;
+END $$
+DELIMITER ;
