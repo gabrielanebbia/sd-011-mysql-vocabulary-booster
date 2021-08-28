f468101ceaaae25ca9bd51d8fb2d3678c22b0e69
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-08-28 20:03:00 -0300

    erro no 17

--- desafio17.sql
+++ desafio17.sql
@@ -1,10 +1,8 @@
-USE w3schools;
-
 DELIMITER $$
 CREATE TRIGGER trigger_order
 BEFORE INSERT ON w3schools.orders
     FOR EACH ROW
 BEGIN
-	SET NEW.OrderDate = NOW();
+SET NEW.OrderDate = NOW();
 END $$
 DELIMITER ;
