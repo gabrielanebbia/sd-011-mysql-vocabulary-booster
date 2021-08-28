a611c2fe4c2a1a81fa35de8c6f151a8b19efc86f
Daniel <danielfdoficial@gmail.com>
2021-08-28 16:03:26 -0300

    fix desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -2,4 +2,5 @@ SELECT ContactName AS `Nome`,
 Country AS `País`,
 (SELECT (COUNT(Country) - 1) FROM w3schools.customers WHERE Country = `País`) AS `Número de compatriotas`
 FROM w3schools.customers
+HAVING `Número de compatriotas` <> 0
 ORDER BY `Nome`;
