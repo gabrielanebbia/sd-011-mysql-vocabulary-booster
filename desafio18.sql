d73ce1065428a33944f7a2e6a7e50887c4b588e2
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-08-27 23:20:27 -0300

    desafio 18

---
+++ desafio18.sql
@@ -0,0 +1,23 @@
+SELECT
+  CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
+  CONCAT(
+    DAY(jh.START_DATE),
+    '/',
+    MONTH(jh.START_DATE),
+    '/',
+    Year(jh.START_DATE)
+  ) AS `Data de início`,
+  CONCAT(
+    DAY(jh.END_DATE),
+    '/',
+    MONTH(jh.END_DATE),
+    '/',
+    Year(jh.END_DATE)
+  ) AS `Data de rescisão`,
+  ROUND((DATEDIFF(jh.END_DATE, jh.START_DATE) / 365), 2) AS `Anos trabalhados`
+FROM
+  hr.employees AS e
+  INNER JOIN hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+ORDER BY
+  `Nome completo`,
+  `Anos trabalhados`;
