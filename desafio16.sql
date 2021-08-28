2930b84bd2492e9fac7282dc024b813b0b91abbc
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-08-27 23:20:39 -0300

    realiza desafio 16

---
+++ desafio16.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+
+CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(IN email VARCHAR(50))
+BEGIN
+  SELECT COUNT(JH.EMPLOYEE_ID)
+  FROM job_history AS JH
+  INNER JOIN employees AS E
+    ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+  GROUP BY JH.DEPARTMENT_ID
+  HAVING JH.DEPARTMENT_ID = 90;
+END $$
+
+DELIMITER ;
