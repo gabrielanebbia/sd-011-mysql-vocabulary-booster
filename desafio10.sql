c4ecea6acbf54aaa75044bb18baa6035826d47f0
Matheus Gaspar <msouzagaspar@gmail.com>
2021-08-28 11:22:06 +0200

    correção desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -7,4 +7,4 @@ FROM w3schools.order_details AS t1
 INNER JOIN w3schools.products AS t2 ON t1.ProductID = t2.ProductID
 GROUP BY t1.ProductID
 HAVING AVG(Quantity) > 20
-ORDER BY AVG(Quantity);
+ORDER BY AVG(Quantity), ProductName;
