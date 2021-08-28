cae7dba203471572aa32b2cfd156413ce27147c6
Daniel <danielfdoficial@gmail.com>
2021-08-28 16:56:29 -0300

    add desafio 13

---
+++ desafio13.sql
@@ -0,0 +1,7 @@
+SELECT
+p.ProductName AS `Produto`,
+p.Price AS `Preço`
+FROM w3schools.products AS p
+INNER JOIN w3schools.order_details as o_d
+ON o_d.ProductID = p.ProductID
+AND o_d.Quantity > 80;
