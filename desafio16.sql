98eaca8108838fa72b977d7c43c67c0dd08af956
Matheus Alexandre <alexandremhm@gmail.com>
2021-08-28 12:17:48 -0300

    Add requisito 16

---
+++ desafio16.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(emailEmployeer INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE quantityJobs INT;
+SELECT COUNT(a.JOB_ID) FROM job_history AS a
+INNER JOIN employees AS b
+ON a.EMPLOYEE_ID = b.EMPLOYEE_ID
+WHERE b.EMAIL = "NKOCHHAR"
+INTO quantityJobs;
+RETURN quantityJobs;
+END $$
+
+DELIMITER ;
