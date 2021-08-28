7ea05343616930aca64c51c1ece14ec808644687
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-08-28 18:10:42 -0300

    Requisito 13 realizado

--- desafio11.sql
+++ desafio11.sql
@@ -18,6 +18,7 @@ SELECT t1.ContactName AS Nome,
 t1.Country AS País, 
 COUNT(t2.Country) - 1  AS "Número de compatriotas" 
 FROM w3schools.customers as t1, w3schools.customers as t2
-WHERE t1.Country = t2.Country 
+WHERE t1.Country = t2.Country
 GROUP BY t1.CustomerID
+HAVING COUNT(t2.Country) > 1
 ORDER BY t1.ContactName;
