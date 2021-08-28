ef556a3fffbc9ca53306152a7c2deb16c3aac57a
jamneto <netomoura22@gmail.com>
2021-08-28 16:53:42 -0300

    cump req 7

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT UPPER(CONCAT(emp.FIRST_NAME,' ', emp.LAST_NAME)) AS `Nome completo`,
+jhis.START_DATE AS `Data de início`,
+emp.SALARY AS `Salário`
+FROM hr.employees AS emp
+INNER JOIN hr.job_history AS jhis
+ON emp.EMPLOYEE_ID = jhis.EMPLOYEE_ID
+WHERE MONTH(jhis.START_DATE) BETWEEN 1 AND 3
+ORDER BY `Nome completo`, `Data de início`;
