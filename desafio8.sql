90b122b4e773bf4c63bfdddfaf504d21d8efa60b
gabiazevedo <gabiazevedoms@gmail.com>
2021-08-27 22:21:10 -0300

    Finaliza requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,18 @@
+SELECT ContactName AS 'Nome de contato',
+ShipperName AS 'Empresa que fez o envio',
+OrderDate AS 'Data do pedido'
+FROM
+w3schools.customers AS c
+INNER JOIN
+w3schools.orders AS o
+ON
+c.CustomerID = o.CustomerID
+INNER JOIN
+w3schools.shippers AS s
+ON
+s.ShipperID = o.ShipperID
+WHERE
+ShipperName = 'Speedy Express'
+OR
+ShipperName = 'United Package'
+ORDER BY ContactName, ShipperName, OrderDate;
