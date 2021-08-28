723f513e827bfd0f52a71a8b1f6de268f1e2f0ef
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-08-28 11:49:03 -0300

    Corrige Desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,1 +1,12 @@
-SELECT t1.ContactName AS 'Nome', t1.Country AS 'País', (SELECT COUNT(Country)-1 FROM w3schools.customers WHERE Country = t1.Country ) AS 'Número de compatriotas' FROM w3schools.customers AS t1 INNER JOIN w3schools.customers AS t2 ON t1.Country = t2.Country GROUP BY t1.ContactName HAVING COUNT(t1.Country) > 1 ORDER BY t1.ContactName;
+SELECT
+t1.ContactName AS 'Nome',
+t1.Country AS 'País',
+(
+SELECT
+COUNT(*)-1 FROM w3schools.customers
+GROUP BY Country
+HAVING Country = t1.Country
+) AS `Número de compatriotas`
+FROM w3schools.customers AS t1
+HAVING `Número de compatriotas` <> 0 
+ORDER BY ContactName;
