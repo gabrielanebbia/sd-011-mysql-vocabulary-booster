64dd5074f3764e3dccdea4e26f2c120ca6aa32b7
Matheus Gaspar <msouzagaspar@gmail.com>
2021-08-28 21:21:30 +0200

    desafio 13

---
+++ desafio13.sql
@@ -0,0 +1,7 @@
+SELECT
+ProductName AS 'Produto',
+Price AS 'Preço'
+FROM w3schools.products AS p
+INNER JOIN w3schools.order_details AS o ON p.ProductID = o.ProductID
+WHERE Quantity > 80
+ORDER BY ProductName;
