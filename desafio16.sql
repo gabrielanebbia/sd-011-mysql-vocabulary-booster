eb0742811223b63160bcddb4684b0495f9e71e62
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-08-27 23:58:47 -0300

    corrige correcao

--- desafio16.sql
+++ desafio16.sql
@@ -1,13 +1,15 @@
 DELIMITER $$
 
-CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(IN email VARCHAR(50))
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
+RETURNS VARCHAR(50) READS SQL DATA
 BEGIN
-  SELECT COUNT(JH.EMPLOYEE_ID)
-  FROM job_history AS JH
-  INNER JOIN employees AS E
-    ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
-  GROUP BY JH.DEPARTMENT_ID
-  HAVING JH.DEPARTMENT_ID = 90;
+  DECLARE qty_jobs INT;
+  SELECT COUNT(*)
+  FROM job_history AS job_history
+  INNER JOIN employees AS employees 
+    ON job_history.EMPLOYEE_ID = employees.EMPLOYEE_ID
+    AND employees.EMAIL = email INTO qty_jobs;
+  RETURN qty_jobs;
 END $$
 
 DELIMITER ;
