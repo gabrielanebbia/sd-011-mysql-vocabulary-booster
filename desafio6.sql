ac2a260e30f19067f16390e4ece89e306f6d954a
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-08-28 17:36:04 -0300

    requisito 6 concluido

---
+++ desafio6.sql
@@ -0,0 +1,10 @@
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
+j.job_title AS Cargo,
+e.hire_date AS 'Data de início do cargo',
+d.department_name AS Departamento
+FROM hr.employees AS e
+INNER JOIN hr.jobs AS j
+ON j.job_id = e.job_id
+INNER JOIN hr.departments AS d
+ON e.department_id = d.department_id
+ORDER BY `Nome completo` DESC, job_title;
