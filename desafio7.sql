f47c282ea7462699d67b1939af1535875f6be497
OldRoge <rogeriomrjr@icloud.com>
2021-08-28 15:45:06 -0300

    Fixing error - Month should be between january and March

--- desafio7.sql
+++ desafio7.sql
@@ -1,3 +1,5 @@
+select * from hr.employees;
+select * from job_history;
 SELECT
 UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)) AS "Nome completo",
 job_history.START_DATE AS "Data de início",
@@ -5,4 +7,5 @@ employees.SALARY AS "Salário"
 FROM hr.employees AS employees
 INNER JOIN hr.job_history AS job_history
 ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
+WHERE MONTH(job_history.START_DATE) <= 03
 ORDER BY `Nome completo` ASC, `Data de início` ASC;
