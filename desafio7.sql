34cceb39633ed25c884c7dc762a3b9d0e12acee0
Renan <renan.vamo@gmail.com>
2021-08-28 12:13:56 -0300

    add o req 7 e 8

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) 'Nome Completo', 
+jh.START_DATE 'Data de início',
+e.SALARY 'Salário'
+FROM hr.job_history jh
+INNER JOIN hr.employees e
+ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE MONTH(jh.START_DATE) IN (01, 02, 03)
+ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME), jh.START_DATE; 
