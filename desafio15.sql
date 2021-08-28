69725f04abd8b5576f1d0661b915be1eb8c994d6
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-08-28 12:06:25 -0300

    Corrige Desafio 15

--- desafio15.sql
+++ desafio15.sql
@@ -1,3 +1,14 @@
 DELIMITER $$
-CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100)) BEGIN SELECT (SELECT ROUND(AVG(SALARY), 2) FROM hr.employees WHERE hr.employees.JOB_ID = t2.JOB_ID) AS 'Média salarial' FROM hr.employees AS t1 INNER JOIN hr.jobs AS t2 ON t1.JOB_ID = t2.JOB_ID WHERE t2.JOB_TITLE = cargo GROUP BY t2.JOB_TITLE; END
-$$ DELIMITER ;
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
+BEGIN
+SELECT
+(
+SELECT ROUND(AVG(SALARY), 2) FROM hr.employees WHERE hr.employees.JOB_ID = t2.JOB_ID
+) AS 'Média salarial'
+FROM hr.employees AS t1
+INNER JOIN hr.jobs AS t2
+ON t1.JOB_ID = t2.JOB_ID
+WHERE t2.JOB_TITLE = cargo
+GROUP BY `Média salarial`;
+ END $$
+DELIMITER ;
