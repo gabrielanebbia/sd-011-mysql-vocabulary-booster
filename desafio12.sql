feb725af3f183bae1dba3a561bd92944e56aa49d
murilodospacos <murilopacos@gmail.com>
2021-08-28 13:27:55 -0300

    requisito 12 lint

--- desafio12.sql
+++ desafio12.sql
@@ -5,7 +5,7 @@ E.PHONE_NUMBER AS 'Telefone funcionário 1',
 CONCAT(D.FIRST_NAME,' ', D.LAST_NAME) AS 'Nome completo funcionário 2',
 D.SALARY AS 'Salário funcionário 2',
 D.PHONE_NUMBER AS 'Telefone funcionário 2'
-FROM hr.employees AS E
-LEFT JOIN hr.employees AS D ON D.JOB_ID = E.JOB_ID
+FROM hr.employees AS E, hr.employees AS D
+WHERE D.JOB_ID = E.JOB_ID
 AND D.EMPLOYEE_ID <> E.EMPLOYEE_ID 
 ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
