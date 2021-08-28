a1657b452d75f49a1227873767826b74fc0fbb01
Renan <renan.vamo@gmail.com>
2021-08-28 10:15:12 -0300

    refatora os req 1 e 4

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,6 @@
-SELECT c.COUNTRY_NAME 'País', r.REGION_NAME 'Status Inclusão'
-FROM hr.countries c
-INNER JOIN regions r
-ON r.REGION_ID = c.REGION_ID
-WHERE r.REGION_NAME = 'Europe';
+SELECT COUNTRY_NAME 'País', (
+SELECT IF (REGION_NAME = 'Europe', 'incluído', 'não incluído')
+FROM hr.regions
+WHERE hr.countries.REGION_ID = hr.regions.REGION_ID
+) AS 'Status Inclusão'
+FROM hr.countries;
