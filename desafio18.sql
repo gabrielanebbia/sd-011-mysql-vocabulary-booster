451d6b00b14bca84488dad2fd7d384b25ca9cb87
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-08-28 00:53:36 -0300

    desafio 18 correcao

--- desafio18.sql
+++ desafio18.sql
@@ -1,6 +1,6 @@
 SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
-convert(varchar, jh.START_DATE, 103) AS `Data de início`,
-convert(varchar, jh.END_DATE, 103) AS `Data de rescisão`,
+DATE_FORMAT(jh.START_DATE, "%d %m %Y") AS `Data de início`,
+DATE_FORMAT(jh.END_DATE, "%d %m %Y") AS `Data de rescisão`,
 ROUND((DATEDIFF(jh.END_DATE, jh.START_DATE)/365), 2) AS `Anos trabalhados`
 from hr.employees AS e
 INNER JOIN hr.job_history AS jh
