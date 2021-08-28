d6d9c9d4346b9331d823562695c0cb622d2be483
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-08-28 19:07:32 -0300

    fix: req 17

--- desafio17.sql
+++ desafio17.sql
@@ -3,6 +3,6 @@ CREATE TRIGGER trigger_order_insert
 BEFORE INSERT ON w3chools.orders
 FOR EACH ROW
 BEGIN
-SET NEW.OrderDtate = CURRENT_DATE();
+SET NEW.OrderDate = CURRENT_DATE();
 END $$
 DELIMITER ;
