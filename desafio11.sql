d98f60fbe4922ef70042b3ac0122ac92f099b8dc
murilodospacos <murilopacos@gmail.com>
2021-08-28 12:39:21 -0300

    requisito 11 ajustes

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,9 @@
 SELECT 
+c.ContactName AS 'Nome',
 c.Country AS 'País',
 COUNT(d.CustomerID) AS 'Número de compatriotas'
 FROM w3schools.customers AS c
 INNER JOIN w3schools.customers AS d ON d.Country = c.Country
 AND d.CustomerID <> c.CustomerID
 GROUP BY c.CustomerID
-ORDER BY c.CustomerID;
+ORDER BY `Nome`;
