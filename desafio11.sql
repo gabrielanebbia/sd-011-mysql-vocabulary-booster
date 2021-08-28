d820f0b2111180ff5c5dcb3ef07b42a17ae295c8
Victor Faria <vitilevi@hotmail.com>
2021-08-28 20:11:26 -0300

    Corrige req 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,22 +1,8 @@
-DELIMITER $$
-CREATE FUNCTION numeroDeCompatriotas(pais VARCHAR(50))
-RETURNS INT READS SQL DATA
-BEGIN
-DECLARE total INT;
-SELECT case
-WHEN COUNT(*) > 1 THEN (COUNT(*) - 1)
-WHEN COUNT(*) = 0 THEN 0
-END AS 'Número de compatriotas'
-FROM w3schools.customers AS c
-WHERE c.Country = pais
-INTO total;
-RETURN total;
-END$$
-DELIMITER ;
-
 SELECT c.ContactName AS 'Nome',
 c.Country AS 'País',
-numeroDeCompatriotas(c.Country) AS 'Número de compatriotas'
+(SELECT COUNT(Country) - 1
+FROM w3schools.customers
+WHERE w3schools.customers.Country = c.Country) AS 'Número de compatriotas'
 FROM w3schools.customers AS c
-WHERE numeroDeCompatriotas(c.Country) IS NOT NULL
+HAVING `Número de compatriotas` > 0
 ORDER BY c.ContactName;
