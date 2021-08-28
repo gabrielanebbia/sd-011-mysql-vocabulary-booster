bc915a62ba50a7dbd465462167f8b0371cdeb09a
Vinicius <viniciusfod@gmail.com>
2021-08-28 13:25:27 -0300

    req-10

---
+++ desafio10.sql
@@ -0,0 +1,9 @@
+SELECT p.ProductName AS Produto,
+       MIN(order_details.Quantity) AS 'Mínima',
+       MAX(order_details.Quantity) AS 'Máxima',
+       ROUND(AVG(order_details.Quantity), 2) AS 'Média'
+FROM w3schools.products p
+INNER JOIN w3schools.order_details AS order_details ON p.ProductID = order_details.ProductID
+GROUP BY p.ProductID
+HAVING AVG(order_details.Quantity) > 20.00
+ORDER BY Média, Produto ASC;
