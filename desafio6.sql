fe601ef91393791d82da8ccd09180764c6c7e7b7
OldRoge <rogeriomrjr@icloud.com>
2021-08-28 13:13:01 -0300

    Used the wrong table to get the date about when employees started

--- desafio6.sql
+++ desafio6.sql
@@ -1,12 +1,14 @@
 SELECT CONCAT(employees.FIRST_NAME, " ", employees.LAST_NAME) AS "Nome completo",
         jobs.JOB_TITLE AS Cargo,
-        employees.HIRE_DATE AS "Data de início do cargo",
+        job_history.START_DATE AS "Data de início do cargo",
         departments.DEPARTMENT_NAME AS "Departamento"
 FROM hr.employees AS employees
-INNER JOIN hr.jobs AS jobs
-ON employees.JOB_ID = jobs.JOB_ID
+INNER JOIN hr.job_history AS job_history
+ON employees.JOB_ID = job_history.JOB_ID
 INNER JOIN hr.departments AS departments
 ON employees.DEPARTMENT_ID = departments.DEPARTMENT_ID
+INNER JOIN hr.jobs AS jobs
+ON jobs.JOB_ID = employees.JOB_ID
 ORDER BY
 CONCAT(employees.FIRST_NAME, " ", employees.LAST_NAME) DESC,
 jobs.JOB_TITLE ASC;
