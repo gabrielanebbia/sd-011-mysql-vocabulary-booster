fabea5a5b02102b3c0a5ab1538ce8183c312d262
Pedro Piquini <piquini.pedro@gmail.com>
2021-08-27 21:02:15 -0300

    desafio 10 completo

---
+++ desafio10.sql
@@ -0,0 +1,9 @@
+SELECT p.ProductName AS 'Produto',
+MIN(od.Quantity) AS 'Mínima',
+MAX(od.Quantity) AS 'Máxima',
+ROUND(AVG(od.Quantity), 2) AS 'Média'
+FROM order_details AS od
+INNER JOIN products AS p
+ON p.ProductID = od.ProductID
+GROUP BY `Produto` HAVING AVG(od.Quantity) > 20
+ORDER BY `Média`, `Produto`;
