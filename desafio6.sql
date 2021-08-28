f85293db91989d4d22dd0bfff7761b019618e78d
MatheusAraujoDev <matheusaraujo.dev@gmail.com>
2021-08-28 13:04:43 -0300

    Ajusta letra maiuscula req 06

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,8 @@
 SELECT 
-CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome Completo',
+CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
 j.JOB_TITLE AS Cargo,
 jh.START_DATE AS 'Data de início do cargo',
 d.DEPARTMENT_NAME AS Departamento
-
 FROM
 hr.employees AS e
 INNER JOIN hr.job_history  AS jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
