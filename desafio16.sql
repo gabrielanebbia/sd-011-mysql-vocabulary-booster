7f53159782ad675aaeb0dadaae11b3e0a299088a
Diego Ribeiro <dihrm2001@gmail.com>
2021-08-28 12:02:58 -0300

    Desafio 16

---
+++ desafio16.sql
@@ -0,0 +1,17 @@
+USE hr;
+
+DELIMITER $$
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(search_email VARCHAR(30))
+RETURNS INT READS SQL DATA
+BEGIN
+  DECLARE quantidade_de_empregos INT;
+  (SELECT COUNT(*)
+  FROM hr.job_history
+  WHERE EMPLOYEE_ID IN (
+    SELECT EMPLOYEE_ID
+    FROM hr.employees
+    WHERE EMAIL = search_email
+  )) INTO quantidade_de_empregos;
+  RETURN quantidade_de_empregos;
+END $$
+DELIMITER ;
