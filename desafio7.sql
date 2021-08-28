dfa01ae9a429dc623333ae5f2bde39fde4368f73
Marconi <marconieng@gmail.com>
2021-08-28 19:52:18 -0300

    Adiciona desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+SELECT
+UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+jh.START_DATE AS 'Data de início do cargo',
+e.SALARY AS 'Sálario'
+FROM hr.employees AS e
+
+INNER JOIN hr.job_history AS jh
+ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE MONTH(jh.START_DATE) BETWEEN '1' AND '3'
+
+ORDER BY `Nome completo` ASC, `Data de início do cargo` ASC;
