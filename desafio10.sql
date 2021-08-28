7ee259a8fee80921f94e8afa2a3e12dd295df523
Bruno Duarte <brunoarduarte@gmail.com>
2021-08-28 08:53:27 -0300

    Desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,8 @@
+SELECT P.ProductName AS Produto,
+MIN(OD.Quantity) AS Mínima,
+MAX(OD.Quantity) AS Máxima,
+ROUND(AVG(OD.Quantity), 2) AS Média
+FROM products AS P
+INNER JOIN order_details AS OD ON P.ProductID = OD.ProductID
+GROUP BY P.ProductName HAVING Média > 20.00
+ORDER BY Média, Produto;
