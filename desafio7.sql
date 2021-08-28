6c2a5bc30291ee14ecf464436f26b6d67a45844c
OldRoge <rogeriomrjr@icloud.com>
2021-08-28 15:29:05 -0300

    Try to fix requirement 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,3 @@
-select * from hr.employees;
-select * from job_history;
 SELECT
 UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)) AS "Nome completo",
 job_history.START_DATE AS "Data de início",
@@ -7,4 +5,4 @@ employees.SALARY AS "Salário"
 FROM hr.employees AS employees
 INNER JOIN hr.job_history AS job_history
 ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
-ORDER BY `Nome completo`, `Data de início`;
+ORDER BY `Nome completo` ASC, `Data de início` ASC;
