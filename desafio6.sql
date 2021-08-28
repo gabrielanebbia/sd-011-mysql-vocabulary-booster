4c1e9a18d09310ea15274eba4574866fb77597c6
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-08-28 17:49:14 -0300

    resolvendo problema no requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -5,6 +5,8 @@ d.department_name AS Departamento
 FROM hr.employees AS e
 INNER JOIN hr.jobs AS j
 ON j.job_id = e.job_id
+INNER JOIN hr.job_history AS jh
+ON jh.employee_id = e.employee_id
 INNER JOIN hr.departments AS d
 ON e.department_id = d.department_id
 ORDER BY `Nome completo` DESC, job_title;
