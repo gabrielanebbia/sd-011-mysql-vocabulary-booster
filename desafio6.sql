ac191829c4d8657d7ebfb97f8f236ce2ab27f98b
guinhoanjos <iago.anjos@hotmail.com>
2021-08-28 11:50:45 -0300

    Alias no requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT distinct CONCAT(emp.first_name, ' ', emp.last_name) AS `Nome completo`,
 jobs.JOB_TITLE AS Cargo,
 job.START_DATE AS `Data de início do cargo`,
-dep.DEPARTMENT_NAME
+dep.DEPARTMENT_NAME AS Departamento
 FROM hr.employees AS emp
 RIGHT JOIN hr.job_history AS job 
 ON job.EMPLOYEE_ID = emp.EMPLOYEE_ID
