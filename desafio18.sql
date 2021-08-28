46d457ac4ba95239935276e06b0e8684446c5006
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-08-28 00:26:50 -0300

    realiza desafio 18

---
+++ desafio18.sql
@@ -0,0 +1,8 @@
+SELECT
+  CONCAT(employess.FIRST_NAME, ' ', employess.LAST_NAME) AS `Nome completo`,
+  CONCAT(DAY(job_history.START_DATE), '/', MONTH(job_history.START_DATE), '/', YEAR(job_history.START_DATE)) AS `Data de início`,
+  CONCAT(DAY(job_history.END_DATE), '/', MONTH(job_history.END_DATE), '/', YEAR(job_history.END_DATE)) AS `Data de rescisão`,
+  ROUND(DATEDIFF(job_history.START_DATE, job_history.END_DATE) / 365, 2) AS `Anos trabalhados`
+FROM job_history AS job_history
+  INNER JOIN employess AS employess ON job_history.EMPLOYEE_ID = employess.EMPLOYEE_ID
+ORDER BY `Nome completo` , `Anos trabalhados`;
