ad790f9885e04b29844ea0f2f6f54b37b811f06b
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-08-28 17:10:33 -0300

    REQ 12

--- a.todo/desafio12.sql
+++ a.todo/desafio12.sql
@@ -1,14 +1,10 @@
 SELECT
-CONCAT(FIRST_NAME, ' ' ,LAST_NAME) AS `Nome completo funcionário 1`,
-SALARY AS `Salário funcionário 1`,
-PHONE_NUMBER AS `Telefone funcionário 1`,
-(
-SELECT
-CONCAT(FIRST_NAME, ' ' ,LAST_NAME)
-FROM hr.employees AS E
-WHERE E.JOB_ID = t1.JOB_ID
-) AS `Nome completo funcionário 2`
-FROM hr.employees t1;
-
-SALARY AS `Salário funcionário 2`,
-PHONE_NUMBER AS `Telefone funcionário 2`
+CONCAT(t1.FIRST_NAME, ' ' ,t1.LAST_NAME) AS `Nome completo funcionário 1`,
+t1.SALARY AS `Salário funcionário 1`,
+t1.PHONE_NUMBER AS `Telefone funcionário 1`,
+CONCAT(t2.FIRST_NAME, ' ' ,t2.LAST_NAME) AS `Nome completo funcionário 2`,
+t2.SALARY AS `Salário funcionário 2`,
+t2.PHONE_NUMBER AS `Telefone funcionário 2`
+FROM hr.employees t1, hr.employees t2
+WHERE t2.JOB_ID = t1.JOB_ID AND t1.FIRST_NAME <> t2.FIRST_NAME
+ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
