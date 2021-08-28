a7e3b51afe2dd63f8e1b20bb2a20735ea6031dea
jhenriquemargrafm <jh.margraf@gmail.com>
2021-08-28 19:10:17 -0300

    Req 15 - query ao inves de procedure

--- desafio15.sql
+++ desafio15.sql
@@ -1,10 +1,14 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `buscar_media_por_cargo`(IN position_name VARCHAR(50))
+DELIMITER $$
+
+CREATE PROCEDURE 'buscar_media_por_cargo'(IN position_name VARCHAR(100))
 BEGIN
-	SELECT ROUND(AVG(salary), 2) AS 'Média salarial'
-	FROM hr.employees
-	WHERE job_id IN (
-		SELECT job_id
-		FROM hr.jobs
-        WHERE job_title = position_name
+SELECT ROUND(AVG(salary), 2) AS 'Média salarial'
+FROM hr.employees
+WHERE job_id IN (
+SELECT job_id
+FROM hr.jobs
+WHERE job_title = position_name
 	);
-END
+END $$
+
+DELIMITER;
