9b104ea8bf43038cad603962e07f6f9d8f37eba9
MatheusAraujoDev <matheusaraujo.dev@gmail.com>
2021-08-28 12:28:38 -0300

    Adiciona req 05

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+SELECT
+j.JOB_TITLE AS 'Cargo',
+ROUND(AVG(j.MAX_SALARY - j.MIN_SALARY)) AS 'Variação Salarial',
+ROUND(AVG(j.MIN_SALARY)/12, 2) AS 'Média mínima mensal',
+ROUND(AVG(j.MAX_SALARY)/12, 2) AS 'Média máxima mensal'
+FROM hr.jobs AS j
+GROUP BY JOB_TITLE
+ORDER BY ROUND(AVG(MAX_SALARY - MIN_SALARY)), JOB_TITLE;
