367717365b3dea5ca99eb6747afe4c66d70cde53
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-08-28 17:48:28 -0300

    fix: req6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT(employee.FIRST_NAME, ' ', employee.LAST_NAME) AS 'Nome completo',
 j.JOB_TITLE AS 'Cargo',
 job.START_DATE AS 'Data de início do cargo',
-dep.DEPARTMENT_NAME AS 'Department'
+dep.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.employees AS employee
 INNER JOIN hr.job_history AS job ON employee.EMPLOYEE_ID = job.EMPLOYEE_ID
 INNER JOIN hr.departments AS dep ON dep.DEPARTMENT_ID = job.DEPARTMENT_ID
