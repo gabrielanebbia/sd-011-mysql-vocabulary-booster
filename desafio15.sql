e9d3040f2ed8870daf930c9bf8502c04bb5a5776
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-08-28 16:15:53 -0300

    requisito 15

--- desafio15.sql
+++ desafio15.sql
@@ -3,9 +3,9 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
 BEGIN
-	SELECT AVG(E.SALARY) AS 'Média salarial'
-    FROM hr.employees AS E
-    INNER JOIN
+SELECT ROUND(AVG(E.SALARY), 2) AS 'Média salarial'
+FROM hr.employees AS E
+INNER JOIN
 hr.jobs  AS J ON E.JOB_ID = J.JOB_ID
 WHERE J.JOB_TITLE = cargo;
 END $$
