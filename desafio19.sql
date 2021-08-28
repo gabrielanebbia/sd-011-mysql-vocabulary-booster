f7a92ce3f12875e67b79d46b0129d64c4854fe0c
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-08-27 23:31:34 -0300

    desafio 19

--- desafio19.sql
+++ desafio19.sql
@@ -1,10 +1,12 @@
-DELIMITER $$
+DELIMITER $ $ CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(hire_month int, hire_year int) RETURNS INT READS SQL DATA BEGIN DECLARE employees_hired int
+SELECT
+  COUNT(*)
+FROM
+  hr.employees
+WHERE
+  year(HIRE_DATE) = hire_year
+  AND MONTH(HIRE_DATE) = hire_month INTO employees_hired;
 
-CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(parametro1, parametro2, ..., parametroN)
-RETURNS tipo_de_dado tipo_de_retorno
-BEGIN
-    query_sql
-    RETURN resultado_a_ser_retornado;
-END $$
+RETURN employees_hired;
 
-DELIMITER ;
+END $ $ DELIMITER;
