17a1f6a63cb3647e567a9cb091630788df6e6018
Renan <renan.vamo@gmail.com>
2021-08-28 12:30:55 -0300

    refatora o req 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,4 +1,4 @@
-SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) 'Nome Completo', 
+SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) 'Nome completo', 
 jh.START_DATE 'Data de início',
 e.SALARY 'Salário'
 FROM hr.job_history jh
