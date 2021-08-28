37a76cee6b12d9ebd45ed4bc4599dfddf4b6d707
Felipe Buso <felipe_buso@hotmail.com>
2021-08-28 10:04:36 -0300

    correção desafio 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,12 +1,11 @@
-SELECT DISTINCT CONCAT(E.FirstName, ' ', E.Lastname) AS 'Nome completo',
+SELECT CONCAT(E.FirstName, ' ', E.Lastname) AS 'Nome completo',
 (
 SELECT COUNT(EmployeeID)
 FROM w3schools.orders
 WHERE EmployeeID = E.EmployeeID
 ) AS 'Total de pedidos'
 FROM w3schools.employees E
-INNER JOIN w3schools.orders
-ON (
+WHERE (
 SELECT COUNT(EmployeeID)
 FROM w3schools.orders
 WHERE EmployeeID = E.EmployeeID
