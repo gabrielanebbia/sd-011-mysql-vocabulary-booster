cb72de39bf4b2ac0c81274bdb419e283c96e3de6
IsleneGomes <islenegomes@gmail.com>
2021-08-28 12:16:04 -0300

    desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,8 @@
+SELECT c.ContactName AS "Nome",
+c.Country AS "País",
+COUNT(d.CustomerID) AS "Número de compatriotas"
+FROM w3schools.customers AS c
+INNER JOIN w3schools.customers AS d ON d.Country = c.Country
+AND d.CustomerID <> c.CustomerID
+GROUP BY c.CustomerID
+ORDER BY `Nome`;
