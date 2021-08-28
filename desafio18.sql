f2cd4cfc67f2cd2e828629dbd02e583f9b014d40
PH Assis <pieit@live.com>
2021-08-27 22:11:58 -0300

    .

--- desafio18.sql
+++ desafio18.sql
@@ -5,7 +5,6 @@ SELECT CONCAT(EMP.FIRST_NAME, ' ', EMP.LAST_NAME) AS `Nome completo`,
 DATE_FORMAT(JH.START_DATE, '%d/%m/%Y') AS `Data de início`,
 DATE_FORMAT(JH.END_DATE, '%d/%m/%Y') AS `Data de rescisão`,
 ROUND(DATEDIFF(JH.END_DATE, JH.START_DATE) / 365,2) AS `Anos trabalhados`
-       
 FROM hr.employees AS EMP
 INNER JOIN hr.job_history AS JH
 ON EMP.EMPLOYEE_ID = JH.EMPLOYEE_ID
