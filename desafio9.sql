53e1a2c0833e7d42451bb3ec852b525b22260b0a
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-08-28 10:10:26 -0300

    Corrige sql desafio 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,1 +1,8 @@
-SELECT CONCAT(t1.FirstName, ' ', t1.LastName) AS 'Nome de contato', COUNT(t2.EmployeeID) AS `Total de pedidos` FROM w3schools.employees AS t1 INNER JOIN w3schools.orders AS t2 ON t1.EmployeeID = t2.EmployeeID GROUP BY t1.FirstName ORDER BY `Total de pedidos`;
+SELECT
+CONCAT(t1.FirstName, ' ', t1.LastName) AS `Nome completo`,
+COUNT(t2.EmployeeID) AS `Total de pedidos`
+FROM w3schools.employees AS t1
+INNER JOIN w3schools.orders AS t2
+ON t1.EmployeeID = t2.EmployeeID
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos`;
