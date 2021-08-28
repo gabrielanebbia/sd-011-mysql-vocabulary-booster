04218ceadc6db885184af12f981898298d7c1e0d
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-08-28 13:05:36 -0300

    adiciona requisito 18

---
+++ desafio18.sql
@@ -0,0 +1,11 @@
+SELECT
+concat(employees.first_name, ' ', employees.last_name) AS `Nome completo`,
+DATE_FORMAT(jobHistory.start_date, '%d/%m/%Y') AS `Data de início`,
+DATE_FORMAT(jobHistory.end_date, '%d/%m/%Y') AS `Data de rescisão`,
+TRUNCATE(DATEDIFF(jobHistory.end_date, jobHistory.start_date) / 365, 2) AS `Anos trabalhados`
+FROM job_history AS jobHistory
+INNER JOIN employees AS employees
+ON jobHistory.EMPLOYEE_ID = employees.EMPLOYEE_ID
+ORDER BY
+concat(employees.first_name, ' ', employees.last_name),
+DATEDIFF(jobHistory.end_date, jobHistory.start_date) / 365;
