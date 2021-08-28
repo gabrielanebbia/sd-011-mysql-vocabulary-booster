aafc7ae90b378676ac208322edd5f0a1262610b2
murilodospacos <murilopacos@gmail.com>
2021-08-28 12:34:07 -0300

    requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,8 @@
+SELECT 
+c.Country AS 'País',
+COUNT(d.CustomerID) AS 'Número de compatriotas'
+FROM w3schools.customers AS c
+INNER JOIN w3schools.customers AS d ON d.Country = c.Country
+AND d.CustomerID <> c.CustomerID
+GROUP BY c.CustomerID
+ORDER BY c.CustomerID;
