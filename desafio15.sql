2a44415ed264b4863f8912faa6b8a95130818e41
Guilherme Nunes <guiicnunes@gmail.com>
2021-08-27 23:45:39 -0300

    FIX: Desafios 15 e 16

--- desafio15.sql
+++ desafio15.sql
@@ -1,16 +1,15 @@
-DELIMITER $$ 
+ELIMITER $$ 
 
-CREATE PROCEDURE buscar_media_por_cargo(
-INOUT jobTitle VARCHAR(300))
+CREATE PROCEDURE buscar_media_por_cargo(INOUT jobTitle VARCHAR(300))
 BEGIN
-SELECT AVG(SALARY) INTO jobTitle
+SET jobTitle = (SELECT AVG(SALARY)
 FROM
 hr.employees AS e
 INNER JOIN
 hr.jobs AS j
 ON
 e.JOB_ID = j.JOB_ID
-WHERE JOB_TITLE = jobTitle;
+WHERE JOB_TITLE = jobTitle);
 END $$ 
 
 DELIMITER ;
