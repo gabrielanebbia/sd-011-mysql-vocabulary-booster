28d0d7e176e720c9e7352b8ea09c04ff509721a8
Matheus Gaspar <msouzagaspar@gmail.com>
2021-08-28 11:56:49 +0200

    desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,13 @@
+SELECT
+t2.ContactName AS 'Nome',
+t2.Country AS 'País',
+.country_count - 1 AS 'Número de compatriotas'
+FROM (
+SELECT
+COUNT(*) as country_count,
+Country
+FROM w3schools.customers
+GROUP BY Country
+) AS t1
+INNER JOIN w3schools.customers AS t2 ON  t1.Country = t2.Country
+ORDER BY t2.ContactName;
