45fc2836f8ac0a81445d92a0ba235f89b6586a9f
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-08-28 13:06:30 -0300

    arruma requisito 18

--- desafio18.sql
+++ desafio18.sql
@@ -2,7 +2,7 @@ SELECT
 concat(employees.first_name, ' ', employees.last_name) AS `Nome completo`,
 DATE_FORMAT(jobHistory.start_date, '%d/%m/%Y') AS `Data de início`,
 DATE_FORMAT(jobHistory.end_date, '%d/%m/%Y') AS `Data de rescisão`,
-TRUNCATE(DATEDIFF(jobHistory.end_date, jobHistory.start_date) / 365, 2) AS `Anos trabalhados`
+CAST(DATEDIFF(jobHistory.end_date, jobHistory.start_date) / 365 AS DECIMAL(10, 2)) AS `Anos trabalhados`
 FROM job_history AS jobHistory
 INNER JOIN employees AS employees
 ON jobHistory.EMPLOYEE_ID = employees.EMPLOYEE_ID
