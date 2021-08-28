131cf870faab43e48277f23133b02e4fc179d2e7
Vinicius <viniciusfod@gmail.com>
2021-08-28 09:15:29 -0300

    req-7

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Nome',
 j.JOB_TITLE AS 'Cargo',
 e.HIRE_DATE AS 'Data de início do cargo',
-d.DEPARTMENT_NAME AS 'Departamento',
+d.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.employees e
 INNER JOIN hr.jobs j ON j.JOB_ID = e.JOB_ID
 INNER JOIN hr.departments d ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
