c5ead6c78587ca093001c5d3b1fcfcc49c66b725
anaclarabck <ackyotoku@gmail.com>
2021-08-28 04:05:29 -0300

    [UPDATE] fixed answer 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,6 +1,6 @@
 SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-DATE_FORMAT(jh.START_DATE, '%Y/%m/%d') AS 'Data de início',
-DATE_FORMAT(jh.END_DATE, '%Y/%m/%d') AS 'Data de rescisão',
+DATE_FORMAT(jh.START_DATE, '%d/%m/%Y') AS 'Data de início',
+DATE_FORMAT(jh.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
 ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE)/365, 2) AS 'Anos trabalhados'
 FROM hr.job_history jh
 LEFT JOIN hr.employees e
