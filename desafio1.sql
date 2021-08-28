cd70c3579c441cafe827c7d41037921845dc0fe9
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-08-28 12:38:24 -0300

    COrrecoes Desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -4,9 +4,9 @@
 #A primeira deve possuir o alias "País" e exibir o nome do país.
 #A segunda deve possuir o alias "Status Inclusão" e deve exibir "incluído" se o país em questão está incluso em "Europe", ou "não incluído", caso contrário.
 #Os resultados devem estar ordenados pelo nome do país em ordem alfabética.
-SELECT c.COUNTRY_NAME, ( 
-SELECT IF ((SELECT REGION_ID FROM hr.regions WHERE hr.regions.REGION_ID = r.REGION_ID) = 1, "incluído", "não incluído"))
+SELECT c.COUNTRY_NAME as País, ( 
+IF ((SELECT REGION_ID FROM hr.regions WHERE hr.regions.REGION_ID = c.REGION_ID) = 1, "incluído", "não incluído"))
 AS "Status Inclusão" FROM hr.countries AS c
 INNER JOIN hr.regions as r
 ON c.REGION_ID = r.REGION_ID
-ORDER BY c.COUNTRY_NAME ASC; 
+ORDER BY c.COUNTRY_NAME ASC;
