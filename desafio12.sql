bb10db638f1f0065f1eb23f865d9e60740f831f3
IsleneGomes <islenegomes@gmail.com>
2021-08-28 13:15:00 -0300

    Corrigindo desafio 12

--- desafio12.sql
+++ desafio12.sql
@@ -4,7 +4,8 @@ e.PHONE_NUMBER AS 'Telefone funcionário 1',
 CONCAT(f.FIRST_NAME, ' ', f.LAST_NAME) AS "Nome completo funcionário 2",
 f.SALARY AS 'Salário funcionário 2',
 f.PHONE_NUMBER AS 'Telefone funcionário 2'
-FROM hr.employees AS e
-LEFT JOIN hr.employees AS f ON e.JOB_ID = f.JOB_ID
-AND e.EMPLOYEE_ID <> f.EMPLOYEE_ID
+FROM hr.employees AS e,
+hr.employees AS f
+WHERE e.JOB_ID = f.JOB_ID
+HAVING `Nome completo funcionário 1` <> `Nome completo funcionário 2`
 ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
