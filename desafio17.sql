a3d0c1e18946c2aa044009820a994ca2d6a5a7cb
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-08-28 20:43:58 -0300

    Requisito 17 Realizado

---
+++ desafio17.sql
@@ -0,0 +1,16 @@
+#Crie uma TRIGGER que, a cada nova inserção realizada na tabela orders, insira automaticamente a data atual na coluna OrderDate
+#Use o banco w3schools como referência
+#A partir disso, o avaliador vai executar dois processos:
+#Inserir na tabela orders um novo registro com os dados: CustomerID com valor 4, EmployeeID com valor 2 e ShipperID com valor 2;
+#Montar uma query que mostre todos os CustomerID igual a 4.
+#Retomando que não é necessário incluir as queries para INSERIR e SELECIONAR, elas já vão ser executadas automaticamente pelo próprio teste.
+DELIMITER $$
+
+CREATE TRIGGER add_in_orders
+BEFORE INSERT ON w3schools.orders
+FOR EACH ROW
+BEGIN
+	 SET NEW.OrderDate = NOW();   
+END $$
+
+DELIMITER ;
