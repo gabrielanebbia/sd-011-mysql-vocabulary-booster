312d2bbfad514208310a7b4bea57a5030a650753
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-08-27 22:57:50 -0300

    ajusta desafio 15

--- desafio15.sql
+++ desafio15.sql
@@ -1,8 +1,12 @@
 DELIMITER $$
 
-CREATE PROCEDURE buscar_media_por_cargo()
+CREATE PROCEDURE buscar_media_por_cargo(IN position VARCHAR(50))
 BEGIN
-    SELECT ROUND(MAX_SALARY - MIN_SALARY, 2) AS `Média salarial` FROM jobs;
+    SELECT ROUND(AVG(SALARY), 2) AS `Média salarial` 
+    FROM employees AS E
+    INNER JOIN jobs AS J
+    ON J.JOB_ID = E.JOB_ID
+    AND J.JOB_TITLE = position;
 END $$
 
 DELIMITER ;
