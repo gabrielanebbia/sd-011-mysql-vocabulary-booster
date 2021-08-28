3db1c78f61e0d0a03262f74342f28b0f77331cbe
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-08-28 19:23:51 -0300

    nunca vai

--- desafio16.sql
+++ desafio16.sql
@@ -1,9 +1,9 @@
 DELIMITER $$
 CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(IN email VARCHAR(100))
 BEGIN
-	SELECT COUNT(jh.JOB_ID) AS "Quantidade de Empregos"
+SELECT COUNT(jh.JOB_ID) AS "Quantidade de Empregos"
     FROM
-		hr.job_history AS jh
+	hr.job_history AS jh
         INNER JOIN
         hr.employees AS e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
         WHERE email = e.EMAIL;
