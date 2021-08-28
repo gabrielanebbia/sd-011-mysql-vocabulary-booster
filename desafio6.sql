47201fa2e3863388d022720ea3c7286b49cdba74
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-08-28 17:37:14 -0300

    fix: req 6-2

--- desafio6.sql
+++ desafio6.sql
@@ -1,13 +1,3 @@
--- SELECT CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS 'Nome completo',
--- job.JOB_TITLE AS 'Cargo',
--- hist.START_DATE AS 'Data de início do cargo',
--- dep.DEPARTMENT_NAME AS 'Departamento'
--- FROM hr.employees AS emp
--- INNER JOIN job_history AS hist ON hist.EMPLOYEE_ID = emp.EMPLOYEE_ID
--- INNER JOIN jobs AS job ON job.JOB_ID = hist.JOB_ID
--- INNER JOIN departments AS dep ON dep.DEPARTMENT_ID = emp.DEPARTMENT_ID
--- ORDER BY CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) DESC, JOB_TITLE;
-
 SELECT CONCAT(employee.FIRST_NAME, ' ', employee.LAST_NAME) AS 'Nome completo',
 j.JOB_TITLE AS 'Cargo',
 job.START_DATE AS 'Data de início de cargo',
