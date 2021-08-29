0430d7d5445071d0e4e4aab7422daea92754976d
OldRoge <rogeriomrjr@icloud.com>
2021-08-29 20:29:20 -0300

    Refactored requirement 17

--- desafio17.sql
+++ desafio17.sql
@@ -1,13 +1,13 @@
 DELIMITER $$
 CREATE TRIGGER adiciona_nova_data_de_pedido
-AFTER INSERT ON orders
+BEFORE INSERT ON orders
 FOR EACH ROW
 BEGIN
-INSERT INTO orders (
-                    CustomerID,
-                    EmployeeID,
-                    ShipperID
-                    )
-VALUES (NEW.CustomerID, NEW.EmployeeID, NEW.ShipperID);
+SET NEW.CustomerID = NEW.CustomerID,
+    NEW.EmployeeID = NEW.EmployeeID,
+    NEW.OrderDate = NOW(),
+    NEW.ShipperID = NEW.ShipperID;
 END $$
 DELIMITER ;
+
+INSERT INTO orders(CustomerID, EmployeeID, ShipperID) VALUES (4, 2, 2);
