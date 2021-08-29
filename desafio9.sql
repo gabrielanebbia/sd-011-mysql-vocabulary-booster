767502d370f9f4015dd6db8c8b80f8301ddedf06
Jessica Arruda <sicarruda@gmail.com>
2021-08-29 16:36:44 -0300

    Exercicio 9

---
+++ desafio9.sql
@@ -0,0 +1,10 @@
+SELECT * from w3schools.employees;
+SELECT * from w3schools.orders;
+
+SELECT CONCAT(E.FirstName ,' ',E.LastName) AS "Nome completo",
+COUNT(O.EmployeeID) AS "Total de pedidos"
+FROM w3schools.orders AS O
+INNER JOIN w3schools.employees AS E
+ON E.EmployeeID = O.EmployeeID
+GROUP BY E.EmployeeID
+ORDER BY 2 ASC;
