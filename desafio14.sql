72815a02c0c047f1c5b49b6df4a2b6642c501e9a
Jessica Arruda <sicarruda@gmail.com>
2021-08-29 20:28:55 -0300

    Exercicio 14 correção

--- desafio14.sql
+++ desafio14.sql
@@ -1,4 +1,4 @@
-SELECT  Country FROM "País" AS w3schools.suppliers
+SELECT  Country AS "Paí­s" FROM w3schools.suppliers
 UNION DISTINCT
 SELECT Country FROM  w3schools.customers
 ORDER BY 1 LIMIT 5;
