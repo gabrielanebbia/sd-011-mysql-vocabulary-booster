3547be717b0a2d410dd77cf62e19bb28bd6fa234
OldRoge <rogeriomrjr@icloud.com>
2021-08-29 01:44:47 -0300

    Fixing erro

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,8 @@
 SELECT
 t1.ContactName AS Nome,
 t1.Country AS País,
-COUNT(t2.Country) - 1 as "Número de compatriotas"
+COUNT(t2.Country) as "Número de compatriotas"
 FROM w3schools.customers AS t1, w3schools.customers AS t2
-WHERE t1.Country = t2.Country
+WHERE t1.Country = t2.Country AND t1.ContactName <> t2.ContactName
 GROUP BY Nome, País
 ORDER BY Nome;
