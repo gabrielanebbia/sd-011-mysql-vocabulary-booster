cb746d82629b6f5ac98adfc7ff816b8d86576be0
Juliana Happatsch <jhappatsch@gmail.com>
2021-08-29 01:15:45 -0300

    requisito 14 - tentando corrigir

--- desafio11.sql
+++ desafio11.sql
@@ -2,6 +2,7 @@ SELECT c1.ContactName AS 'Nome',
 c1.Country AS 'País',
 ( SELECT COUNT(Country) - 1
 FROM w3schools.customers c2
-WHERE c2.Country = c1.Country ) AS 'Número de compatriotas'
+WHERE c2.Country = c1.Country ) AS `Número de compatriotas`
 FROM w3schools.customers c1
+HAVING `Número de compatriotas` <> 0
 ORDER BY c1.ContactName;
