659669f05ff6cc1f6f37f911583513d7d203d6c5
Renan <renan.vamo@gmail.com>
2021-08-29 15:56:54 -0300

    refatora o req 11

--- desafio10.sql
+++ desafio10.sql
@@ -1,10 +1,10 @@
 SELECT p.ProductName 'Produto', 
 MIN(od.Quantity) 'Mínima',
 MAX(od.Quantity) 'Máxima',
-AVG(od.Quantity) 'Média'
+ROUND(AVG(od.Quantity), 2) 'Média'
 FROM w3schools.order_details od
 INNER JOIN w3schools.products p
 ON od.ProductID = p.ProductID
 GROUP BY od.ProductID
 HAVING AVG(od.Quantity) > 20
-ORDER BY AVG(od.Quantity);
+ORDER BY AVG(od.Quantity), p.ProductName;
