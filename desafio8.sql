83184215a26d833f4c562211a0bda2601a8538f9
Marconi <marconieng@gmail.com>
2021-08-28 21:01:50 -0300

    Adiciona desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,14 @@
+SELECT 
+ContactName AS 'Nome de contato',
+sh.ShipperName AS 'Empresa que fez o envio',
+ord.OrderDate AS 'Data do pedido'
+FROM w3schools.customers AS cus
+
+INNER JOIN w3schools.orders AS ord
+ON ord.CustomerID = cus.CustomerID
+
+INNER JOIN w3schools.shippers AS sh
+ON sh.ShipperID = ord.ShipperID
+
+HAVING `Empresa que fez o envio` != 'Federal Shipping'
+ORDER BY `Nome de contato` ASC, `Data do pedido` ASC;
