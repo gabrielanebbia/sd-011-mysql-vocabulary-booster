b530e53d26d668b946ba1b0050a0488fb9264be6
Raphael <raphaellfpg@gmail.com>
2021-08-28 21:01:43 -0300

    Feat: Requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(E.FIRST_NAME ,' ', E.LAST_NAME) AS 'Nome Completo',
+SELECT CONCAT(E.FIRST_NAME ,' ', E.LAST_NAME) AS 'Nome completo',
 J.JOB_TITLE AS 'Cargo',
 E.HIRE_DATE AS 'Data de início do cargo',
 D.DEPARTMENT_NAME AS 'Departamento'
