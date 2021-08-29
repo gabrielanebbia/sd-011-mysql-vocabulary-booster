366776595eeb226c0bb18a0422b3042d5a650509
Savio Moraes <smmsavio@hotmail.com>
2021-08-28 22:56:59 -0300

    retira selects de visualização das tabelas nas primeiras linhas

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,3 @@
-SELECT * FROM hr.jobs;
-SELECT * FROM hr.employees;
-
 SELECT JOB_TITLE AS 'Cargo',
 (MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
 ROUND((MIN_SALARY / 12), 2) AS 'Média mínima mensal',
