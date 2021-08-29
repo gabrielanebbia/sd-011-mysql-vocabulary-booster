9d0bdbefd2fd9a3505d0e67e3fb4fb1f8c3f21a3
Savio Moraes <smmsavio@hotmail.com>
2021-08-29 19:09:09 -0300

    insere alias Número de compatriotas que estava faltando na terceira coluna

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,6 @@
 SELECT customer.ContactName AS 'Nome',
 customer.Country AS 'País',
-COUNT(C.Country)
+COUNT(C.Country) AS 'Número de compatriotas'
 FROM w3schools.customers AS customer
 INNER JOIN w3schools.customers AS C ON customer.Country = C.Country
 WHERE customer.CustomerID <> C.CustomerID
