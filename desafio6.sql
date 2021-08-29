58d27e41c66c3a0151b5f18bf4e5ba7a30e19ea6
Savio Moraes <smmsavio@hotmail.com>
2021-08-28 23:42:14 -0300

    desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,18 @@
+SELECT * FROM hr.jobs;
+SELECT * FROM hr.employees;
+SELECT DEPARTMENT_ID FROM hr.employees;
+SELECT * FROM hr.departments;
+
+SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome completo',
+J.JOB_TITLE AS 'Cargo',
+E.HIRE_DATE AS 'Data de início do cargo',
+D.DEPARTMENT_NAME AS 'Departamento'
+FROM hr.employees AS E
+INNER JOIN hr.jobs AS J ON E.JOB_ID = J.JOB_ID
+INNER JOIN hr.departments AS D ON E.DEPARTMENT_ID = D.DEPARTMENT_ID
+GROUP BY CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)
+ORDER BY CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) ASC, JOB_TITLE ASC;
+ 
+
+
+
