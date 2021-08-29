130e18fb59c48597bf3786daeb12ca2d763e2b4b
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-08-28 21:24:35 -0300

    foiiiii

--- desafio1.sql
+++ desafio1.sql
@@ -2,7 +2,7 @@ SELECT
     COUNTRY_NAME AS 'País',
     IF(REGION_ID = 1,
         'incluído',
-        'não Incluído') AS 'Status Inclusão'
+        'não incluído') AS 'Status Inclusão'
 FROM
     hr.countries
 ORDER BY 1;
