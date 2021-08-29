6e22468cf3e0586f1dcf36786f80f1e07219bd7e
Hamander <1hamander@gmail.com>
2021-08-29 13:11:08 -0300

    Desafio 15 completo

---
+++ desafio15.sql
@@ -0,0 +1,16 @@
+USE hr;
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
+BEGIN
+SELECT 
+ROUND(AVG(em.SALARY), 2) AS 'Média salarial'
+FROM jobs jo
+INNER JOIN employees em
+ON jo.JOB_ID = em.JOB_ID
+WHERE jo.JOB_TITLE LIKE CONCAT('%', cargo, '%');
+END $$
+
+DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');
