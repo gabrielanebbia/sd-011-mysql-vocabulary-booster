130f697470ef742c54d0807a2df006de308c8975
Raphael <raphaellfpg@gmail.com>
2021-08-29 03:59:24 -0300

    Feat: Requisito 12

--- desafio12.sql
+++ desafio12.sql
@@ -1,9 +1,9 @@
 SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome completo funcionário 1',
 E.SALARY AS 'Salário funcionário 1',
 E.PHONE_NUMBER AS 'Telefone funcionário 1',
-CONCAT(E2.FIRST_NAME, ' ', E2.LAST_NAME) AS 'Nome completo funcionário 1',
-E2.SALARY AS 'Salário funcionário 1',
-E2.PHONE_NUMBER AS 'Telefone funcionário 1'
+CONCAT(E2.FIRST_NAME, ' ', E2.LAST_NAME) AS 'Nome completo funcionário 2',
+E2.SALARY AS 'Salário funcionário 2',
+E2.PHONE_NUMBER AS 'Telefone funcionário 2'
 FROM hr.employees AS E
 INNER JOIN hr.employees AS E2
 ON E.JOB_ID = E2.JOB_ID AND E.LAST_NAME <> E2.LAST_NAME
