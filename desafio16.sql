a48934676331c067f4ce769eb3870c9c8db1bc47
Maran <maranmorimoto@gmail.com>
2021-08-29 17:04:47 -0300

    testing requirement 16

--- desafio16.sql
+++ desafio16.sql
@@ -1,12 +1,29 @@
+-- DELIMITER $$
+
+-- CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario (IN var_email VARCHAR(20))
+-- BEGIN
+-- SELECT COUNT(e.employee_id) AS 'total_empregos'
+-- FROM hr.job_history j
+-- INNER JOIN hr.employees e 
+-- ON j.employee_id = e.employee_id
+-- WHERE var_email = e.email;
+-- END $$
+
+-- DELIMITER ; 1ª tentativa, nao deu certo pois o retorno deve ser um array contendo um objeto e com a procedure acima o retorno nao era esse
 DELIMITER $$
 
-CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario (IN var_email VARCHAR(20))
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(var_email varchar(20))
+RETURNS INT READS SQL DATA
 BEGIN
-SELECT COUNT(e.employee_id) AS 'total_empregos'
+DECLARE total_empregos INT;
+SELECT COUNT(*)
 FROM hr.job_history j
 INNER JOIN hr.employees e 
 ON j.employee_id = e.employee_id
-WHERE var_email = e.email;
+WHERE var_email = e.email INTO total_empregos;
+RETURN total_empregos;
 END $$
 
 DELIMITER ;
+
+SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
