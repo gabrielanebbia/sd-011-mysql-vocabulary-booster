21c5c80e8d4e65e6b9041ae34054e367befc1a8c
Reginara Gomes <refagomes@gmail.com>
2021-08-29 11:52:06 -0300

    Corrige requisito 12. Agora vai

--- desafio12.sql
+++ desafio12.sql
@@ -3,7 +3,7 @@ CONCAT(employee_one.FIRST_NAME, ' ', employee_one.LAST_NAME) AS 'Nome completo f
 employee_one.SALARY AS 'Salário funcionário 1',
 employee_one.PHONE_NUMBER AS 'Telefone funcionário 1',
 CONCAT(employee_two.FIRST_NAME, ' ', employee_two.LAST_NAME) AS 'Nome completo funcionário 2',
-employee_two.SALARY AS 'Salário funcionário ',
+employee_two.SALARY AS 'Salário funcionário 2',
 employee_two.PHONE_NUMBER AS 'Telefone funcionário 2'
 FROM
 hr.employees AS employee_one
