00676dc30c1d73f795f092f001f0d5acfc92746c
Jessica Arruda <sicarruda@gmail.com>
2021-08-29 19:54:59 -0300

    Exercicio 13

---
+++ desafio13.sql
@@ -0,0 +1,9 @@
+SELECT * from w3schools.products;
+SELECT * FROM w3schools.order_details;
+SELECT P.ProductName AS "Produto",
+P.Price AS "Preço"
+FROM w3schools.products AS P
+INNER JOIN w3schools.order_details AS AD
+ON P.ProductID = AD.ProductID
+WHERE AD.Quantity > 80
+ORDER BY ProductName;
