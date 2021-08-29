1819a5ed7455f6ad710b14818a803b655e9fb00b
Juliana Happatsch <jhappatsch@gmail.com>
2021-08-29 02:04:43 -0300

    requisito 15 - tentando corrigir 2 (primeira ideia que por causa do ponto e virgula tinha dado erro r

--- desafio15.sql
+++ desafio15.sql
@@ -1,15 +1,12 @@
 DELIMITER $$
 
-CREATE PROCEDURE buscar_media_por_cargo(
-IN cargo VARCHAR(50),
-OUT media DOUBLE
-)
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
 BEGIN
-SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial'
-FROM (
-SELECT * FROM hr.jobs
-WHERE JOB_TITLE = cargo
-) INTO media;
+SELECT ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
+FROM hr.employees e
+INNER JOIN hr.jobs j
+ON j.JOB_ID = e.JOB_ID
+WHERE j.JOB_TITLE = cargo;
 END $$
 
 DELIMITER ;
