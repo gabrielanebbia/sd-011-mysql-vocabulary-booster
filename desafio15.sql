62ab9e10673504fcb3733aad7e922c17a6795ba2
Renan <renan.vamo@gmail.com>
2021-08-29 20:36:11 -0300

    Adiciona o req 14 e 15

---
+++ desafio15.sql
@@ -0,0 +1,10 @@
+DELIMITER //
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
+BEGIN
+SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial'
+FROM hr.employees e
+INNER JOIN hr.jobs j
+ON e.JOB_ID = j.JOB_ID
+WHERE cargo = j.JOB_TITLE;
+END //
+DELIMITER ;
