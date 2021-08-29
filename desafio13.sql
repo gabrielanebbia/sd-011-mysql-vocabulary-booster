f7bfdda6dd8ddd31be561195c21562cb42e592ab
OldRoge <rogeriomrjr@icloud.com>
2021-08-29 11:17:23 -0300

    Done requirement 13

---
+++ desafio13.sql
@@ -0,0 +1,8 @@
+SELECT
+ProductName AS "Produto",
+Price AS "Preço"
+FROM w3schools.products AS products
+INNER JOIN w3schools.order_details AS order_details
+ON products.ProductID = order_details.ProductID
+WHERE Quantity > 80
+ORDER BY `Produto`;
