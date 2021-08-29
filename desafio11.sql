bbe6b131fec7496d97016fb0123ded39497890fa
Savio Moraes <smmsavio@hotmail.com>
2021-08-29 19:02:20 -0300

    desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,8 @@
+SELECT customer.ContactName AS 'Nome',
+customer.Country AS 'País',
+COUNT(C.Country)
+FROM w3schools.customers AS customer
+INNER JOIN w3schools.customers AS C ON customer.Country = C.Country
+WHERE customer.CustomerID <> C.CustomerID
+GROUP BY customer.CustomerID
+ORDER BY customer.ContactName ASC;
