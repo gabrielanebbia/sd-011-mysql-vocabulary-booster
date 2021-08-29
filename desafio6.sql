fb8ce2207a15322a327f4b5ce57c6fa59d081742
Luan Xavier <luantenorio3@gmail.com>
2021-08-29 19:03:37 -0300

    Requisito 6: mudar query

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,12 @@
 SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome completo',
 J.JOB_TITLE AS Cargo,
-E.HIRE_DATE AS 'Data de início do cargo',
+H.START_DATE AS 'Data de início do cargo',
 D.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.employees AS E
 INNER JOIN hr.jobs AS J
 ON J.JOB_ID = E.JOB_ID
+INNER JOIN hr.job_history AS H
+ON H.EMPLOYEE_ID = E.EMPLOYEE_ID
 INNER JOIN hr.departments AS D
 ON E.DEPARTMENT_ID = D.DEPARTMENT_ID
 ORDER BY CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) DESC, Cargo ASC;
