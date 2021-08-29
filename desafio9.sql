bbc0e3a536e4cf8e26998924220c83f870085f5e
DiegoCS777 <diegomethal@gmail.com>
2021-08-29 19:01:51 -0300

    Desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT(e.FirstName, '' , e.LastName) AS 'Nome completo',
+COUNT(*) AS 'Total de pedidos'
+FROM w3schools.employees AS e
+INNER JOIN 
+w3schools.orders AS o
+ON o.EmployeeID = e.EmployeeID
+GROUP BY CONCAT(FirstName, '' , LastName)
+ORDER BY COUNT(*);
