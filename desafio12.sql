722eaebefdde4d00fa00eda7243ff59da71facc1
OldRoge <rogeriomrjr@icloud.com>
2021-08-29 10:49:55 -0300

    Done requirement 12

---
+++ desafio12.sql
@@ -0,0 +1,17 @@
+select * from hr.employees;
+select * from hr.jobs;
+SELECT
+CONCAT(func1.FIRST_NAME, ' ', func1.LAST_NAME) AS "Nome completo funcionário 1",
+func1.SALARY AS "Salário funcionário 1",
+func1.PHONE_NUMBER AS "Telefone funcionário 1",
+CONCAT(func2.FIRST_NAME, ' ', func2.LAST_NAME) AS "Nome completo funcionário 2",
+func2.SALARY AS "Salário funcionário 2",
+func2.PHONE_NUMBER AS "Telefone funcionário 2"
+FROM hr.employees AS func1, hr.employees AS func2
+WHERE
+CONCAT(func1.FIRST_NAME, ' ', func1.LAST_NAME)
+  <>
+CONCAT(func2.FIRST_NAME, ' ', func2.LAST_NAME)
+AND func1.JOB_ID = func2.JOB_ID
+ORDER BY `Nome completo funcionário 1`,
+`Nome completo funcionário 1`;
