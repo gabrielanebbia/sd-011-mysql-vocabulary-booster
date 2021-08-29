25806648899a5c9dedeeed92cab24dfbeb5aad21
menezesrenan7 <renan_menezes07@hotmail.com>
2021-08-29 16:01:59 -0300

    resolução de alguns requisitos

--- desafio11.sql
+++ desafio11.sql
@@ -1,1 +1,1 @@
-SELECT t1.CustomerName AS Nome, t1.Country AS País, (SELECT COUNT(t2.Country) -1 FROM w3schools.customers AS t2 WHERE t1.Country = t2.Country) AS `Número de compatriotas` FROM w3schools.customers AS t1 ORDER BY t1.CustomerName ASC;
+SELECT t1.CustomerName AS Nome, t1.Country AS País, (SELECT COUNT(t2.Country) -1 FROM w3schools.customers AS t2 WHERE t1.Country = t2.Country) AS `Número de compatriotas` FROM w3schools.customers AS t1 WHERE (SELECT COUNT(country) FROM w3schools.customers GROUP BY country HAVING country = c1.country) - 1 > 0 ORDER BY t1.CustomerName ASC;
--- desafio5.sql
+++ desafio5.sql
@@ -1,1 +1,1 @@
-SELECT JOB_TITLE AS Cargo, MAX_SALARY - MIN_SALARY AS `Variação Salarial`, ROUND(MIN_SALARY / 12, 2) AS `Média mínima mensal`, ROUND(MIN_SALARY / 12, 2) AS `Média máxima mensal` FROM hr.jobs ORDER BY `Variação Salarial` ASC, Cargo;
+SELECT JOB_TITLE AS `Cargo`, (MAX_SALARY - MIN_SALARY) AS `Variação Salarial`, ROUND(MIN_SALARY / 12, 2) AS `Média mínima mensal`, ROUND(MIN_SALARY / 12, 2) AS `Média máxima mensal` FROM hr.jobs ORDER BY (MAX_SALARY -MIN_SALARY), JOB_TITLE;
