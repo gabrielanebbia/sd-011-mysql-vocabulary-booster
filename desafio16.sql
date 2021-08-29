2117bb3702393053ae4144649f24f858f490543a
jamneto <netomoura22@gmail.com>
2021-08-29 00:59:56 -0300

    alteraçoes reqs 16 e 18

--- desafio16.sql
+++ desafio16.sql
@@ -1,14 +1,14 @@
 DELIMITER $$
 
-CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario (email VARCHAR(100))
-RETURNS INT DETERMINISTIC
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario (email VARCHAR(50))
+RETURNS INT READS SQL DATA
 BEGIN
+DECLARE total INT;
 SELECT COUNT(job.EMPLOYEE_ID)
 FROM hr.employees AS emp
 INNER JOIN hr.job_history AS job ON job.EMPLOYEE_ID = emp.EMPLOYEE_ID
-WHERE emp.EMAIL = email
-GROUP BY emp.EMPLOYEE_ID;
-RETURN COUNT(job.EMPLOYEE_ID);
+WHERE emp.EMAIL = email INTO total;
+RETURN total;
 END $$
 
 DELIMITER ;
--- desafio18.sql
+++ desafio18.sql
@@ -2,8 +2,9 @@ SELECT
 CONCAT(emp.FIRST_NAME, " ", emp.LAST_NAME) AS "Nome completo",
 DATE_FORMAT(job.START_DATE,'%d/%m/%Y') AS "Data de início",
 DATE_FORMAT(job.END_DATE,'%d/%m/%Y') AS "Data de rescisão",
-ROUND(TIMESTAMPDIFF(DAY, job.START_DATE, job.END_DATE)/365, 2) AS "Anos trabalhados"
+ROUND(DATEDIFF(job.END_DATE, job.START_DATE)/365, 2) AS "Anos trabalhados"
 FROM hr.employees AS emp
 INNER JOIN hr.job_history AS job 
 ON job.EMPLOYEE_ID = emp.EMPLOYEE_ID
-ORDER BY CONCAT(emp.FIRST_NAME, " ", emp.LAST_NAME) ASC;
+ORDER BY CONCAT(emp.FIRST_NAME, " ", emp.LAST_NAME) ASC,
+ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE)/365, 2);
