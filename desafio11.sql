89010790f2beb11f792ab50ec704b25cd523bc2c
IsaacMagno <isaacmagno98@gmail.com>
2021-08-29 18:17:58 -0300

    agora ou nunca agora vai

--- desafio11.sql
+++ desafio11.sql
@@ -3,5 +3,5 @@ t1.Country AS País,
 ((SELECT COUNT(t2.Country)
 FROM w3schools.customers AS t2 WHERE (t1.Country = t2.Country)) - 1) AS `Número de compatriotas`
 FROM w3schools.customers AS t1
-HAVING `Número de compatriotas` > 1
+HAVING `Número de compatriotas` >= 1
 ORDER BY Nome ASC;
--- desafio4.sql
+++ desafio4.sql
@@ -1,5 +1,4 @@
-SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
-SELECT t2.JOB_TITLE AS `Cargo`, ROUND(AVG(t1.SALARY),2) AS `Média salarial`,
+SELECT t2.JOB_TITLE AS `Cargo`, ROUND(AVG(t1.SALARY), 2) AS `Média salarial`,
 CASE WHEN t1.SALARY BETWEEN 2000 AND 5800 THEN 'Júnior'
 WHEN t1.SALARY BETWEEN 5801 AND 7550 THEN 'Pleno'
 WHEN t1.SALARY BETWEEN 7501 AND 10500 THEN 'Sênior'
@@ -8,5 +7,5 @@ END AS 'Senioridade'
 FROM hr.employees AS t1
 INNER JOIN hr.jobs AS t2
 ON t1.JOB_ID = t2.JOB_ID
-GROUP BY t2.JOB_TITLE
-ORDER BY `Média salarial`, Cargo ASC;
+GROUP BY `Cargo`
+ORDER BY `Média salarial`, `Cargo` ASC;
