5735dc5d6dc1de77473423c385d7b3fab919d284
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-08-29 14:05:37 -0300

    task 3

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,6 @@
 SELECT job_title as 'Cargo',
-CASE WHEN max_salary > 30000 THEN 'Altíssimo'
+CASE 
+WHEN max_salary > 30000 THEN 'Altíssimo'
 WHEN max_salary >= 20001 THEN 'Alto'
 WHEN max_salary >= 10001 THEN 'Médio'
 ELSE 'Baixo'
---
+++ desafio3.sql
@@ -0,0 +1,4 @@
+SELECT job_title as 'Cargo',
+max_salary - min_salary as 'Diferença entre salários máximo e mínimo'
+FROM hr.jobs 
+ORDER BY max_salary - min_salary, Cargo ASC;
