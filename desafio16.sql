ac14e56c8177bb9edf46547eb28bd2bf7ef019b8
Maran <maranmorimoto@gmail.com>
2021-08-29 16:12:20 -0300

    requirement 16

---
+++ desafio16.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+
+CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario (IN var_email VARCHAR(20))
+BEGIN
+SELECT COUNT(e.employee_id)
+FROM hr.job_history j
+INNER JOIN hr.employees e 
+ON j.employee_id = e.employee_id
+WHERE var_email = e.email;
+END $$
+
+DELIMITER ;
