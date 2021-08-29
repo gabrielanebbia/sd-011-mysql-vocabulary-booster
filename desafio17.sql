6ac98c65091a7b5a9bae54a61d2a7811b26adb34
OldRoge <rogeriomrjr@icloud.com>
2021-08-29 19:41:49 -0300

    Fixing error

--- desafio17.sql
+++ desafio17.sql
@@ -3,7 +3,11 @@ CREATE TRIGGER adiciona_nova_data_de_pedido
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
-INSERT INTO order_date (OrderDate)
+INSERT INTO orders (
+                    CustomerID,
+                    EmployeeID,
+                    ShipperID
+                    )
 VALUES (NEW.CustomerID, NEW.EmployeeID, NEW.ShipperID);
 END $$
 DELIMITER ;
