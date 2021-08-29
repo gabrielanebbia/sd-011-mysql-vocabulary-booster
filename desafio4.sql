32008bf49f2bb37a9566288cb65d7ee8c383ca14
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-08-29 15:08:46 -0300

    task 4 try 1.2

--- desafio4.sql
+++ desafio4.sql
@@ -1,13 +1,11 @@
 SELECT jobs.JOB_TITLE AS 'Cargo',
 ROUND(AVG(emp.SALARY), 2) AS 'Média salarial',
-
 CASE
 WHEN ROUND(AVG(emp.SALARY), 2) > 10500 THEN 'CEO'
 WHEN ROUND(AVG(emp.SALARY), 2) > 7500 THEN 'Sênior'
 WHEN ROUND(AVG(emp.SALARY), 2) > 5800 THEN 'Pleno'
-ELSE 'Junior'
+ELSE 'Júnior'
 END AS 'Senioridade'
-
 FROM hr.jobs AS jobs
 INNER JOIN hr.employees AS emp
 ON jobs.JOB_ID = emp.JOB_ID
