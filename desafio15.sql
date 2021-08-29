331ffb99dc4b76442cac3550816d3fddc642f918
Juliana Happatsch <jhappatsch@gmail.com>
2021-08-29 01:57:46 -0300

    requisito 15 - tentando corrigir

--- desafio15.sql
+++ desafio15.sql
@@ -5,7 +5,7 @@ IN cargo VARCHAR(50),
 OUT media DOUBLE
 )
 BEGIN
-SELECT ROUND(AVG(SALARY), 2)
+SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial'
 FROM (
 SELECT * FROM hr.jobs
 WHERE JOB_TITLE = cargo
