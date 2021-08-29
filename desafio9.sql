07e220ec81b0782469f43bd17a19711ee1295495
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-08-29 13:27:04 -0300

    FEito 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,10 +1,12 @@
--- SELECT * FROM categories;
--- SELECT * FROM customers;
--- SELECT * FROM order_details;
--- SELECT * FROM products;
--- SELECT * FROM employees;
--- SELECT * FROM shippers;
--- SELECT * FROM suppliers;
--- SELECT * FROM w3schools.customers;
--- SELECT * FROM w3schools.orders;
--- SELECT * FROM w3schools.shippers;
+-- 9 - Exibe todos as pessoas funcionárias que já realizaram algum pedido, mostrando também seu total de pedidos feitos
+-- Monte uma query que exiba 02 colunas:
+-- A primeira deve possuir o alias "Nome completo" e exibir o nome completo da pessoa funcionária (não se esqueça do espaço entre o nome e o sobrenome).
+-- A segunda deve possuir o alias "Total de pedidos" e exibir a quantidade total de pedidos feitos pela pessoa.
+-- Ordene seus resultados pelo total de pedidos em ordem crescente.
+SELECT CONCAT(emp.FirstName, ' ', emp.LastName) AS `Nome completo`,
+COUNT(ord.OrderID) AS `Total de pedidos`
+FROM w3schools.employees AS emp
+INNER JOIN w3schools.orders AS ord
+ON emp.EmployeeID = ord.EmployeeID
+GROUP BY emp.EmployeeID
+ORDER BY COUNT(ord.OrderID); 
