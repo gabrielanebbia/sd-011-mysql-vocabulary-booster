bf1e17934c1eb9998dbcfa40f6c6d00353046fcd
DiegoCS777 <diegomethal@gmail.com>
2021-08-29 16:58:32 -0300

    Desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome Completo',
+jh.START_DATE AS 'Data de início',
+e.SALARY AS 'Salário'
+FROM hr.employees AS e
+INNER JOIN
+hr.job_history AS jh
+ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE MONTH(jh.START_DATE) IN (01 , 02, 03)
+ORDER BY CONCAT(FIRST_NAME, ' ', LAST_NAME), START_DATE;
