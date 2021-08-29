b6ea276dc1b8b0a6fd7a2144b6659c528840ac82
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-08-28 23:58:09 -0300

    Corrige Code Climate again

--- desafio10.sql
+++ desafio10.sql
@@ -1,8 +1,4 @@
-SELECT
-	t1.ProductName AS 'Produto',
-    MIN(t2.Quantity) AS 'Mínima',
-    MAX(t2.Quantity) AS 'Máxima',
-    ROUND(AVG(t2.Quantity), 2) AS 'Média'
+SELECT t1.ProductName AS 'Produto', MIN(t2.Quantity) AS 'Mínima', MAX(t2.Quantity) AS 'Máxima', ROUND(AVG(t2.Quantity), 2) AS 'Média'
 FROM w3schools.products AS t1
 INNER JOIN w3schools.order_details AS t2
 ON t1.ProductID = t2.ProductID
--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,4 @@
-SELECT
-	UCASE(CONCAT(t1.FIRST_NAME, ' ', t1.LAST_NAME)) AS 'Nome completo',
-    t2.START_DATE AS 'Data de início',
-    t1.SALARY AS 'Salário'
+SELECT UCASE(CONCAT(t1.FIRST_NAME, ' ', t1.LAST_NAME)) AS 'Nome completo', t2.START_DATE AS 'Data de início', t1.SALARY AS 'Salário'
 FROM hr.employees AS t1
 INNER JOIN hr.job_history AS t2
 ON t1.EMPLOYEE_ID = t2.EMPLOYEE_ID
