aa8258ae08ad044377b75263b4452a07f561f1a4
IsaacMagno <isaacmagno98@gmail.com>
2021-08-29 17:52:47 -0300

    go

--- desafio4.sql
+++ desafio4.sql
@@ -1,5 +1,5 @@
 SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
-SELECT t2.JOB_TITLE AS Cargo, ROUND(AVG(t1.SALARY),2) AS 'Média salarial',
+SELECT t2.JOB_TITLE AS Cargo, ROUND(AVG(t1.SALARY),2) AS `Média salarial`,
 CASE
 WHEN t1.SALARY BETWEEN 2000 AND 5800 THEN 'Júnior'
 WHEN t1.SALARY BETWEEN 5801 AND 7550 THEN 'Pleno'
@@ -10,4 +10,4 @@ FROM hr.employees AS t1
 INNER JOIN hr.jobs AS t2
 ON t1.JOB_ID = t2.JOB_ID
 GROUP BY t2.JOB_TITLE
-ORDER BY t1.SALARY, Cargo ASC;
+ORDER BY `Média salarial`, Cargo ASC;
