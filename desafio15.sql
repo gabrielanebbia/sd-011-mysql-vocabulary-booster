dd24bd36e838e42cfba09c34fbadba4d115e6be5
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-08-28 21:22:42 -0300

    Arrisca req. 15

---
+++ desafio15.sql
@@ -0,0 +1,12 @@
+USE hr;
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(100))
+BEGIN
+    SELECT ROUND(AVG(SALARY),2) AS `Média salarial`
+    FROM hr.employees AS e
+    INNER JOIN hr.jobs AS j
+    ON e.JOB_ID = j.JOB_ID
+    WHERE job = j.JOB_TITLE;
+END $$
+
+DELIMITER ; 
