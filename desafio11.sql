847d286856f718648ab8cfdc84cfeb4d61f60107
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-08-29 16:27:17 -0300

    ex11

--- desafio11.sql
+++ desafio11.sql
@@ -1,10 +1,21 @@
--- SELECT * FROM categories;
--- SELECT * FROM customers;
--- SELECT * FROM order_details;
---  SELECT * FROM w3schools.products;
--- SELECT * FROM employees;
--- SELECT * FROM shippers;
--- SELECT * FROM suppliers;
--- SELECT * FROM w3schools.customers;
--- SELECT * FROM w3schools.orders;
--- SELECT * FROM w3schools.shippers;
+-- 11 - Exibe todas as pessoas clientes que possuem compatriotas, mostrando a quantidade de compatriotas para cada pessoa cliente
+-- Mostre a quantidade de compatriotas para cada pessoa cliente.
+-- Suponha que haja apenas 04 clientes:
+-- Maria, do Brasil.
+-- João, do Brasil.
+-- Gabriela, do Brasil.
+-- Alex, da Irlanda.
+-- Logo, podemos dizer que Maria, João e Gabriela são compatriotas entre si, haja visto que as três pessoas moram no Brasil. Além disso, podemos dizer que tanto Maria quanto João e Gabriela possuem duas pessoas compatriotas associadas. Já Alex não possui compatriota, haja visto que não há outras pessoas da Irlanda.
+-- Monte uma query que exiba 03 colunas:
+-- A primeira deve possuir o alias "Nome" e exibir o nome de contato da pessoa cliente.
+-- A segunda deve possuir o alias "País" e exibir o nome do país em que a pessoa reside.
+-- A terceira deve possuir o alias "Número de compatriotas" e exibir o número de pessoas que moram no mesmo país.
+-- Os resultados devem estar ordenados pelo nome de contato da pessoa cliente em ordem alfabética.
+SELECT ContactName AS `Nome`,
+Country AS `País`,
+(SELECT COUNT(Country)
+FROM w3schools.customers
+WHERE w3schools.customers.Country = cus1.Country 
+AND w3schools.customers.CustomerID <> cus1.CustomerID) AS `Número de compatriotas`
+FROM w3schools.customers AS cus1
+ORDER BY `Nome`;
