5368a0e4a47a6f2f0b6a3df6f4a74946dbeb3af5
Renan <renan.vamo@gmail.com>
2021-08-29 16:30:12 -0300

    Adiciona o req 11

---
+++ desafio11.sql
@@ -0,0 +1,8 @@
+SELECT c1.CustomerName 'Nome',
+c1.Country 'País',
+COUNT(c1.Country) - 1 'Número de compatriotas'
+FROM w3schools.customers AS c1, w3schools.customers AS c2
+WHERE c1.Country = c2.Country
+GROUP BY c1.CustomerName
+HAVING COUNT(c1.Country) - 1 > 1 
+ORDER BY c1.CustomerName;
