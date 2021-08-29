77d1c9d535579c5f0525a1c14a986bdab4c6c731
jamneto <netomoura22@gmail.com>
2021-08-29 01:04:48 -0300

    alteraçoes req 18

--- desafio18.sql
+++ desafio18.sql
@@ -4,7 +4,7 @@ DATE_FORMAT(job.START_DATE,'%d/%m/%Y') AS "Data de início",
 DATE_FORMAT(job.END_DATE,'%d/%m/%Y') AS "Data de rescisão",
 ROUND(DATEDIFF(job.END_DATE, job.START_DATE)/365, 2) AS "Anos trabalhados"
 FROM hr.employees AS emp
-INNER JOIN hr.job_history AS job 
+JOIN hr.job_history AS job 
 ON job.EMPLOYEE_ID = emp.EMPLOYEE_ID
 ORDER BY CONCAT(emp.FIRST_NAME, " ", emp.LAST_NAME) ASC,
-ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE)/365, 2);
+ROUND(DATEDIFF(job.END_DATE, job.START_DATE)/365, 2);
