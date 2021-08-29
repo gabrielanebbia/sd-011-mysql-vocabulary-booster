912e984928b02559ac97f9aabfd6d1323a803b91
Cris Lustosa <cmlustosa@gmail.com>
2021-08-29 12:50:06 -0300

    Corrigindo erro de letra maiucula da palavra completo

--- desafio7.sql
+++ desafio7.sql
@@ -1,4 +1,4 @@
-SELECT UPPER((CONCAT(empl.FIRST_NAME, ' ', empl.LAST_NAME))) AS 'Nome Completo',
+SELECT UPPER((CONCAT(empl.FIRST_NAME, ' ', empl.LAST_NAME))) AS 'Nome completo',
 j_h.START_DATE AS 'Data de início',
 empl.SALARY AS 'Salário'
 FROM
