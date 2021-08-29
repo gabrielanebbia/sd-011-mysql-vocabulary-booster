857d8d425bf2403bd19fca175e6ef5aafe9025a8
Lais Simenikim <l.simenikim@gmail.com>
2021-08-29 20:58:32 -0300

    Add challenge 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,7 @@
-//commit inicial 
+SELECT COUNTRY_NAME AS País,
+CASE
+WHEN REGION_ID = 1 THEN 'incluído'
+WHEN REGION_ID != 1 THEN 'não incluído'
+ELSE REGION_ID = 'Não tem'
+END AS 'Status Inclusão'
+FROM hr.countries;
