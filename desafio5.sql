ac406d1b659e9eb6b7b76717fb4a5b95cc44c2a6
Luan Xavier <luantenorio3@gmail.com>
2021-08-29 17:47:17 -0300

    Requisito 5: simplificar query

--- desafio5.sql
+++ desafio5.sql
@@ -3,7 +3,5 @@ ROUND(MAX(J.MAX_SALARY) - MIN(J.MIN_SALARY), 2) AS 'Variação Salarial',
 ROUND(MIN(J.MIN_SALARY) / 12, 2) AS 'Média mínima mensal',
 ROUND(MAX(J.MAX_SALARY) / 12, 2) AS 'Média máxima mensal'
 FROM hr.jobs AS J
-INNER JOIN hr.employees AS E
-ON J.JOB_ID = E.JOB_ID
 GROUP BY Cargo
 ORDER BY ROUND(MAX(J.MAX_SALARY) - MIN(J.MIN_SALARY), 2) ASC, Cargo ASC;
