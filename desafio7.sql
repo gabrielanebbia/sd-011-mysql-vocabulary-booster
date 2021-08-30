658fb8cb8f11ce4ad821be711361c30ab68edb64
Laura Gusmao <laurargus@gmail.com>
2021-08-30 17:39:14 -0300

    Teste Code Climate

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,7 @@
 SELECT
-	ucase(concat(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
-    jh.START_DATE AS 'Data de início',
-    e.SALARY AS 'Salário'
+ucase(concat(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+jh.START_DATE AS 'Data de início',
+e.SALARY AS 'Salário'
 FROM hr.employees e
 INNER JOIN hr.job_history jh
 ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
