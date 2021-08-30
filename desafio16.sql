4a3bae3ddcf0dc746b1a6a5a6421d74039a03999
Tulio Sploradori <tuliospl@gmail.com>
2021-08-30 12:33:42 -0300

    Req 11 ao 16

---
+++ desafio16.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(input_email VARCHAR(100)) 
+RETURNS int READS SQL DATA
+BEGIN
+DECLARE total_jobs INT;
+SELECT COUNT(*)
+FROM hr.job_history
+WHERE employee_id IN (
+SELECT employee_id 
+FROM hr.employees
+WHERE email = input_email
+);
+INTO total_jobs;
+RETURN total_jobs;
+END $$
+DELIMITER ;
