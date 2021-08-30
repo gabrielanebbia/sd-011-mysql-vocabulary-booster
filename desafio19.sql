01190f3b948b998b767dc7374243f760d368d942
Vinicius <viniciusfod@gmail.com>
2021-08-30 11:12:30 -0300

    req-19 req-20

---
+++ desafio19.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+CREATE FUNCTION hr.exibir_quantidade_pessoas_contratadas_por_mes_e_ano (
+mesContr INT, anoContr INT)
+RETURNS INT DETERMINISTIC
+BEGIN
+DECLARE retorno INT;
+SET retorno = 0;
+SELECT
+COUNT(e.EMPLOYEE_ID) INTO retorno
+FROM hr.employees e
+WHERE YEAR(e.HIRE_DATE) = anoContr AND MONTH(e.HIRE_DATE) = mesContr;
+RETURN retorno;
+END $$
+DELIMITER ;
---
+++ desafio20.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+CREATE PROCEDURE hr.exibir_historico_completo_por_funcionario (
+IN emailFunc VARCHAR(255))
+BEGIN
+SELECT
+CONCAT(e.FIRST_NAME, " ", e.LAST_NAME) AS "Nome completo",
+d.DEPARTMENT_NAME AS Departamento,
+j.JOB_TITLE AS Cargo
+FROM hr.employees e
+LEFT JOIN hr.job_history jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+LEFT JOIN hr.departments d ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
+LEFT JOIN hr.jobs j on j.JOB_ID = jh.JOB_ID
+WHERE e.EMAIL = emailFunc
+ORDER BY d.DEPARTMENT_NAME ASC, j.JOB_TITLE ASC;
+END $$
+DELIMITER ;
