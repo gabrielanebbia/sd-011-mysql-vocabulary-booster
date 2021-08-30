75aa2c895373b4072d65be78a5e2b4803038f56c
OldRoge <rogeriomrjr@icloud.com>
2021-08-29 21:41:47 -0300

    Fixing code climate indentations

--- desafio18.sql
+++ desafio18.sql
@@ -10,7 +10,7 @@ CONCAT(
 DATE_FORMAT(END_DATE, "%d"), '/',
 DATE_FORMAT(END_DATE, "%m"), '/',
 DATE_FORMAT(END_DATE, "%Y")
-       ) AS "Data de rescisão",
+) AS "Data de rescisão",
 ROUND((DATEDIFF(END_DATE, START_DATE) / 365), 2) AS "Anos trabalhados"
 FROM hr.employees AS employees
 INNER JOIN hr.job_history AS job_history
