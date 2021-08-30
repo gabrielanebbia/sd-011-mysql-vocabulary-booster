7e721da2c2da8dba0bcf7ce45e738c231edb284a
Renan <renan.vamo@gmail.com>
2021-08-30 15:21:54 -0300

    refatora o 11

--- desafio11.sql
+++ desafio11.sql
@@ -3,6 +3,6 @@ c1.Country 'País',
 COUNT(c1.Country) - 1 'Número de compatriotas'
 FROM w3schools.customers AS c1, w3schools.customers AS c2
 WHERE c1.Country = c2.Country
-GROUP BY c1.ContactName
+GROUP BY c1.ContactName, c1.Country
 HAVING COUNT(c1.Country) - 1 > 0
 ORDER BY c1.ContactName;
