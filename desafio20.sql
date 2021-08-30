480a29478b75afa5c1a1c605398e6174715950a7
Bruno Duarte <brunoarduarte@gmail.com>
2021-08-29 22:53:57 -0300

    Desafio 20

---
+++ desafio20.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+CREATE PROCEDURE `exibir_historico_completo_por_funcionario`(IN email VARCHAR(100))
+BEGIN
+    SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome completo',
+    D.DEPARTMENT_NAME AS 'Departamento',
+    J.JOB_TITLE AS 'Cargo'
+    FROM employees as E
+    INNER JOIN departments AS D ON E.DEPARTMENT_ID = D.DEPARTMENT_ID
+    INNER JOIN job_history AS JH ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
+    INNER JOIN jobs AS J ON J.JOB_ID = JH.JOB_ID
+    WHERE E.EMAIL = email
+    ORDER BY `Departamento`, `Cargo`;
+END$$
+DELIMITER ;
+
+CALL exibir_historico_completo_por_funcionario('NKOCHHAR');
