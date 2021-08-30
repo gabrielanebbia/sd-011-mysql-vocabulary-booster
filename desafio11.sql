f6a6fe26dd4790fe7fa510f0f003eff55691028b
Vitor <vitorguima@gmail.com>
2021-08-30 19:04:17 -0300

    req 10 e req 11

---
+++ desafio11.sql
@@ -0,0 +1,13 @@
+SELECT 
+C.ContactName AS 'Nome',
+C.Country AS 'País',
+CR.CountryRepeats - 1 AS `Número de compatriotas`
+FROM w3schools.customers AS C
+LEFT JOIN (
+SELECT Country, COUNT(Country) AS CountryRepeats FROM w3schools.customers
+GROUP BY Country
+) AS CR
+ON C.Country = CR.Country
+WHERE CR.CountryRepeats - 1 > 0
+GROUP BY CustomerName
+ORDER BY ContactName;
