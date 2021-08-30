14db0c9732fdabf5af508f2e533fb43726069637
Luan Xavier <luantenorio3@gmail.com>
2021-08-30 12:25:21 -0300

    Requisito 13 finalizado

---
+++ desafio13.sql
@@ -0,0 +1,6 @@
+SELECT P.ProductName AS Produto,
+P.Price AS Preço
+FROM w3schools.products AS P
+INNER JOIN w3schools.order_details AS OD
+ON OD.ProductID = P.ProductID AND OD.Quantity > 80
+ORDER BY Produto;
