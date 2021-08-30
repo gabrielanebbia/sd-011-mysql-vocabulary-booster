6d9853340d2d1703ce0a03920891b17ebcd2b58c
Renan <renan.vamo@gmail.com>
2021-08-30 16:02:44 -0300

    Refatora o req 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,5 +1,5 @@
-SELECT Country FROM w3schools.suppliers
-UNION ALL
-SELECT Country FROM w3schools.customers
-ORDER BY Country
+SELECT Country AS `País` FROM w3schools.suppliers
+UNION
+SELECT Country AS `País` FROM w3schools.customers
+ORDER BY `País`
 LIMIT 5;
