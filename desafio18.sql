155a9b92aefa5c1c242ed3191336643afd3cec8b
OldRoge <rogeriomrjr@icloud.com>
2021-08-29 21:38:18 -0300

    Fixed start_date and end_date format

--- desafio18.sql
+++ desafio18.sql
@@ -1,19 +1,18 @@
 SELECT
 CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS "Nome completo",
+-- https://www.w3schools.com/sql/trymysql.asp?filename=trysql_func_mysql_date_format
 CONCAT(
-       DAY(job_history.START_DATE), '/',
-       MONTH(job_history.START_DATE), '/',
-       YEAR(job_history.START_DATE)
-       ) AS "Data de início",
+DATE_FORMAT(START_DATE, "%d"), '/',
+DATE_FORMAT(START_DATE, "%m"), '/',
+DATE_FORMAT(START_DATE, "%Y")
+) AS "Data de início",
 CONCAT(
-       DAY(job_history.END_DATE), '/',
-       MONTH(job_history.END_DATE), '/',
-       YEAR(job_history.END_DATE)
+DATE_FORMAT(END_DATE, "%d"), '/',
+DATE_FORMAT(END_DATE, "%m"), '/',
+DATE_FORMAT(END_DATE, "%Y")
        ) AS "Data de rescisão",
 ROUND((DATEDIFF(END_DATE, START_DATE) / 365), 2) AS "Anos trabalhados"
 FROM hr.employees AS employees
 INNER JOIN hr.job_history AS job_history
 ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
 ORDER BY `Nome completo`, `Anos trabalhados`;
-
-select * from hr.employees;
