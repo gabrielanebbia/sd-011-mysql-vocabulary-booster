20b165415c72fc9412da85c3d8fa26597644634b
Lais Simenikim <l.simenikim@gmail.com>
2021-08-29 21:23:05 -0300

    Ajust challenge 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,6 @@
 SELECT COUNTRY_NAME AS País,
 CASE
 WHEN REGION_ID = 1 THEN 'incluído'
-WHEN REGION_ID != 1 THEN 'não incluído'
-ELSE REGION_ID = 'Não tem'
+ELSE REGION_ID = 'não incluído'
 END AS 'Status Inclusão'
 FROM hr.countries;
