44b60ef2267b5de274101c7914940f839edb73af
LeoLacana <wilson17414@gmail.com>
2021-08-30 13:40:23 -0300

    requisito 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,5 +1,5 @@
 SELECT CONCAT(first_name, ' ', last_name) AS 'Nome completo',
-CONCAT(DATE_FORMAT(start_date, '%d'), '/', DATE_FORMAT(start_date, '%m'), '/', YEAR(start_date)) AS 'Data de inicio',
+CONCAT(DATE_FORMAT(start_date, '%d'), '/', DATE_FORMAT(start_date, '%m'), '/', YEAR(start_date)) AS 'Data de início',
 CONCAT(DATE_FORMAT(end_date, '%d'), '/', DATE_FORMAT(end_date, '%m'), '/', YEAR(end_date)) AS 'Data de rescisão',
 ROUND(DATEDIFF(end_date, start_date)/365, 2) AS 'Anos trabalhados'
 FROM hr.employees e
