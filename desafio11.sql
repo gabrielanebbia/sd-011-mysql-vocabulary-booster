07a49e41d2fe6430a7b7bfe2c34953f5ca6a2c74
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-08-30 20:02:54 -0300

    Requisito 11 e 12

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,7 @@
 SELECT customersA.ContactName AS `Nome`,
 customersA.Country AS `País`,
-( SELECT
-COUNT(Country) -1
+(SELECT COUNT(Country) -1
 FROM w3schools.customers AS customersB
-WHERE customersB.Country = customersA.Country ) AS `Número de compatriotas`
+WHERE customersB.Country = customersA.Country) AS `Número de compatriotas`
 FROM w3schools.customers AS customersA
 ORDER BY customersA.ContactName;
