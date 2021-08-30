fd06c46503c4b33b4cd5ceab507227c76fa3ae1e
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-08-30 18:29:26 -0300

    Fixing exercise 15 code climate

--- desafio15.sql
+++ desafio15.sql
@@ -3,10 +3,10 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(200))
 BEGIN
-	SELECT ROUND(AVG(E.salary), 2) AS 'Média salarial'
-    FROM hr.employees AS E
-	INNER JOIN hr.jobs AS J ON J.JOB_ID = E.JOB_ID
-	WHERE J.JOB_TITLE = cargo;
+SELECT ROUND(AVG(E.salary), 2) AS 'Média salarial'
+FROM hr.employees AS E
+INNER JOIN hr.jobs AS J ON J.JOB_ID = E.JOB_ID
+WHERE J.JOB_TITLE = cargo;
 END $$
 
 DELIMITER ;
