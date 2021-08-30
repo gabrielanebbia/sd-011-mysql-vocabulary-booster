24712e29ebda9feccac90e827c98779d35d17fa8
jorgemeyrelles <jotaengpuc@gmail.com>
2021-08-30 14:09:47 -0300

    fix: desafios 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,7 @@
 SELECT
 CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
-DATE_FORMAT((jh.start_date), '%d/%m/%y') AS 'Data de início',
-DATE_FORMAT((jh.end_date), '%d/%m/%y') AS 'Data de recisão',
+DATE_FORMAT((jh.start_date), '%d/%m/%Y') AS 'Data de início',
+DATE_FORMAT((jh.end_date), '%d/%m/%Y') AS 'Data de recisão',
 -- CONCAT(FLOOR(DATEDIFF(jh.end_date, jh.start_date)/365), ' ano(s) e ', FLOOR(12*((DATEDIFF(jh.end_date, jh.start_date)/365)-FLOOR(DATEDIFF(jh.end_date, jh.start_date)/365))), ' mes(es)') AS 'Anos de trabalho'
 ROUND(DATEDIFF(jh.end_date, jh.start_date)/365, 2) AS 'Anos trabalhados'
 FROM job_history AS jh
