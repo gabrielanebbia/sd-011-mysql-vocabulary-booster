d483d1015581f6320c1b48ef2d274805dc41da2b
Pedro Piquini <piquini.pedro@gmail.com>
2021-08-30 15:35:02 -0300

    correçao desafio 18, ajustando formato do dia

--- desafio18.sql
+++ desafio18.sql
@@ -1,6 +1,6 @@
 SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-CONCAT(DAY(h.START_DATE), '/', DATE_FORMAT(h.START_DATE, '%m'), '/', YEAR(h.START_DATE)) AS 'Data de início',
-CONCAT(DAY(h.END_DATE), '/', DATE_FORMAT(h.END_DATE, '%m'), '/', YEAR(h.END_DATE)) AS 'Data de rescisão',
+CONCAT(DATE_FORMAT(h.START_DATE, '%d'), '/', DATE_FORMAT(h.START_DATE, '%m'), '/', YEAR(h.START_DATE)) AS 'Data de início',
+CONCAT(DATE_FORMAT(h.START_DATE, '%d'), '/', DATE_FORMAT(h.END_DATE, '%m'), '/', YEAR(h.END_DATE)) AS 'Data de rescisão',
 ROUND((DATEDIFF(h.END_DATE, h.START_DATE) / 365), 2) AS 'Anos trabalhados'
 FROM job_history AS h
 INNER JOIN employees AS e
