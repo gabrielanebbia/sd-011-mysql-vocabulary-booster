cfc89a433d82261c09b3608b05d0d17247fc1c0e
Tulio Sploradori <tuliospl@gmail.com>
2021-08-30 12:35:32 -0300

    Req 11 ao 16

--- desafio12.sql
+++ desafio12.sql
@@ -8,8 +8,8 @@ AS "Nome completo funcionário 2",
 t2.SALARY AS "Salário funcionário 2",
 t2.PHONE_NUMBER AS "Telefone funcionário 2"
 FROM 
- hr.employees AS t1,
-  hr.employees AS t2
+hr.employees AS t1,
+hr.employees AS t2
 WHERE t1.job_id = t2.job_id 
 AND t1.employee_id <> t2.employee_id
 ORDER BY `Nome completo funcionário 1`,
