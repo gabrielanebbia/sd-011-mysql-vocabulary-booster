d34c84d2d05ab3d96219ac7eeabe0bb746eae227
Wilquer82 <wilquer.fig@gmail.com>
2021-08-30 15:29:38 -0300

    corregindo 19

--- desafio19.sql
+++ desafio19.sql
@@ -1,8 +1,19 @@
-SELECT job_title AS Cargo, CASE
-    WHEN max_salary BETWEEN 5000 AND 10000 THEN "Baixo"
-    WHEN max_salary BETWEEN 10001 AND 20000 THEN "Médio"
-    WHEN max_salary BETWEEN 20001 AND 30000 THEN "Alto"
-    WHEN max_salary > 30000 THEN "Altíssimo"
-    END AS Nível
-FROM hr.jobs
-ORDER BY Cargo;
+USE hr;
+
+DELIMITER $$
+
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE data_contratacao INT;
+SELECT
+COUNT(*)
+FROM employees
+WHERE mes = MONTH(HIRE_DATE) AND ano = YEAR(HIRE_DATE)
+    INTO data_contratacao;
+    RETURN data_contratacao;
+END $$
+
+DELIMITER $$;
+
+SELECT EXIBIR_QUANTIDADE_PESSOAS_CONTRATADAS_POR_MES_E_ANO(6, 1987);
