e1c3363ba10f2a5fff16e3ea10d77e24df8906c0
jorgemeyrelles <jotaengpuc@gmail.com>
2021-08-30 14:05:16 -0300

    fix: desafios 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,9 +1,9 @@
 SELECT
 CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
-CONCAT(DAY(jh.start_date), '/', MONTH(jh.start_date),'/', YEAR(jh.start_date)) AS 'Data de início',
-CONCAT(DAY(jh.end_date), '/', MONTH(jh.end_date),'/', YEAR(jh.end_date)) AS 'Data de recisão',
+DATE_FORMAT((jh.start_date), '%d/%m/%y') AS 'Data de início',
+DATE_FORMAT((jh.end_date), '%d/%m/%y') AS 'Data de recisão',
 -- CONCAT(FLOOR(DATEDIFF(jh.end_date, jh.start_date)/365), ' ano(s) e ', FLOOR(12*((DATEDIFF(jh.end_date, jh.start_date)/365)-FLOOR(DATEDIFF(jh.end_date, jh.start_date)/365))), ' mes(es)') AS 'Anos de trabalho'
-ROUND(DATEDIFF(jh.end_date, jh.start_date)/365, 2) AS 'Anos de trabalho'
+ROUND(DATEDIFF(jh.end_date, jh.start_date)/365, 2) AS 'Anos trabalhados'
 FROM job_history AS jh
 INNER JOIN employees AS e
 ON jh.employee_id = e.employee_id
