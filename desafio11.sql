d64bcafd95ae5e48159402268ed1fc2aa4d1872b
Pedro Piquini <piquini.pedro@gmail.com>
2021-08-30 10:10:24 -0300

    desafio 12 completo

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,9 @@
 SELECT c1.ContactName AS 'Nome',
 c1.Country AS 'País',
-(SELECT (COUNT(c2.Country) - 1)
+(SELECT COUNT(c2.Country)
 FROM customers as c2
-WHERE c2.Country = c1.Country
+WHERE c2.Country = c1.Country AND c1.ContactName <> c2.ContactName
 ) AS 'Número de compatriotas'
 FROM customers AS c1
-GROUP BY `Nome` HAVING `Número de compatriotas` > 0
+GROUP BY `Nome`, `País` HAVING `Número de compatriotas` > 0
 ORDER BY `Nome`;
