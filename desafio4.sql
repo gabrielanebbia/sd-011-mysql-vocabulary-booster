07663bca8581bbe3ee21c15c2bed2c8d5946c451
AlexSmith7192 <alexsmith7192@gmail.com>
2021-08-30 18:07:59 -0300

    solves challenge four

---
+++ desafio4.sql
@@ -0,0 +1,15 @@
+SELECT
+  job.JOB_TITLE AS Cargo,
+  ROUND(AVG(emp.SALARY), 2) AS 'Média salarial',
+  CASE
+    WHEN AVG(emp.SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
+    WHEN AVG(emp.SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
+    WHEN AVG(emp.SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
+    WHEN AVG(emp.SALARY) > 10500 THEN 'CEO'
+  END AS Senioridade
+FROM
+  hr.employees AS emp
+  INNER JOIN hr.jobs AS job
+  ON emp.JOB_ID = job.JOB_ID
+GROUP BY Cargo
+ORDER BY AVG(emp.SALARY), Cargo;
