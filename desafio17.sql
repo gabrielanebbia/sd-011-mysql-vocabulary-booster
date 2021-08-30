5eb9a63ba1084d371327aa2c479e957f7841e204
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-08-29 21:38:13 -0300

    Corrige Climate

--- desafio17.sql
+++ desafio17.sql
@@ -1,12 +1,11 @@
-SELECT * FROM orders; -- OrderDate
 USE w3schools;
 DELIMITER $$
 
 CREATE TRIGGER data_atual_insert
-	BEFORE INSERT ON orders
-	FOR EACH ROW
+BEFORE INSERT ON orders
+FOR EACH ROW
 BEGIN
-     SET NEW.OrderDate = NOW();
+      SET NEW.OrderDate = NOW();
 END $$
 
 DELIMITER ;
