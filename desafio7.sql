c080da11bc96f719d5153c3102e9e75a55dc10f1
guinhoanjos <iago.anjos@hotmail.com>
2021-08-30 13:47:28 -0300

    Create desafio7.sql

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT distinct  UCASE(CONCAT(emp.first_name, ' ', emp.last_name)) AS `Nome completo`,
+his.START_DATE AS 'Data de início',
+emp.SALARY
+FROM hr.employees AS emp
+INNER JOIN hr.job_history AS his
+ON emp.EMPLOYEE_ID =  his.EMPLOYEE_ID
+WHERE MONTH(his.START_DATE) < 4
+ORDER BY CONCAT(emp.first_name, ' ', emp.last_name) ASC, his.START_DATE ASC;
