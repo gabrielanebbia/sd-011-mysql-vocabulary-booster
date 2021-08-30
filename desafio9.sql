a6cc62cf4858cde090ec9382af7c57d929635af5
heyset <c.inaciomatheus@gmail.com>
2021-08-30 12:10:13 -0300

    Adiciona desafios 8 e 9

---
+++ desafio9.sql
@@ -0,0 +1,22 @@
+SHOW COLUMNS FROM employees; -- FirstName, LastName, EmployeeID
+
+SHOW TABLES;
+
+SHOW COLUMNS FROM orders;
+
+SELECT * FROM orders;
+
+SELECT * FROM customers WHERE ContactName LIKE '%Anne%';
+
+SELECT
+  CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
+  COUNT(e.EmployeeID) AS 'Total de pedidos'
+FROM
+  employees AS e
+INNER JOIN
+  orders AS o
+  ON o.EmployeeID = e.EmployeeID
+GROUP BY
+  e.EmployeeID
+ORDER BY
+  2 ASC;
