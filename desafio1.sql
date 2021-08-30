631ba46793035a8e057879637aa56f20fa32d8bd
Vitor <vitorguima@gmail.com>
2021-08-30 14:00:13 -0300

    req 1 finalizado

---
+++ desafio1.sql
@@ -0,0 +1,9 @@
+SELECT
+C.COUNTRY_NAME,
+CASE
+WHEN R.REGION_NAME = 'Europe' THEN 'incluído'
+WHEN R.REGION_NAME <> 'Europe' THEN 'não incluído'
+END AS `Status Inclusão`
+FROM hr.countries AS C
+LEFT JOIN hr.regions AS R
+ON C.REGION_ID = R.REGION_ID;
