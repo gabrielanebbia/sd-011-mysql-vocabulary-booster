7d6df0815b53a3f2202ba249350a7897910f98ce
Laura Gusmao <laurargus@gmail.com>
2021-08-30 17:48:32 -0300

    Code Climate

--- desafio4.sql
+++ desafio4.sql
@@ -2,11 +2,11 @@ SELECT
 j.JOB_TITLE AS 'Cargo',
 round(AVG (e.SALARY), 2) AS 'Média salarial',
 CASE
-	WHEN round(AVG (e.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
-	WHEN round(AVG (e.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
-	WHEN round(AVG (e.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
-	WHEN round(AVG (e.SALARY), 2) > 10500 THEN 'CEO'
-	ELSE 'Não especificado'
+WHEN round(AVG (e.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+WHEN round(AVG (e.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+WHEN round(AVG (e.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+WHEN round(AVG (e.SALARY), 2) > 10500 THEN 'CEO'
+ELSE 'Não especificado'
 END AS 'Senioridade'
 FROM hr.jobs AS j
 INNER JOIN hr.employees AS e
