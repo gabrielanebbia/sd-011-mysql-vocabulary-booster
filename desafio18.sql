3d34f6762c44d7a2e13790429e6164bd2e2a5dda
Vinicius <viniciusfod@gmail.com>
2021-08-30 11:16:06 -0300

    req18-fix

--- desafio18.sql
+++ desafio18.sql
@@ -1,8 +1,8 @@
 SELECT
 CONCAT(e.FIRST_NAME , ' ',  e.LAST_NAME) AS 'Nome completo',
-DATE_FORMAT(j.START_DATE,'%d%m%Y')  AS 'Data de início',
-DATE_FORMAT(j.END_DATE,'%d%m%Y')  AS 'Data de rescisão',
-ROUND(TIMESTAMPDIFF(MONTH, j.START_DATE, j.END_DATE)/12,2) AS 'Anos trabalhado'
+DATE_FORMAT(j.START_DATE,'%d/%m/%Y')  AS 'Data de início',
+DATE_FORMAT(j.END_DATE,'%d/%m/%Y')  AS 'Data de rescisão',
+ROUND(TIMESTAMPDIFF(DAY, j.START_DATE, j.END_DATE)/365,2) AS 'Anos trabalhado'
 FROM hr.employees e
 INNER JOIN hr.job_history j ON j.EMPLOYEE_ID = e.EMPLOYEE_ID
 ORDER BY CONCAT(e.FIRST_NAME , ' ',  e.LAST_NAME) ASC,
