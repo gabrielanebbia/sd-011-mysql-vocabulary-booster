836466291d08a8abd0863ec8f59dcd0d60e50ec6
Vinicius <viniciusfod@gmail.com>
2021-08-30 10:17:47 -0300

    req1

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,4 @@
-SELECT 
-	c.COUNTRY_NAME AS País,
-    CASE r.REGION_NAME WHEN "Europe" THEN "incluído" ELSE "não incluído" END AS "Status Inclusão"
-FROM hr.countries c
-INNER JOIN hr.regions r ON c.REGION_ID = r.REGION_ID
-ORDER BY c.COUNTRY_NAME ASC;
+SELECT COUNTRY_NAME AS 'País',
+IF(REGION_ID=1, 'incluído', 'não incluído') AS 'Status Inclusão' 
+FROM hr.countries
+ORDER BY COUNTRY_NAME ASC;
