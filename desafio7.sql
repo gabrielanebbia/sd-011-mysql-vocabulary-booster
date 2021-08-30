b41f4e8814473922d39b2c26aef0be960498c958
Vitor <vitorguima@gmail.com>
2021-08-30 16:35:16 -0300

    req 7 finalizado

---
+++ desafio7.sql
@@ -0,0 +1,10 @@
+SELECT
+E.EMPLOYEE_ID,	
+UPPER(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) AS `Nome completo`,
+JH.START_DATE as `Data de início`,
+E.SALARY AS `Salário`
+FROM hr.employees AS E
+LEFT JOIN hr.job_history AS JH
+ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+WHERE MONTH(JH.START_DATE) IN (1, 2, 3)
+ORDER BY `Nome completo`, DATE(JH.START_DATE);
