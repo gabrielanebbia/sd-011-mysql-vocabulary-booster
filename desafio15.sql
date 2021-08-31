c7f87d65aff588ab37b382fa95a4b54d179dad5b
Amandha W. Barok <amandha.barok@gmail.com>
2021-08-30 21:39:28 -0700

    PELO AMOR DE DEUS VAI

--- desafio15.sql
+++ desafio15.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN nomeDoCargo VARCHAR(100))
 BEGIN 
-SELECT ROUND(AVG(e.SALARY), 2) AS 'Média Salarial'
+SELECT ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
 FROM hr.employees AS e
 INNER JOIN hr.jobs AS j ON e.JOB_ID = j.JOB_ID
 WHERE j.JOB_TITLE = nomeDoCargo;
