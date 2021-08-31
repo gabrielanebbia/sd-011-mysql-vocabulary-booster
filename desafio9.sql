d4ccdc9b64dd8e40b942f9f13aab4d7425e90064
Gustavo Lemes <gustavolemes997@gmail.com>
2021-08-31 19:23:56 -0300

    Requisito 9

---
+++ desafio9.sql
@@ -0,0 +1,8 @@
+SELECT
+	CONCAT(we.FirstName, ' ', we.LastName ) AS 'Nome completo',
+    COUNT(we.EmployeeID) AS 'Total de pedidos'
+FROM w3schools.employees AS we
+INNER JOIN w3schools.orders AS wo
+ON we.EmployeeID = wo.EmployeeID
+GROUP BY CONCAT(we.FirstName, ' ', we.LastName)
+ORDER BY COUNT(we.EmployeeID);
