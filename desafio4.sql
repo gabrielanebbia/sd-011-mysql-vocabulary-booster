a6311c832990e25018dd3e9918772158d885c466
Lais Simenikim <l.simenikim@gmail.com>
2021-08-31 14:32:45 -0300

    Ajust challenge 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,13 +1,13 @@
 SELECT JOB_TITLE AS Cargo, `Média Salarial`,
 CASE
-WHEN `Média salarial` BETWEEN 2000 AND 5800 THEN 'Junior'
+WHEN `Média salarial` BETWEEN 2000 AND 5800 THEN 'Júnior'
 WHEN `Média salarial` BETWEEN 5801 AND 7500 THEN 'Pleno'
 WHEN `Média salarial` BETWEEN 7501 AND 10500 THEN 'Sênior'
 ELSE 'CEO'
 END AS Senioridade
 FROM (
 SELECT JOB_ID,
-ROUND(AVG(SALARY), 2) as 'Média salarial'
+ROUND(AVG(SALARY), 2) AS 'Média salarial'
 FROM hr.employees
 GROUP BY JOB_ID
 )
