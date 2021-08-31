85dac65282ddb8206189607a7ab5931f583b24d5
Gustavo Lemes <gustavolemes997@gmail.com>
2021-08-31 19:36:03 -0300

    Requisito 10

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT
+	wp.ProductName AS Produto,
+	MIN(wod.Quantity) AS 'Mínima',
+    MAX(wod.Quantity) AS 'Máxima',
+    ROUND(AVG(wod.Quantity), 2) AS 'Média'
+FROM w3schools.products AS wp
+INNER JOIN w3schools.order_details AS wod
+ON wp.ProductID = wod.ProductID
+GROUP BY wp.ProductName HAVING ROUND(AVG(wod.Quantity), 2)  > 20
+ORDER BY AVG(wod.Quantity);
