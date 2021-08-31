aeeb2e5e1d713d3b5225065ba2e754b3eda60ed4
Gustavo Lemes <gustavolemes997@gmail.com>
2021-08-31 20:44:28 -0300

    Requisito 14

---
+++ desafio14.sql
@@ -0,0 +1,9 @@
+SELECT
+	DISTINCT Country AS País
+FROM (
+	SELECT Country from w3schools.suppliers
+    UNION ALL
+    SELECT Country from w3schools.customers
+) AS Countries
+ORDER BY Country
+LIMIT 5;
