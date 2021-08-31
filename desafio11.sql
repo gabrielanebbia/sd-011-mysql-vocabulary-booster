02f915e139c2a15824e043fb54378844aaf3804b
Gustavo Lemes <gustavolemes997@gmail.com>
2021-08-31 20:32:57 -0300

    Requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,14 @@
+SELECT 
+	wc3.ContactName as 'Nome',
+    countryData.País,
+    countryData.Counter AS 'Número de compatriotas'
+FROM	(
+		SELECT DISTINCT
+			Country AS País,
+			COUNT(Country)-1 AS Counter
+            from w3schools.customers
+		GROUP BY Country
+    ) AS countryData
+INNER JOIN w3schools.customers AS wc3
+ON wc3.Country = countryData.País
+ORDER BY wc3.ContactName;
