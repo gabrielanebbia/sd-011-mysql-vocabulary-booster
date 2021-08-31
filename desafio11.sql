afaba019e03c967f4b2cbd61515425afceab1714
Gabriel Castro <kastro.gabriel17@gmail.com>
2021-08-31 18:31:51 -0400

    resolved req: 11

---
+++ desafio11.sql
@@ -0,0 +1,8 @@
+SELECT 
+	cm.ContactName AS 'Nome',
+  cm.Country AS 'País',
+  COUNT(cmnew.CustomerID) AS 'Número de compatriotas'
+FROM w3schools.customers AS cm, w3schools.customers AS cmnew
+WHERE cm.Country = cmnew.Country AND cm.CustomerID != cmnew.CustomerID
+GROUP BY Nome, `País`
+ORDER BY Nome;
