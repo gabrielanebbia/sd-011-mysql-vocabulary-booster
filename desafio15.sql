8f3a585d813a126dff49c1e140887623ec180a9d
Amandha W. Barok <amandha.barok@gmail.com>
2021-08-30 21:34:40 -0700

    fixing

--- desafio15.sql
+++ desafio15.sql
@@ -1,9 +1,9 @@
 USE hr;
 DELIMITER $$
 
-CREATE PROCEDURE buscar_media_por_cargo(IN nomeDoCargo VARCHAR(100), OUT mediaSalarialDoCargo DECIMAL(10,2))
+CREATE PROCEDURE buscar_media_por_cargo(IN nomeDoCargo VARCHAR(100))
 BEGIN 
-SELECT ROUND(AVG(e.SALARY), 2) AS 'Média Salarial' INTO mediaSalarialDoCargo 
+SELECT ROUND(AVG(e.SALARY), 2) AS 'Média Salarial'
 FROM hr.employees AS e
 INNER JOIN hr.jobs AS j ON e.JOB_ID = j.JOB_ID
 WHERE j.JOB_TITLE = nomeDoCargo;
