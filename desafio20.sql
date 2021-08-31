10c1665f1e61301cb4bdfdc3c78acd556556697e
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-08-31 00:39:52 -0300

    Desafio 20

---
+++ desafio20.sql
@@ -0,0 +1,18 @@
+USE hr;
+DELIMITER $$
+CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(50))
+BEGIN
+SELECT
+CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS 'Nome completo',
+dep.DEPARTMENT_NAME AS 'Departamento',
+j.JOB_TITLE AS 'Cargo'
+FROM hr.job_history as `history`
+INNER JOIN hr.employees AS emp
+ON `history`.EMPLOYEE_ID = emp.EMPLOYEE_ID
+INNER JOIN hr.departments AS dep
+ON dep.DEPARTMENT_ID = `history`.DEPARTMENT_ID
+INNER JOIN hr.jobs AS j
+ON j.JOB_ID = `history`.JOB_ID
+WHERE emp.EMAIL = email;
+END $$
+DELIMITER ;
