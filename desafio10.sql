bd65a6741fe4ebe2f03dd042b38bf4a7cda7aedf
Felipe Buso <felipe_buso@hotmail.com>
2021-08-31 09:18:29 -0300

    correção item 10

--- desafio10.sql
+++ desafio10.sql
@@ -2,26 +2,26 @@ SELECT P.ProductName AS 'Produto',
 (
 SELECT MIN(OD.Quantity)
 FROM w3schools.order_details OD
-WHERE P.ProductID = ProductID
+WHERE P.ProductID = OD.ProductID
 ) AS 'Mínima',
 (
 SELECT MAX(OD.Quantity)
 FROM w3schools.order_details OD
-WHERE P.ProductID = ProductID
+WHERE P.ProductID = OD.ProductID
 ) AS 'Máxima',
 (
 SELECT ROUND(AVG(OD.Quantity), 2)
 FROM w3schools.order_details OD
-WHERE P.ProductID = ProductID
+WHERE P.ProductID = OD.ProductID
 ) AS 'Média'
 FROM w3schools.products P
 WHERE (
 SELECT AVG(OD.Quantity)
 FROM w3schools.order_details OD
-WHERE P.ProductID = ProductID
+WHERE P.ProductID = OD.ProductID
 ) > 20
 ORDER BY (
 SELECT AVG(OD.Quantity)
 FROM w3schools.order_details OD
-WHERE P.ProductID = ProductID
+WHERE P.ProductID = OD.ProductID
 );
