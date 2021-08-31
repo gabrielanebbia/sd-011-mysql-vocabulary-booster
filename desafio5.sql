41fb68b7a5397991c6f0776f69b948aafb1c0928
Lais Simenikim <l.simenikim@gmail.com>
2021-08-31 15:41:31 -0300

    Ajust challenge 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,7 @@
 SELECT JOB_TITLE AS Cargo,
-(MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
-ROUND(AVG(MIN_SALARY), 2) AS 'Média mínima mensal',
-ROUND(AVG(MAX_SALARY), 2) AS 'Média máxima mensal'
+((MAX_SALARY / 12) - (MIN_SALARY / 12)) AS 'Variação Salarial',
+ROUND(AVG(MIN_SALARY / 12), 2) AS 'Média mínima mensal',
+ROUND(AVG(MAX_SALARY / 12), 2) AS 'Média máxima mensal'
 FROM hr.jobs
 GROUP BY Cargo
 ORDER BY `Variação Salarial`, Cargo;
