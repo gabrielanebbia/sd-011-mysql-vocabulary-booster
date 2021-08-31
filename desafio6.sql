de2a83180bed7ad2310911c3028154a57d105b8f
nandovbr <nandovbr15@hotmail.com>
2021-08-31 11:05:44 -0300

    ajusta requisitos 5 e 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,8 @@
 SELECT CONCAT(e.FIRST_NAME, '', e.LAST_NAME) AS 'Nome completo',
 j.JOB_TITLE AS 'Cargo',
 jh.START_DATE AS 'Data de início do cargo',
-d.DEPARTMENT_NAME AS 'Departamento' FROM hr.job_history jh
+d.DEPARTMENT_NAME AS 'Departamento'
+FROM hr.job_history jh
 INNER JOIN hr.jobs j
 ON jh.JOB_ID = j.JOB_ID
 INNER JOIN hr.departments d
