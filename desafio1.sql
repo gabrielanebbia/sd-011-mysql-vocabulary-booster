0f61b0969cb4d97bc063cbae791ad88fe4af5767
DADA <davimarque2000@gmail.com>
2021-08-31 13:34:31 -0300

    Desafio 1 ordenando tabela

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,1 @@
-SELECT COUNTRY_NAME AS País, IF(REGION_ID = 1, "incluído", "não incluído" ) AS "Status Inclusão" from countries;
+SELECT COUNTRY_NAME AS País, IF(REGION_ID = 1, "incluído", "não incluído" ) AS "Status Inclusão" from countries ORDER BY COUNTRY_NAME;
