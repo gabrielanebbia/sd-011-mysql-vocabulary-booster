8a343a5a41b4269a27be0bd20eef419174ef2e7a
DiegoCS777 <diegomethal@gmail.com>
2021-08-30 23:58:00 -0300

    Correção  Desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,6 @@
-SELECT CONCAT(e.FIRST_NAME,'',e.LAST_NAME) AS `Nome completo`,
-j.JOB_TITLE AS Cargo,
-jh.START_DATE AS `Data de início do cargo`,
+SELECT CONCAT(e.FIRST_NAME,'',e.LAST_NAME) AS 'Nome completo',
+j.JOB_TITLE AS 'Cargo',
+jh.START_DATE AS 'Data de início do cargo',
 d.DEPARTMENT_NAME AS Departamento
 FROM
 hr.job_history AS jh
