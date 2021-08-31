1989f3fd8215c68712755d7914e1ba041fb98793
Amandha W. Barok <amandha.barok@gmail.com>
2021-08-30 21:05:57 -0700

    Finishing the 10th requiriment

--- desafio10.sql
+++ desafio10.sql
@@ -2,7 +2,7 @@ SELECT
 p.ProductName AS 'Produto',
 MIN(o.Quantity) AS 'Mínima',
 MAX(o.Quantity) AS 'Máxima',
-AVG(o.Quantity) AS 'Média'
+ROUND(AVG(o.Quantity),2) AS 'Média'
 FROM w3schools.products AS p
 INNER JOIN w3schools.order_details AS o ON p.ProductID = o.ProductID
 GROUP BY p.ProductID
