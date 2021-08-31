e8186d6b1f48d8c81bfbad2eabae0d0c55206759
Amandha W. Barok <amandha.barok@gmail.com>
2021-08-30 21:03:38 -0700

    6th requiriment

--- desafio6.sql
+++ desafio6.sql
@@ -6,5 +6,5 @@ d.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.job_history AS jh
 INNER JOIN hr.employees AS e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
 INNER JOIN hr.departments AS d ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
-INNER JOIN hr.jobs AS j ON e.JOB_ID = j.JOB_ID
+INNER JOIN hr.jobs AS j ON jh.JOB_ID = j.JOB_ID
 ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC, JOB_TITLE;
