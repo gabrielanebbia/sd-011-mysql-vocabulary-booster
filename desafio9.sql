8a970f515186c228cfa086b93807de5f3b0eab7d
MatheusAraujoDev <matheusaraujo.dev@gmail.com>
2021-08-30 21:18:17 -0300

    Corrige letra maiuscula req 09

--- desafio9.sql
+++ desafio9.sql
@@ -2,7 +2,7 @@
 -- SELECT * FROM w3schools.orders;
 
 SELECT
-CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome Completo',
+CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
 COUNT(o.OrderID) AS 'Total de pedidos'
 FROM w3schools.employees AS e
 INNER JOIN w3schools.orders AS o ON e.EmployeeID = o.EmployeeID
