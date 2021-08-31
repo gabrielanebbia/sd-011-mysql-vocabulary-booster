bab91283dab935e107f7ca1333bbc13cfbef2396
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-08-31 02:15:49 -0300

    requisito 10 concluido

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT p.productName AS Produto,
+MIN(od.quantity) AS 'Mínima',
+MAX(od.quantity) AS 'Máxima',
+ROUND(AVG(od.quantity), 2) AS 'Média'
+FROM w3schools.order_details AS od
+INNER JOIN w3schools.products AS p
+ON od.ProductID = p.ProductID
+GROUP BY Produto
+HAVING `Média` > 20
+ORDER BY `Média`, Produto;
