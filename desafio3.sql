2eb899c462caea64291897c3d47bfcf7cdc138e7
DADA <davimarque2000@gmail.com>
2021-08-31 14:45:26 -0300

    code climate

--- desafio3.sql
+++ desafio3.sql
@@ -1,4 +1,4 @@
   select JOB_TITLE AS 'Cargo' , 
   (MAX_SALARY - MIN_SALARY)
-   AS 'Diferença entre salários máximo e mínimo' from jobs
-   ORDER BY (MAX_SALARY - MIN_SALARY), JOB_TITLE; 
+    AS 'Diferença entre salários máximo e mínimo' from jobs
+    ORDER BY (MAX_SALARY - MIN_SALARY), JOB_TITLE; 
