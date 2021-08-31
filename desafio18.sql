ee1c7dfe619ead1612ff7a263928f4a1725aa9a6
Daniel Leon <daniel.radialista@gmail.com>
2021-08-31 14:06:27 -0300

    desafio 18, pronto e passando nos testes

---
+++ desafio18.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
+DATE_FORMAT(jh.START_DATE, '%d/%m/%Y') AS 'Data de início',
+DATE_FORMAT(jh.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
+ROUND(DATEDIFF(jh.end_date, jh.start_date) / 365, 2) AS 'Anos trabalhados'
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS jh
+ON e.employee_ID = jh.employee_ID
+ORDER BY `Nome completo`, `Anos trabalhados`;
