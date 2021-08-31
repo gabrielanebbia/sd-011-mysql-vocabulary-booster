cde1dbda436b288584e72ab2cedd4b605dcf83a3
Felipe Buso <felipe_buso@hotmail.com>
2021-08-31 10:13:29 -0300

    correção requisito 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,9 +1,10 @@
 SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome completo',
-CONCAT(DAY(JB.START_DATE), '/', LPAD(MONTH(JB.START_DATE), 2, '0'), '/', YEAR(JB.START_DATE)) AS 'Data de início',
-CONCAT(DAY(JB.END_DATE), '/', LPAD(MONTH(JB.END_DATE), 2, '0'), '/', YEAR(JB.END_DATE)) AS 'Data de rescisão',
-ROUND((DATEDIFF(JB.END_DATE, START_DATE) / 365), 2) AS 'Anos trabalhados'
+CONCAT(LPAD(DAY(JB.START_DATE), 2, '0'), '/', LPAD(MONTH(JB.START_DATE), 2, '0'), '/', YEAR(JB.START_DATE)) AS 'Data de início',
+CONCAT(LPAD(DAY(JB.END_DATE), 2, '0'), '/', LPAD(MONTH(JB.END_DATE), 2, '0'), '/', YEAR(JB.END_DATE)) AS 'Data de rescisão',
+ROUND((DATEDIFF(JB.END_DATE, JB.START_DATE) / 365), 2) AS 'Anos trabalhados'
 FROM hr.employees E
 RIGHT JOIN hr.job_history JB
 ON JB.EMPLOYEE_ID = E.EMPLOYEE_ID
+WHERE ROUND((DATEDIFF(JB.END_DATE, JB.START_DATE) / 365), 2) IS NOT NULL
 ORDER BY `Nome completo`,
 `Anos trabalhados`;
