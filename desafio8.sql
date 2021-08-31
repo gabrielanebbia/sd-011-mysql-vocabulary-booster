746c3fb47ecd1891e1d92e4edbbfa172865d49e1
nandovbr <nandovbr15@hotmail.com>
2021-08-31 19:27:55 -0300

    ajusta erro de escrita requisito 8 e adiciona requisitos 9 e 10

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,5 @@
 SELECT c.ContactName AS 'Nome de contato',
-s.ShipperName AS 'Empresa que faz o envio',
+s.ShipperName AS 'Empresa que fez o envio',
 o.OrderDate AS 'Data do pedido'
 FROM w3schools.orders o
 INNER JOIN w3schools.customers c
