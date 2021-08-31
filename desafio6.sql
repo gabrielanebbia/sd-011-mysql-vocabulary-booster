e4ccb651d061fcd36f61e3688c43680178f35e72
DiegoCS777 <diegomethal@gmail.com>
2021-08-30 23:38:41 -0300

    Correção  Desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT(e.FIRST_NAME,'',e.LAST_NAME) AS `Nome completo`,
 j.JOB_TITLE AS Cargo,
 jh.START_DATE AS `Data de início do cargo`,
-departments.DEPARTMENT_NAME AS Departamento
+d.DEPARTMENT_NAME AS Departamento
 FROM
 hr.job_history AS jh
 INNER JOIN
