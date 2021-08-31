4a1cd624ccb97391529568be1f4d214fbc916bc8
Vitor <vitorguima@gmail.com>
2021-08-31 10:26:07 -0300

    requisito 15 finalizado

---
+++ desafio15.sql
@@ -0,0 +1,16 @@
+USE hr;
+
+DELIMITER $$ 
+CREATE PROCEDURE buscar_media_por_cargo(IN nomeCargo VARCHAR(100))
+BEGIN
+SELECT
+ROUND(AVG(E.SALARY), 2) AS `Média Salarial`
+FROM hr.employees AS E
+LEFT JOIN hr.jobs as J
+ON E.JOB_ID = J.JOB_ID
+WHERE J.JOB_TITLE = nomeCargo
+GROUP BY E.JOB_ID;
+END $$
+DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');
