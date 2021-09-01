497a8dd26518144c3b6f12b3b31ef773d4321569
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-09-01 19:53:57 -0300

    resolvendo erro do requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -4,4 +4,5 @@ COUNT(*) - 1 AS 'Número de compatriotas'
 FROM w3schools.customers AS t1, w3schools.customers AS t2
 WHERE t1.country = t2.country
 GROUP BY Nome
+HAVING `Número de compatriotas` > 0
 ORDER BY Nome;
