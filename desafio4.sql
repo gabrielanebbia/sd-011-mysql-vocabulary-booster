948f661f18f6048b8171448049db1461d4ef2094
nsilva90 <nikolas.silva1990@gmail.com>
2021-09-01 11:48:17 -0300

    Desafio 4 corrigido

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,13 @@
-SELECT job_title AS 'Cargo',
-ROUND(((max_salary + min_salary) / 2), 2) AS 'Média salarial',
+SELECT t1.job_title AS 'Cargo',
+ROUND(AVG(t2.salary), 2) AS 'Média salarial',
 CASE
-WHEN ((max_salary + min_salary) / 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
-WHEN ((max_salary + min_salary) / 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
-WHEN ((max_salary + min_salary) / 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
-WHEN ((max_salary + min_salary) / 2) > 10500 THEN 'CEO'
+WHEN (AVG(t2.salary)) BETWEEN 2000 AND 5800 THEN 'Júnior'
+WHEN (AVG(t2.salary)) BETWEEN 5801 AND 7500 THEN 'Pleno'
+WHEN (AVG(t2.salary)) BETWEEN 7501 AND 10500 THEN 'Sênior'
+WHEN (AVG(t2.salary)) > 10500 THEN 'CEO'
 END AS 'Senioridade'
-FROM hr.jobs
-ORDER BY ((max_salary + min_salary) / 2) ASC, job_title ASC;
+FROM jobs AS t1
+INNER JOIN employees AS t2
+ON t1.job_id = t2.job_id
+GROUP BY t1.job_title
+ORDER BY (AVG(t2.salary)) ASC, t1.job_title ASC;
