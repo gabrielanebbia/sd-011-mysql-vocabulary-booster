5f8398c60756350ca250805449b729810d713174
Henrique_Pozzolini <hpmoreira05@gmail.com>
2021-09-01 20:29:09 -0300

    Req15_6

--- desafio15.sql
+++ desafio15.sql
@@ -1,13 +1,13 @@
 USE hr;
 DELIMITER $$
 
-CREATE PROCEDURE busca_media_por_cargo(IN cargo VARCHAR(50))
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
 BEGIN 
 SELECT ROUND(AVG(e.SALARY), 2) AS 'Média Salarial'
-FROM hr.employees AS e
+FROM employees AS e
 INNER JOIN jobs AS j
 ON e.JOB_ID = j.JOB_ID
-WHERE j.JOB_TITLE LIKE CONCAT('%', job_query, '%');
+WHERE j.JOB_TITLE LIKE cargo;
 END $$ 
 
 DELIMITER ; 
