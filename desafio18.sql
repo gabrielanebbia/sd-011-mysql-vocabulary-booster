103db9309f69f310324a27c708079483f55931ac
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-09-01 18:29:52 -0300

    task 18 1.4

--- desafio18.sql
+++ desafio18.sql
@@ -5,4 +5,4 @@ ROUND(DATEDIFF(END_DATE,START_DATE)/365, 2) AS 'Anos trabalhados'
 FROM hr.job_history AS jbh
 INNER JOIN hr.employees AS emp
 ON jbh.EMPLOYEE_ID = emp.EMPLOYEE_ID
-ORDER BY 'Nome completo' ASC, 'Anos trabalhados' ASC;
+ORDER BY 'Nome completo', 'Anos trabalhados' ASC;
