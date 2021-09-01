bd4f24f5ee588547f6e19a61d1270b62db25c559
nsilva90 <nikolas.silva1990@gmail.com>
2021-09-01 09:37:58 -0300

    Desafio 1 corrigido ordenamento alfabético

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,2 @@
-SELECT country_name as 'País', IF (region_id = 1, 'incluído', 'não incluído') as 'Status Inclusão' FROM hr.countries;
+SELECT country_name as 'País', IF (region_id = 1, 'incluído', 'não incluído') as 'Status Inclusão' FROM hr.countries
+ORDER BY country_name ASC;
