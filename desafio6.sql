7b2b95bc08cd58356484e4cbba6267fec29fc0b6
Diego Figueirêdo <8kball@gmail.com>
2021-09-01 16:30:47 -0300

    Desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,16 @@
+SELECT
+	CONCAT(e.first_name, ' ', e.last_name) AS `Nome Completo`,
+	j.job_title AS `Cargo`,
+	h.start_date AS `Data de início do cargo`,
+	d.department_name AS `Departamento`
+FROM
+	hr.employees AS e
+INNER JOIN
+	hr.job_history AS h ON h.employee_id = e.employee_id
+INNER JOIN
+	hr.jobs AS j ON j.job_id = h.job_id
+INNER JOIN
+	hr.departments AS d ON d.department_id = h.department_id
+ORDER BY
+	`Nome Completo` DESC,
+	`Cargo`;
