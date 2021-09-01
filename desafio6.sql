a9d68b10eb85c47c2db9431d93dd8c2829b61469
Alan Gonçalves <vekio.soft@gmail.com>
2021-09-01 15:53:37 -0300

    não aguento maisgit add .! req6

--- desafio6.sql
+++ desafio6.sql
@@ -1,12 +1,15 @@
-SELECT CONCAT(n.FIRST_NAME, ' ', n.LAST_NAME) AS 'Nome completo',
+SELECT 
+CONCAT(n.FIRST_NAME, ' ', n.LAST_NAME) AS 'Nome completo',
 c.JOB_TITLE AS 'Cargo',
 h.START_DATE AS 'Data de início do cargo',
 d.DEPARTMENT_NAME AS 'Departamento'
-FROM hr.jobs AS c
+FROM 
+hr.jobs AS c
 INNER JOIN hr.employees AS n
 ON c.JOB_ID = n.JOB_ID
 INNER JOIN hr.departments AS d
 ON n.DEPARTMENT_ID = d.DEPARTMENT_ID
 INNER JOIN hr.job_history AS h
 ON n.EMPLOYEE_ID = h.EMPLOYEE_ID
-ORDER BY CONCAT(n.FIRST_NAME, ' ', n.LAST_NAME) DESC, c.JOB_TITLE ASC;
+ORDER BY
+ CONCAT(n.FIRST_NAME, ' ', n.LAST_NAME) DESC, c.JOB_TITLE ASC;
