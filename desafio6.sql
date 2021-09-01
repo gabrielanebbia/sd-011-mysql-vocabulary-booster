46533fe2417e22c7a5b642e622f8446f5d0d17f5
DADA <davimarque2000@gmail.com>
2021-09-01 13:12:54 -0300

    Desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,14 @@
+     select 
+  concat(FIRST_NAME, ' ', LAST_NAME) AS "Nome completo",
+  JOB_TITLE AS 'Cargo',
+  START_DATE AS "Data de início",
+  DEPARTMENT_NAME AS 'Departamento'
+    FROM job_history AS jh
+    INNER JOIN jobs AS j
+    ON j.JOB_ID = jh.JOB_ID
+    INNER JOIN departments AS d
+    ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
+    INNER JOIN  employees AS e
+    ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID 
+    order by concat(FIRST_NAME, ' ', LAST_NAME) DESC, JOB_TITLE;
+    
