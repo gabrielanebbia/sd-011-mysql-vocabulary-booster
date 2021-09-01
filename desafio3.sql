65a6b156ac509a99ef2b6de5973ff5aedb0bb790
Diego Figueirêdo <8kball@gmail.com>
2021-09-01 00:38:10 -0300

    Desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,8 @@
+SELECT
+  job_title AS `Cargo`,
+	(max_salary - min_salary) AS `Diferença entre salários máximo e mínimo`
+FROM
+	jobs
+ORDER BY
+	`Diferença entre salários máximo e mínimo`,
+	`Cargo`;
