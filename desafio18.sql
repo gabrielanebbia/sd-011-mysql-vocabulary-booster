5f7773278b336d350ee412640462e7a68f44f62c
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-09-01 18:13:34 -0300

    task 18 1.3

--- desafio18.sql
+++ desafio18.sql
@@ -1,14 +1,8 @@
-SELECT CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME)
-AS 'Nome completo',
-DATE_FORMAT(jbh.START_DATE, '%d/%m/%Y')
-AS 'Data de início',
-DATE_FORMAT(jbh.END_DATE, '%d/%m/%Y')
-AS 'Data de rescisão',
-ROUND(DATEDIFF(END_DATE,START_DATE)/365, 2) 
-AS 'Anos trabalhados'
-FROM hr.job_history
-AS jbh -- job history
-INNER JOIN hr.employees
-AS emp -- employees
+SELECT CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS 'Nome completo',
+DATE_FORMAT(jbh.START_DATE, '%d/%m/%Y') AS 'Data de início',
+DATE_FORMAT(jbh.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
+ROUND(DATEDIFF(END_DATE,START_DATE)/365, 2) AS 'Anos trabalhados'
+FROM hr.job_history AS jbh
+INNER JOIN hr.employees AS emp
 ON jbh.EMPLOYEE_ID = emp.EMPLOYEE_ID
-ORDER BY 'Nome completo', 'Anos trabalhados' ASC;
+ORDER BY 'Nome completo' ASC, 'Anos trabalhados' ASC;
