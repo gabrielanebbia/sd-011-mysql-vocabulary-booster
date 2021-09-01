6d8eb2144fd7d693c406b3a861ed1bdc95fea933
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-09-01 18:59:59 -0300

    Adicionando requisito 7

--- desafio6.sql
+++ desafio6.sql
@@ -1,16 +1,7 @@
--- SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
--- j.JOB_TITLE AS Cargo,
--- e.HIRE_DATE AS 'Data de início do cargo',
--- d.DEPARTMENT_NAME AS 'Departamento'
--- FROM employees AS e
--- INNER JOIN jobs AS j
--- ON e.JOB_ID = j.JOB_ID
--- INNER JOIN departments AS d
--- ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
--- ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC, j.JOB_TITLE;
+
 SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
 j.JOB_TITLE AS Cargo,
-jh.START_DATE,
+jh.START_DATE AS 'Data de início do cargo',
 d.DEPARTMENT_NAME AS 'Departamento'
 FROM job_history AS jh
 INNER JOIN employees AS e
