dc230053d90e434f3cefaf004e755e6daeb5ba27
nsilva90 <nikolas.silva1990@gmail.com>
2021-09-02 08:27:23 -0300

    Desafio 6 concluído + Identação do Desafio 2 alterada

--- desafio6.sql
+++ desafio6.sql
@@ -1,12 +1,12 @@
-SELECT CONCAT(t1.first_name, ' ', t1.last_name) AS 'Nome completo',
-t2.job_title AS 'Cargo',
-t4.start_date AS 'Data de início do cargo',
-t3.department_name AS 'Departamento'
-FROM employees AS t1
-INNER JOIN job_history AS t4
-ON t1.employee_id = t4.employee_id
-INNER JOIN jobs AS t2
-ON t1.job_id = t2.job_id
-INNER JOIN departments AS t3
-ON t1.department_id = t3.department_id
-ORDER BY CONCAT(t1.first_name, ' ', t1.last_name) DESC, t2.job_title ASC;
+SELECT CONCAT(t2.first_name, ' ', t2.last_name) AS 'Nome completo',
+t3.job_title AS 'Cargo',
+t1.start_date AS 'Data de início do cargo',
+t4.department_name AS 'Departamento'
+FROM job_history AS t1
+INNER JOIN employees AS t2
+ON t1.employee_id = t2.employee_id
+INNER JOIN jobs AS t3
+ON t1.job_id = t3.job_id
+INNER JOIN departments AS t4
+ON t1.department_id = t4.department_id
+ORDER BY CONCAT(t2.first_name, ' ', t2.last_name) DESC, t3.job_title ASC;
