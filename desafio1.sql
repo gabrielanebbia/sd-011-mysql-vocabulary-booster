ac0c0d90ad80c8da3060203ceff9cc696cff3f1f
Maria Lisboa <marialisboa1515@gmail.com>
2021-09-01 23:02:36 -0300

    desafio1

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,14 @@
 SELECT
+COUNTRY_NAME AS 'País',
+(
+SELECT
+IF(regions .REGION_NAME = 'Europe', 'incluído', 'não incluído')
+FROM
+regions 
+WHERE
+regions .REGION_ID = countries.REGION_ID
+) AS 'Status Inclusão'
+FROM
+countries 
+ORDER BY
+COUNTRY_NAME ASC;
