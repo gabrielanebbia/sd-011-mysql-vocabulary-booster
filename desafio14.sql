dccf75ef3b3e9ee61a2c8755b659834ecfbc37fb
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-09-02 02:10:02 -0300

    resolvendo erro do requisito 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,4 +1,5 @@
-SELECT Country AS 'País' FROM w3schools.customers
-GROUP BY Country
-ORDER BY Country
+(SELECT Country AS 'País' FROM w3schools.customers)
+UNION
+(SELECT Country AS 'País' FROM w3schools.suppliers)
+ORDER BY `País`
 LIMIT 5;
