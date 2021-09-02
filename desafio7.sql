9779543d1acd7b25ca423d45322e847a4f361461
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-09-02 12:13:37 -0300

    arrumem esses testes

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
 SELECT UCASE(CONCAT(first_name,' ', last_name)) AS 'Nome Completo', DATE(start_date) AS 'Data de início',
-salary AS Salário
+salary AS 'Salário'
 FROM hr.job_history AS JB
 INNER JOIN
 hr.employees AS E ON JB.EMPLOYEE_ID = E.EMPLOYEE_ID
