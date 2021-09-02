8af918b783fdf2a0a4c8871221678b917a1f34b1
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-09-02 17:39:10 -0300

    desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,7 @@
 SELECT country_name AS País,
-	CASE 
-		WHEN region_id = 1 THEN 'inlcuido'
-	ELSE 'Não incluido'
+CASE
+  WHEN region_id = 1 THEN 'inlcuido'
+  ELSE 'Não incluido'
 END AS 'Status'
 FROM hr.countries
 ORDER BY País ASC;
