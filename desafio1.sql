a4986834568eaf91f2221920d8a3cd37788d359a
FredericoCanoeira <fcanoeira@gmail.com>
2021-09-03 00:38:35 +0100

    refactoring requiriment 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,6 @@
 -- Exiba os países e indicando se cada um deles se encontra ou não na região formada pela Europa
 -- Crie uma query que mostre se o país pertence ou não à região "Europe".
-SELECT COUNTRY_NAME AS 'Pais', 
-IF(REGION_ID = 1,'incluído','não incluído') AS 'Status Inclusão'
+SELECT country_name AS 'Pais', 
+IF(region_id = 1,'incluído','não incluído') AS 'Status Inclusão'
 FROM hr.countries
-ORDER BY COUNTRY_NAME;
+ORDER BY country_name;
