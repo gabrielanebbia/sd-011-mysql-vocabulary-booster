a0ccf31c7358400a0f6dd638e406ffdfcefa107c
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-09-01 22:55:29 -0300

    included name of bd together in the name of column

--- desafio6.sql
+++ desafio6.sql
@@ -1,11 +1,11 @@
 SELECT concat(first_name, " ", last_name) AS 'Nome Completo', j.job_title AS Cargo
 ,d.Start_date AS 'Data de início do cargo', de.department_name AS 'Departamento'  
-FROM employees AS e
+FROM hr.employees AS e
 INNER JOIN jobs AS j
 ON e.job_id = j.job_id
-INNER JOIN job_history AS d
+INNER JOIN hr.job_history AS d
 ON e.job_id = d.job_id
-INNER JOIN departments AS de
+INNER JOIN hr.departments AS de
 ON d.department_id = de.department_id
 ORDER BY concat(first_name, " ", last_name) Desc
 , de.department_name ASC;
