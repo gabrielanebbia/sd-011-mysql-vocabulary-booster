4612ada1d238347812a4399c6479f45b6c702646
Gustavo Lemes <gustavolemes997@gmail.com>
2021-09-02 02:14:54 -0300

    Requisito 15

---
+++ desafio15.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
+BEGIN
+SELECT ROUND(AVG(he.SALARY), 2) AS 'Média salarial'
+FROM hr.employees AS he
+INNER JOIN hr.jobs AS hj
+ON he.JOB_ID = hj.JOB_ID
+WHERE hj.JOB_TITLE = cargo; 
+END $$
+
+DELIMITER ;
