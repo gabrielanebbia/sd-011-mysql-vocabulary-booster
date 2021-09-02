dfe1e47ee87780227edd61274f3aa11d982976e0
Gustavo Lemes <gustavolemes997@gmail.com>
2021-09-02 02:57:00 -0300

    Corrige(?) req  17

--- desafio17.sql
+++ desafio17.sql
@@ -1,4 +1,9 @@
+DELIMITER $$
 CREATE TRIGGER w3schools.insert_order
 BEFORE INSERT ON w3schools.orders
 FOR EACH ROW
-SET NEW.OrderDate = NOW();
+BEGIN
+SET NEW.OrderDate = CURRENT_DATE();
+END$$
+DELIMITER ;
+
