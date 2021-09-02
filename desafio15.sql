aad59f9d8815768a07104089e4e7bf3370eac3fa
guinhoanjos <iago.anjos@hotmail.com>
2021-09-01 22:50:55 -0300

    Create desafio15.sql

---
+++ desafio15.sql
@@ -0,0 +1,15 @@
+USE hr;
+
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN CARGO VARCHAR(100))
+BEGIN
+    SELECT ROUND(AVG(SALARY),2) AS 'Média salarial' FROM employees
+WHERE JOB_ID = (
+SELECT JOB_ID FROM jobs
+WHERE JOB_TITLE = CARGO
+)
+;
+END $$
+
+DELIMITER ;
