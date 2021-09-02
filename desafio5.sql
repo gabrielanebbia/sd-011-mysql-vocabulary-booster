bc8bc6ae0a8e7e9bf54695b8e895f357661b4237
anacarol-r-santos <kroline0693@gmail.com>
2021-09-02 14:05:31 -0300

    desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+SELECT
+JOB_TITLE Cargo,
+(MAX_SALARY - MIN_SALARY) 'Variação Salarial',
+ROUND(MIN(MIN_SALARY/12), 2) 'Média mínima mensal',
+ROUND(MAX(MAX_SALARY/12), 2) 'Média máxima mensal'
+FROM hr.jobs
+GROUP BY JOB_TITLE
+ORDER BY (MAX_SALARY - MIN_SALARY), JOB_TITLE;
