31e93122b3f00860317709ae6fb21552fa4e683f
getuliogutemberg <getulio.dev@gmail.com>
2021-09-02 15:58:41 -0300

    Resolve requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,12 @@
+-- 7 - Faça um relatório que mostra o histórico de cargos das pessoas empregadas que iniciaram seus cargos nos meses de janeiro, fevereiro ou março
+SELECT
+  UCASE(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) AS 'Nome completo',
+  JH.START_DATE AS 'Data de início',
+  E.SALARY AS 'Salário'
+FROM
+  hr.job_history AS JH
+  INNER JOIN hr.employees AS E ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+  WHERE MONTH(JH.START_DATE) IN (1,2,3)
+ORDER BY
+  CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) ASC,
+  JH.START_DATE ASC;
