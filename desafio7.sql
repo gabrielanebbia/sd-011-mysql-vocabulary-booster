ad485f9617d9e9b21a0f345d50d527faef783fa9
IgorMendez <igor.emp@hotmail.com>
2021-09-02 00:29:12 -0300

    Desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT UCASE(CONCAT(e.first_name, ' ', e.last_name)) AS 'Nome completo',
+jh.start_date AS 'Data de início',
+salary AS 'Salário'
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS jh
+ON jh.employee_id = e.employee_id
+WHERE MONTH(start_date) IN (01, 02, 03)
+ORDER BY Nome completo, hire_date;
