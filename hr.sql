3f1f8e4f9fe352b52baf1a50c1a6d5bad8ba2447
IgorMendez <igor.emp@hotmail.com>
2021-09-02 02:52:46 -0300

    correção

--- hr.sql
+++ hr.sql
@@ -1,14 +1,13 @@
--- MySQL dump 10.13  Distrib 5.6.33, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.26, for Linux (x86_64)
 --
--- Host: localhost    Database: w3resour_hr
+-- Host: 127.0.0.1    Database: hr
 -- ------------------------------------------------------
--- Server version 5.6.33-0ubuntu0.14.04.1
-DROP database if exists hr;
-CREATE database hr;
+-- Server version	8.0.26-0ubuntu0.21.04.3
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -19,10 +18,10 @@ CREATE database hr;
 --
 -- Table structure for table `countries`
 --
-use hr;
+
 DROP TABLE IF EXISTS `countries`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `countries` (
   `COUNTRY_ID` varchar(2) NOT NULL,
   `COUNTRY_NAME` varchar(40) DEFAULT NULL,
@@ -48,7 +47,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `departments`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `departments` (
   `DEPARTMENT_ID` decimal(4,0) NOT NULL DEFAULT '0',
   `DEPARTMENT_NAME` varchar(30) NOT NULL,
@@ -76,7 +75,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `employees`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `employees` (
   `EMPLOYEE_ID` decimal(6,0) NOT NULL DEFAULT '0',
   `FIRST_NAME` varchar(20) DEFAULT NULL,
@@ -114,7 +113,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `job_history`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `job_history` (
   `EMPLOYEE_ID` decimal(6,0) NOT NULL,
   `START_DATE` date NOT NULL,
@@ -144,7 +143,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `jobs`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `jobs` (
   `JOB_ID` varchar(10) NOT NULL DEFAULT '',
   `JOB_TITLE` varchar(35) NOT NULL,
@@ -170,7 +169,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `locations`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `locations` (
   `LOCATION_ID` decimal(4,0) NOT NULL DEFAULT '0',
   `STREET_ADDRESS` varchar(40) DEFAULT NULL,
@@ -201,7 +200,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `regions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `regions` (
   `REGION_ID` decimal(5,0) NOT NULL,
   `REGION_NAME` varchar(25) DEFAULT NULL,
@@ -219,10 +218,14 @@ LOCK TABLES `regions` WRITE;
 INSERT INTO `regions` VALUES (1,'Europe'),(2,'Americas'),(3,'Asia'),(4,'Middle East and Africa');
 /*!40000 ALTER TABLE `regions` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/* WRITE YOUR QUERY HERE */
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-
-SELECT COUNT(DISTINCT job_id)
-FROM employees;
-
+-- Dump completed on 2021-09-02  2:50:19
