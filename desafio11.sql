6b9a0ca4b0ca34706d4cb523c5da3b87fef5cb6b
Alan Gonçalves <vekio.soft@gmail.com>
2021-09-02 08:57:11 -0300

    fix lint req11

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,9 @@
 SELECT C.ContactName AS 'Nome',
 c.Country AS 'País',
-(SELECT COUNT(Country) - 1 FROM w3schools.customers WHERE w3schools.customers.Country = c.Country)
-AS 'Número de compatriotas'
+(
+  SELECT COUNT(Country) - 1 FROM w3schools.customers 
+    WHERE w3schools.customers.Country = c.Country
+) AS 'Número de compatriotas'
 FROM w3schools.customers AS c
 HAVING `Número de compatriotas` > 0
 ORDER BY c.ContactName;
