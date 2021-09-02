3c596ac492d8c8edc258865cbf4d08331948896b
IgorMendez <igor.emp@hotmail.com>
2021-09-02 02:10:38 -0300

    Desafio 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,3 +1,5 @@
-SELECT DISTINCT Country AS 'País' FROM w3schools.customers
-ORDER BY Country
+(SELECT Country AS 'País' FROM w3schools.customers)
+UNION
+(SELECT Country AS 'País' FROM w3schools.suppliers)
+ORDER BY País
 LIMIT 5;
