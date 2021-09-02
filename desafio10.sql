7c70182a140b752eac7a14dc4219cf1895599d77
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-09-02 20:34:51 -0300

    Corrigindo requisito 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,6 +1,6 @@
 SELECT p.ProductName AS Produto,
-MIN(od.Quantity) AS Mínimo,
-MAX(od.Quantity) AS Máximo,
+MIN(od.Quantity) AS Mínima,
+MAX(od.Quantity) AS Máxima,
 ROUND(AVG(od.Quantity), 2) AS Média
 FROM order_details AS od
 INNER JOIN products AS p
