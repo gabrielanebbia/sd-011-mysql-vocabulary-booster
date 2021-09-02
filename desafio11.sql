864af58647f72e93e5a74349524e61640aeccad9
Alan Gonçalves <vekio.soft@gmail.com>
2021-09-02 08:48:59 -0300

    refat req11

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,7 @@
-SELECT c1.ContactName AS 'Nome',
-c1.Country AS 'País',
-COUNT(c1.Country) AS 'Número de compatriotas'
-FROM w3schools.customers AS c1
-INNER JOIN w3schools.customers AS c2
-ON c1.Country = c2.Country AND c1.CustomerID != c2.CustomerID
-GROUP BY c1.ContactName, c2.Country
-ORDER BY c1.ContactName ASC;
+SELECT C.ContactName AS 'Nome',
+c.Country AS 'País',
+(SELECT COUNT(Country) - 1 FROM w3schools.customers WHERE w3schools.customers.Country = c.Country)
+AS 'Número de compatriotas'
+FROM w3schools.customers AS c
+HAVING `Número de compatriotas` > 0
+ORDER BY c.ContactName;
