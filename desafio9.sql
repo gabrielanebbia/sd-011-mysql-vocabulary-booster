96756f0ea46c0e32193cff3eb82e32ff28c31e36
IgorMendez <igor.emp@hotmail.com>
2021-09-02 00:31:56 -0300

    Desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,7 @@
+ SELECT CONCAT(e.firstName, ' ', e.lastName) AS 'Nome completo',
+COUNT(*) AS 'Total de pedidos'
+FROM w3schools.employees AS e
+INNER JOIN w3schools.orders AS o
+ON o.EmployeeID = e.EmployeeID
+GROUP BY Nome completo
+ORDER BY Total de pedidos;
