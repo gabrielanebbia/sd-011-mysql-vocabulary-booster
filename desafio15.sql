933bcc474e7414fbe433d2832060c203641e7600
Alan Gonçalves <vekio.soft@gmail.com>
2021-09-02 13:36:17 -0300

    requisito 15

--- desafio15.sql
+++ desafio15.sql
@@ -2,11 +2,11 @@ USE hr;
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN getTitle VARCHAR(100))
 BEGIN
-SELECT AVG(s.SALARY) AS 'Média salarial' 
+SELECT ROUND(AVG(s.SALARY), 2) AS 'Média salarial' 
 FROM employees AS s
 LEFT JOIN hr.jobs AS t
 ON s.JOB_ID = t.JOB_ID
-WHERE JOB_TITLE = getTitle
+WHERE t.JOB_TITLE = getTitle
 GROUP BY s.JOB_ID;
 END $$
 DELIMITER ;
