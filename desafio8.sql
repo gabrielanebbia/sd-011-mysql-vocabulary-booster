6c3fded403aeb0ab3632a1d22f33b3c7b6cb1342
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-09-02 17:44:38 -0300

    Corrigindo requisito 8

--- desafio8.sql
+++ desafio8.sql
@@ -5,4 +5,4 @@ FROM orders AS o
 INNER JOIN customers AS c ON o.CustomerID = c.CustomerID
 INNER JOIN shippers AS s ON o.ShipperID = s.ShipperID
 WHERE o.ShipperID 	BETWEEN 1 AND 2
-ORDER BY c.ContactName, s.ShipperName;
+ORDER BY c.ContactName, s.ShipperName, o.OrderDate;
