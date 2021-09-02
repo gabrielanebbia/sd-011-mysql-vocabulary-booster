2b72af2d62ea07111aee1a518997e8eb3b2ce048
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-09-02 01:43:40 -0300

    tentando resolver requisito 13

--- desafio13.sql
+++ desafio13.sql
@@ -2,6 +2,7 @@ SELECT ProductName AS Produto,
 Price AS 'Preço'
 FROM w3schools.products AS p
 INNER JOIN w3schools.order_details AS o
-ON o.ProductID = p.ProductID AND o.Quantity > 80
+ON o.ProductID = p.ProductID
 GROUP BY ProductName
+HAVING SUM(Quantity) > 80
 ORDER BY Produto;
