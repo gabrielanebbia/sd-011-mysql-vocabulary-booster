aa42f7f692507d596fd83e982bd21967654d0db2
pmaiamateus <pmaiamateus@gmail.com>
2021-09-02 14:34:11 -0300

    att desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -2,8 +2,5 @@ select JOB_TITLE as 'Cargo',
 MAX_SALARY - MIN_SALARY as 'Variação Salarial',
 round(min_salary/12, 2) as 'Média mínima mensal',
 round(max_salary/12, 2) as 'Média máxima mensal'
-from hr.employees as e
-inner join hr.jobs as j
-on e.JOB_ID = j.JOB_ID
-group by Cargo
+from hr.jobs
 order by MAX_SALARY - MIN_SALARY, JOB_TITLE;
