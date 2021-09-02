8e2b283f07ff005cb42c83e5b6999a36edaa3f84
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-09-02 14:53:48 -0300

    REQ 12

---
+++ desafio12.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT(E1.first_name,' ', E1.last_name) AS 'Nome completo funcionário 1',
+E1.salary AS 'Salário funcionário 1', E1.phone_number AS 'Telefone funcionário 1',
+CONCAT(E2.first_name,' ', E2.last_name) AS 'Nome completo funcionário 2',
+E2.salary AS 'Salário funcionário 2', E2.phone_number AS 'Telefone funcionário 2'
+FROM hr.employees AS E1, hr.employees AS E2
+WHERE E1.EMPLOYEE_ID <> E2.EMPLOYEE_ID
+AND E1.JOB_ID = E2.JOB_ID
+ORDER BY 1, 4;
