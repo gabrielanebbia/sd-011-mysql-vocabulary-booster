5429ce55099e39b9d49f665fd28382b67deb6fc2
Alan Gonçalves <vekio.soft@gmail.com>
2021-09-02 14:05:14 -0300

    requisito 16

---
+++ desafio16.sql
@@ -0,0 +1,15 @@
+USE hr;
+DELIMITER $$
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(jobs VARCHAR(100))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE get_jobs INT;
+SELECT COUNT(*)
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS h
+ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE e.EMAIL = jobs INTO get_jobs;
+RETURN get_jobs;
+END $$
+DELIMITER ;
+SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
