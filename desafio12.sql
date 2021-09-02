b45d4e43519eb57d51782eb369328c9e33e371c7
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-09-02 01:04:30 -0300

    tentando resolver erro no requisito 12

--- desafio12.sql
+++ desafio12.sql
@@ -6,4 +6,5 @@ t2.salary AS 'Salário funcionário 2',
 t2.phone_number AS 'Telefone funcionário 2'
 FROM hr.employees AS t1, hr.employees AS t2
 WHERE t1.JOB_ID = t2.JOB_ID AND t1.FIRST_NAME <> t2.FIRST_NAME AND t1.LAST_NAME <> t2.LAST_NAME
+GROUP BY `Nome completo funcionário 1`, `Salário funcionário 1`, `Telefone funcionário 1`
 ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
