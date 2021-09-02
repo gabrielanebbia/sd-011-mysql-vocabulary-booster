5322a26f544288b07d38b9fcfd390c5587edabb9
anacarol-r-santos <kroline0693@gmail.com>
2021-09-02 17:44:12 -0300

    tentando corrigir o desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -5,5 +5,5 @@ o.OrderDate 'Data do pedido'
 FROM w3schools.customers c
 INNER JOIN w3schools.orders o ON c.CustomerID = o.CustomerID
 INNER JOIN w3schools.shippers s ON s.ShipperID = o.ShipperID
-WHERE s.ShipperName = 'Speedy Express' OR 'United Package'
+WHERE s.ShipperName IN('Speedy Express', 'United Package')
 ORDER BY c.ContactName, s.ShipperName, o.OrderDate;
