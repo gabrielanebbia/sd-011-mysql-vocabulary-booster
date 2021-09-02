09e5a5a99dbb61480402b5be35b616d23af48faf
JoaoCabrera <joaocabrerati@gmail.com>
2021-09-02 19:37:15 -0300

    Adiciona requisitos 8, 9 e 10

---
+++ desafio10.sql
@@ -0,0 +1,8 @@
+SELECT  P.ProductName AS 'Produto', MIN(O.Quantity) AS 'Mínima',
+MAX(O.Quantity) AS 'Máxima', ROUND(AVG(O.Quantity), 2)  AS 'Média'
+FROM w3schools.products AS P
+INNER JOIN w3schools.order_details AS O
+ON O.ProductID = P.ProductID
+GROUP BY P.ProductName
+HAVING Média > 20.00
+ORDER BY Média, Produto;
