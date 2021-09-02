96bca4ff7342d4e4475a8ce7c41bcc9dab1549e9
anacarol-r-santos <kroline0693@gmail.com>
2021-09-02 15:38:33 -0300

    desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT 
+CONCAT(UCASE(e.FIRST_NAME), ' ', UCASE(e.LAST_NAME)) 'Nome completo',
+jh.START_DATE 'Data de início',
+e.SALARY 'Salário'
+FROM hr.employees e
+INNER JOIN job_history jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE MONTH(jh.START_DATE) BETWEEN 01 AND 03
+ORDER BY CONCAT(e.FIRST_NAME, ' ',e.LAST_NAME), jh.START_DATE;
