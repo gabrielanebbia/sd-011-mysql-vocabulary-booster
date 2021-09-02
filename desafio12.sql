57f8dc71ec9ef4001dd75291d27d699596097a02
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-09-02 01:09:56 -0300

    tentando resolver erro no requisito 12

--- desafio12.sql
+++ desafio12.sql
@@ -5,6 +5,5 @@ CONCAT(t2.first_Name, ' ', t2.last_Name) AS 'Nome completo funcionário 2',
 t2.salary AS 'Salário funcionário 2',
 t2.phone_number AS 'Telefone funcionário 2'
 FROM hr.employees AS t1, hr.employees AS t2
-WHERE t1.JOB_ID = t2.JOB_ID AND t1.FIRST_NAME <> t2.FIRST_NAME AND t1.LAST_NAME <> t2.LAST_NAME
-GROUP BY `Nome completo funcionário 1`, `Salário funcionário 1`, `Telefone funcionário 1`
+WHERE t1.JOB_ID = t2.JOB_ID AND t1.EMPLOYEE_ID <> t2.EMPLOYEE_ID
 ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
