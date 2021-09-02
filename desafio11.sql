2a91c0a89cac3b5a66bf3eef39ebd5d4ec2cd129
Alan Gonçalves <vekio.soft@gmail.com>
2021-09-02 09:06:21 -0300

    fix tab req 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,8 @@
 SELECT C.ContactName AS 'Nome',
 c.Country AS 'País',
 (
-  SELECT COUNT(Country) - 1 FROM w3schools.customers 
-    WHERE w3schools.customers.Country = c.Country
+SELECT COUNT(Country) - 1 FROM w3schools.customers 
+WHERE w3schools.customers.Country = c.Country
 ) AS 'Número de compatriotas'
 FROM w3schools.customers AS c
 HAVING `Número de compatriotas` > 0
