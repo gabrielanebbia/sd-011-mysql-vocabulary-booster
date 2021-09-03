14966bd93ca2fdb33b2b5a2f12a6b3e96903a56f
getuliogutemberg <getulio.dev@gmail.com>
2021-09-03 01:25:38 -0300

    Resolve requisito 13

--- desafio13.sql
+++ desafio13.sql
@@ -1,1 +1,7 @@
 -- 13 - Exibe todos produtos que já tiveram um pedido associado requerindo uma quantidade desse produto maior que 80
+SELECT WP.ProductName AS 'Produto', WP.price AS 'Preço'
+FROM w3schools.order_details AS WO
+INNER JOIN w3schools.products AS WP
+ON WO.ProductID = WP.ProductID
+WHERE WO.Quantity > 80
+ORDER BY WP.ProductName;
