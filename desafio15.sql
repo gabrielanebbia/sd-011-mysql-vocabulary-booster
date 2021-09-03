dd29b7a51ea4324a22d0c17f7c6241cbf49ff416
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-09-03 16:43:21 -0300

    req 15 traz o resultado certo e nao passa no evaluator

--- desafio15.sql
+++ desafio15.sql
@@ -6,7 +6,7 @@ SELECT ROUND(AVG(salary), 2) AS 'Média salarial'
 FROM hr.jobs AS J
 INNER JOIN hr.employees AS E
 ON J.JOB_ID = E.JOB_ID
-WHERE job = J.JOB_TITLE
+WHERE job = J.JOB_TITLE;
 END $$
 
 DELIMITER ;
