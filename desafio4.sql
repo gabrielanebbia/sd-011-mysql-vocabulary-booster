a4abacc9c282bf44966270cab1c20d4056cff3ef
Maria Lisboa <marialisboa1515@gmail.com>
2021-09-03 10:07:43 -0300

    desafio 4 e 5

--- desafio4.sql
+++ desafio4.sql
@@ -1,20 +1,20 @@
 SELECT
-  jo.JOB_TITLE AS 'Cargo',
-  ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
-  CASE
-    WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
-    WHEN ROUND(AVG(e.SALARY), 2) < 7500 THEN 'Pleno'
-    WHEN ROUND(AVG(e.SALARY), 2) < 10500 THEN 'Sênior'
-    WHEN ROUND(AVG(e.SALARY), 2) > 10500 THEN 'CEO'
-  END AS 'Senioridade'
+jo.JOB_TITLE AS 'Cargo',
+ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
+CASE
+WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+WHEN ROUND(AVG(e.SALARY), 2) < 7500 THEN 'Pleno'
+WHEN ROUND(AVG(e.SALARY), 2) < 10500 THEN 'Sênior'
+WHEN ROUND(AVG(e.SALARY), 2) > 10500 THEN 'CEO'
+END AS 'Senioridade'
 FROM
-  jobs AS jo
+jobs AS jo
 INNER JOIN
-  employees AS em
+employees AS em
 ON
-  jo.JOB_ID = em.JOB_ID
+jo.JOB_ID = em.JOB_ID
 GROUP BY
-  em.JOB_ID
+em.JOB_ID
 ORDER BY
-  ROUND(AVG(e.SALARY), 2) ASC,
-  jo.JOB_TITLE ASC;
+ROUND(AVG(e.SALARY), 2) ASC,
+jo.JOB_TITLE ASC;
