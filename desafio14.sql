cb749ecab1c5279aa1903f5e47c8e445db25cbcf
Michelle <mls.srbek@gmail.com>
2021-09-03 12:58:09 -0300

    challenges 14 correction

--- desafio14.sql
+++ desafio14.sql
@@ -1,3 +1,3 @@
 SELECT Country AS País FROM w3schools.customers 
 UNION 
-SELECT Country AS País FROM w3schools.suppliers ORDER BY Country LIMIT 5;
+SELECT Country AS País FROM w3schools.suppliers ORDER BY País LIMIT 5;
