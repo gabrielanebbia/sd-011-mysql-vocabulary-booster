83a87d0efe51faab9ae573eea03f8823c9fd2353
gab-boff <gmeninboff@gmail.com>
2021-09-03 13:59:49 -0300

    a

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 SELECT
-	CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS `Nome completo`,
-    jb.JOB_TITLE AS Cargo,
-    jbhist.START_DATE AS `Data de início do cargo`,
-    dep.DEPARTMENT_NAME AS Departamento 
+CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS `Nome completo`,
+jb.JOB_TITLE AS Cargo,
+jbhist.START_DATE AS `Data de início do cargo`,
+dep.DEPARTMENT_NAME AS Departamento 
 FROM hr.employees as emp
 INNER JOIN hr.job_history AS jbhist
 	ON jbhist.EMPLOYEE_ID = emp.EMPLOYEE_ID
