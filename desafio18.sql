54b856795fbe470e3dd5fcc0364c6a03cde3ef44
Maria Lisboa <marialisboa1515@gmail.com>
2021-09-03 11:05:35 -0300

    desafio18

--- desafio18.sql
+++ desafio18.sql
@@ -1,13 +1,13 @@
 SELECT
-CONCAT(em.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-DATE_FORMAT(jhis.START_DATE, '%d/%m/%Y') AS 'Data de início',
-DATE_FORMAT(jhis.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
-ROUND(DATEDIFF(jhis.END_DATE, jhis.START_DATE) / 365, 2) AS 'Anos trabalhados'
+CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
+DATE_FORMAT(jh.START_DATE, '%d/%m/%Y') AS 'Data de início',
+DATE_FORMAT(jh.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
+ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE) / 365, 2) AS 'Anos trabalhados'
 FROM
-job_history AS jhis
+job_history AS jh
 INNER JOIN
-employees AS em
-ON em.EMPLOYEE_ID = jhis.EMPLOYEE_ID
+employees AS e
+ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 ORDER BY
 1 ASC,
 4 ASC;
