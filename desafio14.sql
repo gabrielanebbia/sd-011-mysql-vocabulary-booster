009616514dce9dc463996e3db5b7d1312204fea5
Maria Lisboa <marialisboa1515@gmail.com>
2021-09-03 11:08:01 -0300

    desafio14

--- desafio14.sql
+++ desafio14.sql
@@ -1,7 +1,1 @@
-SELECT DISTINCT Country AS 'País' FROM
-(
-SELECT Country FROM suppliers
-UNION ALL
-SELECT Country FROM customers
-)
-ORDER BY Country ASC LIMIT 5 ;
+SELECT Country AS 'País' FROM w3schools.customers UNION SELECT Country AS 'País' FROM w3schools.suppliers ORDER BY `País` LIMIT 5;
