4b294a073308b857c1286af4bca368aa38edffa6
Michel Pereira <michelbecauro@gmail.com>
2021-09-03 09:08:32 -0300

    Corrige issues do Codeclimate no requisito 16

--- desafio16.sql
+++ desafio16.sql
@@ -2,11 +2,11 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email varchar(40))
 RETURNS smallint READS SQL DATA
 BEGIN
-	DECLARE jobs smallint;
-	SELECT COUNT(*) FROM hr.job_history jh
-	INNER JOIN hr.employees e
-	ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
-	WHERE e.email = email INTO jobs;
-	RETURN jobs;
+DECLARE jobs smallint;
+SELECT COUNT(*) FROM hr.job_history jh
+INNER JOIN hr.employees e
+ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE e.email = email INTO jobs;
+RETURN jobs;
 END $$
 DELIMITER ;
