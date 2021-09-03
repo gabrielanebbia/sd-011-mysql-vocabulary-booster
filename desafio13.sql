87aff55ecb25e5277a80c57e4d3dbf25c5d22e1f
Diego Figueirêdo <8kball@gmail.com>
2021-09-02 23:47:55 -0300

    Desafio 13

---
+++ desafio13.sql
@@ -0,0 +1,14 @@
+SELECT
+p.productName AS `Produto`,
+p.price AS `Preço`
+FROM
+w3schools.order_details AS o
+INNER JOIN
+w3schools.products AS p
+ON o.productId = p.productId
+GROUP BY
+`Produto`
+HAVING
+MAX(o.quantity) > 80
+ORDER BY
+`Produto`;
