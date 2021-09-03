2aac4af4409ee8736ffd3eb8aa2cab1314b9d3f7
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-09-03 19:26:27 -0300

    req 18

---
+++ desafio18.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT(first_name,' ', last_name) AS 'Nome completo',
+DATE_FORMAT(START_DATE, '%d/%m/%Y') AS 'Data de início',
+DATE_FORMAT(END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
+ROUND(DATEDIFF(end_date, start_date) / 365, 2) AS 'Anos trabalhados'
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS jh
+ON e.JOB_ID = jh.JOB_ID
+ORDER BY 1,4;
