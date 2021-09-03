72203a92e429d55e08bf8177d9ac7d6b0b540b26
anacarol-r-santos <kroline0693@gmail.com>
2021-09-02 22:29:17 -0300

    desafio 16

---
+++ desafio16.sql
@@ -0,0 +1,21 @@
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email_func VARCHAR(100))
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE historico_funcionario INT;
+    SELECT COUNT(*)
+    FROM job_history
+    WHERE EMPLOYEE_ID IN (
+    SELECT EMPLOYEE_ID 
+    FROM employees
+    WHERE EMAIL = email_func
+    )   
+    INTO historico_funcionario;
+    RETURN historico_funcionario;
+END $$
+
+DELIMITER ;
+
+-- SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
