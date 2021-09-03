81d35117532fbd4153a082e0866cb058577dc314
anacarol-r-santos <kroline0693@gmail.com>
2021-09-03 13:26:47 -0300

    não sei pq nao aceita o desafio 18

--- desafio18.sql
+++ desafio18.sql
@@ -3,7 +3,7 @@ SELECT
 CONCAT(e.FIRST_NAME, ' ', LAST_NAME) 'Nome completo',
 DATE_FORMAT(jh.START_DATE, "%d/%m/%Y") 'Data de início',
 DATE_FORMAT(jh.END_DATE, "%d/%m/%Y") 'Data de rescisão',
-ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE)/365.25, 2) 'Anos trabalhados'
+(ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE)/365.25, 2)) 'Anos trabalhados'
 FROM employees e INNER JOIN job_history jh
 ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 ORDER BY `Nome completo`, `Anos trabalhados`;
