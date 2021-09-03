3d854ecaf1ec57559c55ea82df1d0177c1b5b932
FredericoCanoeira <fcanoeira@gmail.com>
2021-09-03 10:50:26 +0100

    fixing requirement 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,8 @@
 -- Exiba os cargos com sua variação salarial e suas médias máxima e mínima mensal, considerando salários máximo e minímo como anuais
 -- warning Considere os salários máximo e mínimo de um cargo como salários anuais. warning
-SELECT job_title AS "Cargo", 
-(max_salary - min_salary) AS "Variação Salarial",
-ROUND((min_salary /12), 2) AS "Média mínima mensal",
-ROUND((max_salary /12), 2) AS "Média mínima mensal"
+SELECT job_title AS 'Cargo', 
+(max_salary - min_salary) AS 'Variação Salarial',
+ROUND((min_salary/ 12), 2) AS 'Média mínima mensal',
+ROUND((max_salary/ 12), 2) AS 'Média mínima mensal'
 FROM hr.jobs
 ORDER BY `Variação Salarial`, Cargo;
