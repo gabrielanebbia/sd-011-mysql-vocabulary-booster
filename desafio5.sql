802641dbca62396770fb0816c9772cb6ee14474b
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-09-03 15:48:02 -0300

    req 5 mais uma vez

--- desafio5.sql
+++ desafio5.sql
@@ -1,4 +1,5 @@
-SELECT JOB_TITLE AS Cargo, (MAX_SALARY - MIN_SALARY) AS 'Variação Salarial', 
-ROUND((MIN_SALARY / 12), 2) AS 'Média mínima mensal', ROUND((MAX_SALARY / 12), 2) AS 'Média máxima mensal'
+SELECT job_title AS 'Cargo', (max_salary - min_salary) AS 'Variação Salarial',
+ROUND((min_salary/ 12),2) AS 'Média mínima mensal',
+ROUND((max_salary / 12), 2) AS 'Média máxima mensal'
 FROM hr.jobs
-ORDER BY 'Variação Salarial', Cargo;
+ORDER BY `Variação salarial`, Cargo;
