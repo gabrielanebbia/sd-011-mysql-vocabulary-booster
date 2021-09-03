ddae0a6d42fb8f2eacc3259491ac0eb558c0e944
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-09-03 16:26:41 -0300

    req 15

---
+++ desafio15.sql
@@ -0,0 +1,12 @@
+USE hr;
+DELIMITER $$;
+CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(50))
+BEGIN 
+SELECT ROUND(AVG(salary), 2) AS 'Média salarial'
+FROM hr.jobs AS J
+INNER JOIN hr.employees AS E
+ON J.JOB_ID = E.JOB_ID
+WHERE job = J.JOB_TITLE;
+END $$;
+
+DELIMITER ;
