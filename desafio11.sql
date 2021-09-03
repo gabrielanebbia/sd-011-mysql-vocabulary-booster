085fb675814c94f41a44603e2933b4fd9542a17d
JoaoCabrera <joaocabrerati@gmail.com>
2021-09-03 02:45:25 -0300

    Corrige requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -2,4 +2,5 @@ SELECT C1.ContactName AS 'Nome', C1.Country AS 'País', COUNT(C2.Country) - 1 AS
 FROM w3schools.customers AS C1, w3schools.customers AS C2
 WHERE C1.Country = C2.Country
 GROUP BY C1.ContactName, C2.Country
+HAVING COUNT(C2.Country) - 1 <> 0
 ORDER BY C1.ContactName;
