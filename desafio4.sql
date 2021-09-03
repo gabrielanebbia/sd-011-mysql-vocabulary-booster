113d6485c45a9ca0b85cd543c99f0ae92b658dee
Michel Pereira <michelbecauro@gmail.com>
2021-09-03 05:19:04 -0300

    Corrige Requisito 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,5 +1,5 @@
 
-SELECT job_title, ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
+SELECT job_title AS Cargo, ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
 CASE
 WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
 WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
