0ed9f16cd40a3af857a0fe630489ff7b59048d1e
igordmf <igordanielmf@gmail.com>
2021-09-03 20:10:37 -0300

    Requisito 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,7 @@
 SELECT
 job_title AS 'Cargo',
 (max_salary - min_salary) AS 'Variação Salarial',
-ROUND(AVG (min_salary / 12), 2) AS 'Média mínima mensal',
-ROUND(AVG (max_salary / 12), 2) AS 'Média máxima mensal'
+CAST((min_salary / 12) AS DECIMAL(10, 2)) AS 'Média mínima mensal',
+CAST((max_salary / 12) AS DECIMAL(10, 2)) AS 'Média máxima mensal'
 FROM hr.jobs
-GROUP BY job_title
 ORDER BY (max_salary - min_salary), job_title;
