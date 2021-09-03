c43f4772e9a41fdd1f2741dc227ca22cfbfd5a1d
Maria Lisboa <marialisboa1515@gmail.com>
2021-09-03 11:24:03 -0300

    desafio9

--- desafio9.sql
+++ desafio9.sql
@@ -1,16 +1,4 @@
-SHOW TABLES;
-SHOW COLUMNS FROM orders;
-SELECT * FROM orders;
-SELECT * FROM customers WHERE ContactName LIKE '%Anne%';
-SELECT
-CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
-COUNT(e.EmployeeID) AS 'Total de pedidos'
-FROM
-employees AS e
-INNER JOIN
-orders AS o
-ON o.EmployeeID = e.EmployeeID
-GROUP BY
-e.EmployeeID
-ORDER BY
-2 ASC;
+SELECT CONCAT(e.FirstName, ' ', e.LastName) 
+AS `Nome completo`,  COUNT(*) AS `Total de pedidos` 
+FROM w3schools.orders o INNER JOIN w3schools.employees e 
+ON o.EmployeeID = e.EmployeeID GROUP BY `Nome completo` ORDER BY `Total de pedidos` ASC; 
