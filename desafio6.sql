a78336c3a51cc9215b302e682e8e7f0eff4ab0fd
gab-boff <gmeninboff@gmail.com>
2021-09-03 14:01:00 -0300

    a

--- desafio6.sql
+++ desafio6.sql
@@ -5,9 +5,9 @@ jbhist.START_DATE AS `Data de início do cargo`,
 dep.DEPARTMENT_NAME AS Departamento 
 FROM hr.employees as emp
 INNER JOIN hr.job_history AS jbhist
-	ON jbhist.EMPLOYEE_ID = emp.EMPLOYEE_ID
+ON jbhist.EMPLOYEE_ID = emp.EMPLOYEE_ID
 INNER JOIN hr.jobs AS jb
-	ON jb.JOB_ID = jbhist.JOB_ID
+ON jb.JOB_ID = jbhist.JOB_ID
 INNER JOIN hr.departments AS dep
-	ON dep.DEPARTMENT_ID = jbhist.DEPARTMENT_ID
+ON dep.DEPARTMENT_ID = jbhist.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, Cargo;
