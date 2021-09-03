c2d54cc2111fcac73f1285a99c6d256168c7524f
Alexandre <naitwa.alexandre@gmail.com>
2021-09-03 06:25:07 -0300

    08

---
+++ desafio8.sql
@@ -0,0 +1,1 @@
+SELECT c.ContactName AS `Nome de contato`, s.ShipperName AS `Empresa que fez o envio`, OrderDate AS `Data do pedido` FROM w3schools.orders o INNER JOIN w3schools.customers c ON o.CustomerID = c.CustomerID INNER JOIN w3schools.shippers s ON o.ShipperID = s.ShipperID WHERE o.ShipperID IN (1, 2) ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido` ASC;
