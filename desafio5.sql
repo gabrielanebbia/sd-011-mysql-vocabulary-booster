db8fd3d75cd49b53a38b79d15e4ce057c1179b73
camiloLelis <lafirmafuerte@gmail.com>
2021-09-03 16:37:16 -0300

    requisito-7-requisito-3

--- desafio5.sql
+++ desafio5.sql
@@ -1,3 +1,12 @@
+/* SELECT j.JOB_TITLE AS Cargo,
+ROUND(j.MAX_SALARY - j.MIN_SALARY , 2) AS `Variação Salarial`,
+ROUND(MIN(e.SALARY), 2) AS  `Média mínima mensal`,
+ROUND(MAX(e.SALARY), 2) AS  `Média mínima mensal`
+ FROM hr.employees AS e 
+ INNER JOIN hr.jobs AS j 
+ ON e.JOB_ID = j.JOB_ID 
+ GROUP BY Cargo
+ ORDER BY `Variação Salarial`, Cargo; */
 SELECT JOB_TITLE AS 'Cargo', 
 (MAX_SALARY - MIN_SALARY) 'Variação Salarial',
 ROUND((MIN_SALARY / 12),2) 'Média mínima mensal',
--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,6 @@
 SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
-jh.START_DATE as `Data de início`,
-e.SALARY as `Salário`
+jh.START_DATE AS `Data de início`,
+e.SALARY AS `Salário`
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS jh
 ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
