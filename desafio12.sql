74631edbee9474fad2d700688da544de9cf587bd
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-09-03 13:53:22 -0300

    corrigindo requisito 12

--- desafio12.sql
+++ desafio12.sql
@@ -5,4 +5,5 @@ CONCAT(E2.FIRST_NAME, ' ', E2.LAST_NAME) AS 'Nome completo funcionário 2',
 E2.SALARY AS 'Salário funcionário 2',
 E2.PHONE_NUMBER as 'Telefone funcionário 2'
 FROM employees AS E1, employees AS E2
-WHERE E1.JOB_ID = E2.JOB_ID  AND E1.EMPLOYEE_ID <> E2.EMPLOYEE_ID;
+WHERE E1.JOB_ID = E2.JOB_ID  AND E1.EMPLOYEE_ID <> E2.EMPLOYEE_ID
+ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
