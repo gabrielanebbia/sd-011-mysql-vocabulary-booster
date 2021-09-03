ca90547c99ad13d48fbfc7d3eb1c6746cccd76b1
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-09-03 20:28:04 -0300

    refazendo req 20

--- desafio20.sql
+++ desafio20.sql
@@ -3,14 +3,14 @@ DELIMITER $$
 
 CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(100))
 BEGIN
-SELECT CONCAT(E.first_name,' ',E.last_name) AS 'Nome completo',
-D.department_name AS Departamento,
-J.job_title AS Cargo
+SELECT CONCAT(first_name,' ',last_name) AS 'Nome completo',
+department_name AS Departamento,
+job_title AS Cargo
 FROM hr.employees AS E
 INNER JOIN hr.departments AS D
 ON E.department_id = D.department_id
 INNER JOIN hr.job_history AS JB
-ON E.job_id = jb.job_id
+ON E.job_id = JB.job_id
 INNER JOIN hr.jobs AS J
 ON E.job_id = J.job_id
 WHERE email = E.email
