ea772177d4e93bee18b9c1beddaa0f1a7eec3a7d
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-09-03 15:44:15 -0300

    ajustando req 5

--- desafio4.sql
+++ desafio4.sql
@@ -3,7 +3,7 @@ CASE
 WHEN  AVG(salary) BETWEEN 2000 AND 5800 THEN 'Júnior'
 WHEN  AVG(salary) BETWEEN 5801 AND 7500 THEN 'Pleno'
 WHEN  AVG(salary) BETWEEN 7501 AND 10500 THEN 'Sênior'
-WHEN  AVG(salary) > 10500 THEN 'CEO'
+ELSE 'CEO'
 END AS Senioridade
 FROM hr.employees AS E
 INNER JOIN hr.jobs AS J
