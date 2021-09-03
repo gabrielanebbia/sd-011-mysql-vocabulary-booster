0ecfbc9627f358a5276f2d3fee8aa10dcd9d3321
camiloLelis <lafirmafuerte@gmail.com>
2021-09-03 11:40:07 -0300

    requisito-6-tenta-3

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,6 @@
-SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) `Nome completo`,	
-(SELECT JOB_TITLE FROM hr.jobs WHERE hr.jobs.JOB_ID = hr.employees.JOB_ID) as `Cargo`,
-HIRE_DATE `Data de início do cargo`,
-(SELECT DEPARTMENT_NAME FROM hr.departments WHERE hr.departments.DEPARTMENT_ID = hr.employees.DEPARTMENT_ID) as `Departamento`
-FROM hr.employees
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) `Nome completo`,	
+(SELECT JOB_TITLE FROM hr.jobs WHERE hr.jobs.JOB_ID = e.JOB_ID) as `Cargo`,
+(SELECT START_DATE FROM hr.job_history as jj WHERE hr.jj.JOB_ID = e.JOB_ID limit 1) as `Data de início do cargo`,
+(SELECT DEPARTMENT_NAME FROM hr.departments WHERE hr.departments.DEPARTMENT_ID = e.DEPARTMENT_ID) as `Departamento`
+FROM hr.employees as e
 ORDER BY `Nome completo` DESC, Cargo;
