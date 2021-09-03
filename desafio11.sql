6532b285a7b2a55dcee2aefe77004a05c8b4f315
Michel Pereira <michelbecauro@gmail.com>
2021-09-03 04:47:35 -0300

    Adiciona requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,6 @@
+SELECT c.ContactName AS Nome, c.Country AS País, COUNT(*) - 1 AS `Número de compatriotas`
+ FROM w3schools.customers c, w3schools.customers c2
+WHERE c.Country = c2.Country
+GROUP BY c.ContactName, c.Country
+HAVING `Número de compatriotas` > 0
+ORDER BY c.ContactName;
