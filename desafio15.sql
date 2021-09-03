2b57821a8f78d6c1fc6544629daec5b7ba2b4100
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-09-03 16:53:24 -0300

    refazendo req 15

--- desafio15.sql
+++ desafio15.sql
@@ -1,13 +1,12 @@
 USE hr;
-DELIMITER $$;
+DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(50))
 BEGIN 
 SELECT ROUND(AVG(salary), 2) AS 'Média salarial'
 FROM hr.jobs AS J
 INNER JOIN hr.employees AS E
 ON J.JOB_ID = E.JOB_ID
-WHERE job = J.JOB_TITLE
-GROUP BY E.JOB_ID;
+WHERE job = J.JOB_TITLE;
 END $$
 
 DELIMITER ;
