6cd4aefc7f4285bb8306b6981c6ccbce8e480549
FredericoCanoeira <fcanoeira@gmail.com>
2021-09-03 10:58:46 +0100

    fixing requirement 5 II

--- desafio5.sql
+++ desafio5.sql
@@ -2,7 +2,7 @@
 -- warning Considere os salários máximo e mínimo de um cargo como salários anuais. warning
 SELECT job_title AS 'Cargo', 
 (max_salary - min_salary) AS 'Variação Salarial',
-ROUND((min_salary/ 12), 2) AS 'Média mínima mensal',
-ROUND((max_salary/ 12), 2) AS 'Média mínima mensal'
+ROUND(min_salary/ 12, 2) AS 'Média mínima mensal',
+ROUND(max_salary/ 12, 2) AS 'Média mínima mensal'
 FROM hr.jobs
 ORDER BY `Variação Salarial`, Cargo;
