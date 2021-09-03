f946e3c099bb04e346011519a091957c43980277
Michelle <mls.srbek@gmail.com>
2021-09-03 11:16:18 -0300

    challenges corrections

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
 SELECT cst.ContactName AS Nome, cst.Country AS País, 
 COUNT(c.CustomerID) AS 'Número de compatriotas'
 FROM w3schools.customers AS cst, w3schools.customers AS c
-WHERE cst.Country = cst.Country AND cst.CustomerID != c.CustomerID
+WHERE cst.Country = c.Country AND cst.CustomerID != c.CustomerID
 GROUP BY cst.ContactName, cst.Country ORDER BY Nome;
--- desafio9.sql
+++ desafio9.sql
@@ -2,4 +2,4 @@ SELECT CONCAT(emp.FirstName, ' ', emp.LastName) AS 'Nome completo',
 COUNT(ord.EmployeeID) AS 'Total de pedidos'
 FROM w3schools.employees AS emp
 INNER JOIN w3schools.orders AS ord ON emp.EmployeeID = ord.EmployeeID
-GROUP BY 'Nome completo' ORDER BY COUNT(ord.EmployeeID);
+GROUP BY `Nome completo` ORDER BY COUNT(ord.EmployeeID);
