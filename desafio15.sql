3381329c28ab587caf28fc40c1d029e594bf6b3f
DADA <davimarque2000@gmail.com>
2021-09-03 02:16:27 -0300

    desafio 15

---
+++ desafio15.sql
@@ -0,0 +1,11 @@
+  DELIMITER $$
+  CREATE PROCEDURE buscar_media_por_cargo(IN nome_do_cargo VARCHAR(100))
+  BEGIN
+    SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial' FROM hr.employees AS e
+    INNER JOIN hr.jobs AS j ON e.JOB_ID = j.JOB_ID
+    WHERE JOB_TITLE = nome_do_cargo
+    GROUP BY JOB_TITLE;
+  END $$
+  DELIMITER ;
+  CALL buscar_media_por_cargo('Programmer');
+  
