0f16242e4d136a2e64d6058fd6a64dfd89be3f75
igordmf <igordanielmf@gmail.com>
2021-09-03 21:35:09 -0300

    Requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT
+CONCAT(UCASE(e.first_name) , ' ' , UCASE(e.last_name)) AS `Nome completo`,
+j_h.start_date AS 'Data de início',
+e.salary AS 'Salário'
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS j_h
+ON e.employee_id = j_h.employee_id
+ORDER BY CONCAT(UCASE(e.first_name) , ' ' , UCASE(e.last_name)), j_h.start_date;
