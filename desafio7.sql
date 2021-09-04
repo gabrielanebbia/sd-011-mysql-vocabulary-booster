691c76ae6211a26fba13bd80c317bd9d5d55504a
igordmf <igordanielmf@gmail.com>
2021-09-03 21:44:22 -0300

    Requisito 7

--- desafio7.sql
+++ desafio7.sql
@@ -5,4 +5,5 @@ e.salary AS 'Salário'
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS j_h
 ON e.employee_id = j_h.employee_id
+WHERE MONTH(j_h.start_date) IN (1, 2, 3)
 ORDER BY CONCAT(UCASE(e.first_name) , ' ' , UCASE(e.last_name)), j_h.start_date;
