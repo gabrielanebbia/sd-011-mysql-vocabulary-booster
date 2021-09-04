12c1c2f5c01b82d611216c73851ceb8979148aff
gab-boff <gmeninboff@gmail.com>
2021-09-03 21:58:38 -0300

    Desafio 16

--- desafio15.sql
+++ desafio15.sql
@@ -3,10 +3,10 @@ DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(100))
 BEGIN
 SELECT ROUND(AVG(SALARY),2) AS `Média salarial`
-FROM hr.employees AS e
-INNER JOIN hr.jobs AS j
-ON e.JOB_ID = j.JOB_ID
-WHERE job = j.JOB_TITLE;
+FROM hr.employees AS emp
+INNER JOIN hr.jobs AS job
+ON emp.JOB_ID = job.JOB_ID
+WHERE job = job.JOB_TITLE;
 END $$
 
-DELIMITER ; 
+DELIMITER ;
---
+++ desafio16.sql
@@ -0,0 +1,16 @@
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(EMAIL VARCHAR(100))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE TOTAL SMALLINT;
+SELECT COUNT(emp.EMAIL) FROM jbhist.employees AS emp
+INNER JOIN jbhist.job_history AS jbhist
+ON emp.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE EMAIL = emp.EMAIL
+INTO TOTAL;
+RETURN TOTAL;
+END $$
+
+DELIMITER ;
