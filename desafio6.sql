7c50eb566a17cf4b467f495b61ac328e8921bbfc
igordmf <igordanielmf@gmail.com>
2021-09-03 21:09:51 -0300

    Requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -7,7 +7,7 @@ FROM hr.job_history AS j_h
 INNER JOIN hr.employees AS e
 ON j_h.employee_id = e.employee_id
 INNER JOIN hr.jobs AS j
-ON j.job_id = e.job_id
+ON j_h.job_id = j.job_id
 INNER JOIN hr.departments AS d
-ON d.department_id = e.department_id
-ORDER BY CONCAT (e.first_name, ' ', e.last_name) DESC, j.job_title;
+ON j_h.department_id = d.department_id
+ORDER BY CONCAT(e.first_name, ' ', e.last_name) DESC, j.job_title;
