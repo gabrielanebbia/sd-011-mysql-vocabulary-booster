331b0c604831893d6aab63aef65c9f78c2892b69
gabiazevedo <gabiazevedoms@gmail.com>
2021-09-05 18:23:40 -0300

    Finaliza desafio 19

---
+++ desafio19.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano INT)
+RETURNS INT READS SQL DATA
+  BEGIN
+    DECLARE qtd_func INT;
+    SELECT COUNT(*) FROM hr.employees
+    WHERE MONTH(HIRE_DATE) = mes AND YEAR(HIRE_DATE) = ano INTO qtd_func;
+    RETURN qtd_func;
+  END $$
+
+DELIMITER ;
