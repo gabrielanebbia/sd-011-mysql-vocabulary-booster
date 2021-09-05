f3c2e5d0b243515efb2bd12ef9f48d10837bf4df
Savio Moraes <smmsavio@hotmail.com>
2021-09-05 13:08:24 -0300

    Inicia desafio 17

---
+++ desafio17.sql
@@ -0,0 +1,12 @@
+USE w3schools;
+DELIMITER $$
+
+CREATE TRIGGER trigger_orders_update
+    BEFORE UPDATE ON orders
+    FOR EACH ROW
+BEGIN
+    INSERT INTO orders(OrderID, CustomerID, EmployeeID, OrderDate, ShipperID)
+    VALUES(NEW.OrderID, NEW.CustomerID, NEW.EmployeeID, DATE(NOW()), NEW.ShipperID);
+END $$
+
+DELIMITER ;
