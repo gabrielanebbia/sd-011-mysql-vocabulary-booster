c0ee5cf155296e3ff46f022c4fcaa9add758e3b4
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-09-05 18:55:38 -0300

    corrige desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,11 @@
-SELECT wcust.CONTACTNAME AS 'Nome de contato',
-wship.SHIPPERNAME AS 'Empresa que fez o envio',
-word.ORDERDATE AS 'Data do pedido'
+SELECT 
+wcust.CONTACTNAME AS 'Nome de contato',
+word.ORDERDATE AS 'Data do pedido',
+wship.SHIPPERNAME AS 'Empresa que fez o envio'
 FROM w3schools.customers AS wcust
-INNER JOIN w3schools.shippers AS wship
 INNER JOIN w3schools.orders AS word
 ON word.CustomerID = wcust.CustomerID
+INNER JOIN w3schools.shippers AS wship
+ON word.ShipperID = wship.ShipperID
 WHERE wship.ShipperName IN ('Speedy Express', 'United Package')
 ORDER BY wcust.CONTACTNAME, wship.SHIPPERNAME, word.ORDERDATE;
