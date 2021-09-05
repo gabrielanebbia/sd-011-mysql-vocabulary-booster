e8998671da23169b036864a588b1e00a0c3bf390
gabiazevedo <gabiazevedoms@gmail.com>
2021-09-05 18:02:10 -0300

    Finaliza desafio 18

---
+++ desafio18.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
+DATE_FORMAT(`START_DATE`, '%d/%m/%Y') AS 'Data de início',
+DATE_FORMAT(`END_DATE`, '%d/%m/%Y') AS 'Data de rescisão',
+ROUND(DATEDIFF(jh.START_DATE, jh.END_DATE) / 365, 2) AS 'Anos trabalhados'
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS jh
+ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+ORDER BY `Nome completo`, `Anos trabalhados`;
