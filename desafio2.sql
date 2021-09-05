f1ceead2738f2f6ef039dcdd0bea20a54cd5cb41
Rafael Schmitz <schimitaum@gmail.com>
2021-09-04 21:29:25 -0300

    refactor

--- desafio2.sql
+++ desafio2.sql
@@ -1,11 +1,9 @@
 SELECT j.JOB_TITLE AS 'Cargo',
 CASE
-        WHEN e.salary BETWEEN 5000 AND 10000 THEN 'Baixo'
-        WHEN e.salary BETWEEN 10001 AND 20000 THEN 'Médio'
-        WHEN e.salary BETWEEN 20001 AND 30000 THEN 'Alto'
-        WHEN e.salary > 30000 THEN 'Altíssimo'
+        WHEN j.MAX_SALARY BETWEEN 5000 AND 10000 THEN 'Baixo'
+        WHEN j.MAX_SALARY BETWEEN 10001 AND 20000 THEN 'Médio'
+        WHEN j.MAX_SALARY BETWEEN 20001 AND 30000 THEN 'Alto'
+        WHEN j.MAX_SALARY > 30000 THEN 'Altíssimo'
     END AS 'Nível'
-FROM employees AS e
-LEFT JOIN jobs AS j
-ON e.job_id = j.job_id
+FROM jobs AS j
 ORDER BY j.JOB_TITLE;
