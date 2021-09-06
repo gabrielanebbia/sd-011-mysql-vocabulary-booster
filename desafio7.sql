f3000f5805b45e0c00c484c91a4af28382556129
doug-santana <douguinhodns@gmail.com>
2021-09-05 21:14:29 -0300

    fix reqs 6 e 7

--- desafio7.sql
+++ desafio7.sql
@@ -4,5 +4,5 @@ e.SALARY AS Salário
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS jh
 ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
-WHERE MONTH(jh.START_DATE) BETWEEN 1 AND 3
+WHERE MONTH(jh.START_DATE) IN (1, 2, 3)
 ORDER BY `Nome Completo`, `Data de início`;
