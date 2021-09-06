0196f69483158d5802a016f2f0dc73cbc83fcf92
samuelvanguarda <samuelvanguarda@gmail.com>
2021-09-06 17:39:38 -0300

    Requisitos 16 e 17

---
+++ desafio16.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100))
+RETURNS INT
+READS SQL DATA
+BEGIN
+SELECT 
+COUNT(*) AS `Total`
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS j
+ON e.employee_id = j.employee_id
+AND e.email = email
+INTO email;
+
+RETURN email;
+END $$
+DELIMITER ;
