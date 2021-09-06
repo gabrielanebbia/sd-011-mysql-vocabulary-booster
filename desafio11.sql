fcb699d555198b4bcd0afa370aebbefcd059d091
igordmf <igordanielmf@gmail.com>
2021-09-06 01:22:12 -0300

    Requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -5,4 +5,5 @@ COUNT(*) - 1 AS 'Número de compatriotas'
 FROM w3schools.customers AS c1, w3schools.customers AS c2
 WHERE c1.country = c2.country
 GROUP BY c1.customername
+HAVING COUNT(*) - 1 > 0
 ORDER BY c1.contactname;
