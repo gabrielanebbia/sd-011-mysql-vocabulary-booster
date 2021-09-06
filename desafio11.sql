8d7e70e274b3a55335ad47cb3cfb3d81526780df
igordmf <igordanielmf@gmail.com>
2021-09-06 01:29:47 -0300

    Requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,19 @@
 SELECT
 c1.contactname AS 'Nome',
 c1.country AS 'País',
-COUNT(*) - 1 AS 'Número de compatriotas'
+(SELECT
+COUNT(country)
+FROM w3schools.customers
+GROUP BY country
+HAVING country = c1.country
+) - 1 AS 'Número de compatriotas'
 FROM w3schools.customers AS c1, w3schools.customers AS c2
 WHERE c1.country = c2.country
 GROUP BY c1.customername
-HAVING COUNT(*) - 1 > 0
+HAVING (SELECT
+COUNT(country)
+FROM w3schools.customers
+GROUP BY country
+HAVING country = c1.country
+) - 1 > 0
 ORDER BY c1.contactname;
