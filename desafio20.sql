b62b3fbb1883a14b6046ec51ce4d96356cb544fc
doug-santana <douguinhodns@gmail.com>
2021-09-05 21:56:52 -0300

    req 20

---
+++ desafio20.sql
@@ -0,0 +1,17 @@
+BEGIN
+  SELECT
+    CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS `Nome completo`,
+    departments.DEPARTMENT_NAME AS `Departamento`,
+    jobs.JOB_TITLE AS `Cargo`
+  FROM  job_history AS job_history
+  INNER JOIN employees AS employees
+    ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
+  INNER JOIN departments AS departments
+   ON departments.DEPARTMENT_ID = job_history.DEPARTMENT_ID
+  INNER JOIN jobs AS jobs
+    ON jobs.JOB_ID = job_history.JOB_ID
+    WHERE employees.EMAIL = email
+    ORDER BY `Departamento`, `Cargo`;
+END $$
+
+DELIMITER ;
