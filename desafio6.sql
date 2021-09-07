d65bb86d28be1326560ac5f1b88f4affa59f3efd
Lais Simenikim <l.simenikim@gmail.com>
2021-09-07 14:19:45 -0300

    Ajust challenge 6

--- desafio6.sql
+++ desafio6.sql
@@ -3,7 +3,7 @@ J.JOB_TITLE AS Cargo,
 JH.START_DATE AS 'Data de início do cargo',
 D.DEPARTMENT_NAME AS Departamento
 FROM hr.employees AS E
-INNER JOIN hr.jobs AS J ON E.JOB_ID = J.JOB_ID
+INNER JOIN hr.jobs AS J ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
 INNER JOIN hr.job_history AS JH ON J.JOB_ID = JH.JOB_ID
 INNER JOIN hr.departments AS D ON JH.DEPARTMENT_ID = D.DEPARTMENT_ID
 ORDER BY `Nome Completo` DESC, Cargo;
