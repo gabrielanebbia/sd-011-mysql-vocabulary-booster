b4ed22ce1a9bad2c5bbe8acb730ec1f169fb5ccd
murilodospacos <murilopacos@gmail.com>
2021-09-07 11:35:36 -0300

    requisito 18 deixando ano com 4 casa

--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,7 @@
 SELECT 
 CONCAT(e.FIRST_NAME, ' ',e.LAST_NAME) AS 'Nome completo',
-DATE_FORMAT(jh.START_DATE, '%d/%m/%y') AS 'Data de início',
-DATE_FORMAT(jh.END_DATE, '%d/%m/%y') AS 'Data de rescisão',
+DATE_FORMAT(jh.START_DATE, '%d/%m/%Y') AS 'Data de início',
+DATE_FORMAT(jh.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
 ROUND(TIMESTAMPDIFF( DAY,jh.START_DATE, jh.END_DATE)/365, 2) AS 'Anos trabalhados' 
 FROM hr.employees AS e
 INNER JOIN hr.job_history jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
