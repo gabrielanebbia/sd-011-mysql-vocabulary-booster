05f1ac812567a942b6c5d21a10318d3502b338c6
murilodospacos <murilopacos@gmail.com>
2021-09-07 10:32:54 -0300

    requisito 18

---
+++ desafio18.sql
@@ -0,0 +1,8 @@
+SELECT 
+CONCAT(e.FIRST_NAME, ' ',e.LAST_NAME) AS 'Nome completo',
+DATE_FORMAT(jh.START_DATE, '%d%m%y') AS 'Data de início',
+DATE_FORMAT(jh.END_DATE, '%d%m%y') AS 'Data de rescisão',
+ROUND(TIMESTAMPDIFF( DAY,jh.START_DATE, jh.END_DATE)/365, 2) AS 'Anos trabalhados' 
+FROM hr.employees AS e
+INNER JOIN hr.job_history jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+ORDER BY `Nome completo`, `Anos trabalhados`;
