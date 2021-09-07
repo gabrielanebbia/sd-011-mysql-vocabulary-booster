94b95105d8fab81ed654901989a62c8de176cf6c
murilodospacos <murilopacos@gmail.com>
2021-09-06 21:02:16 -0300

    requisito 15 retirando espaços

--- desafio15.sql
+++ desafio15.sql
@@ -1,14 +1,11 @@
 USE hr;
 DELIMITER $$
-
 CREATE PROCEDURE buscar_media_por_cargo(IN nome_cargo VARCHAR(100))
 BEGIN
-	SELECT ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
+SELECT ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
 FROM hr.jobs j
 INNER JOIN hr.employees e ON e.JOB_ID = j.JOB_ID
 WHERE JOB_TITLE = nome_cargo;
 END $$
-
 DELIMITER ;
-
 CALL buscar_media_por_cargo('Programmer');
