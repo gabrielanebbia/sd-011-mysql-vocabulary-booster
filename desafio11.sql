4531396d08b138d54cf53a10aa977f6e003f309d
camiloLelis <lafirmafuerte@gmail.com>
2021-09-07 17:44:57 -0300

    requisito-11-tenta-2

--- desafio11.sql
+++ desafio11.sql
@@ -1,4 +1,7 @@
 SELECT c.CustomerName Nome , c.Country País, 
-(SELECT COUNT(Country) from w3schools.customers cc WHERE cc.Country = c.Country and cc.CustomerID != c.CustomerID) AS `Número de compatriotas` 
+(SELECT COUNT(Country) from w3schools.customers cc 
+WHERE cc.Country = c.Country and cc.CustomerID != c.CustomerID) AS `Número de compatriotas` 
 FROM w3schools.customers c
+WHERE (SELECT COUNT(Country) from w3schools.customers cc 
+WHERE cc.Country = c.Country and cc.CustomerID != c.CustomerID) != 0
 ORDER BY Nome;
