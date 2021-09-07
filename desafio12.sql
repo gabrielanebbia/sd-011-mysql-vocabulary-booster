1b272aea94f832296d78131fb652f2d652e2ec98
camiloLelis <lafirmafuerte@gmail.com>
2021-09-07 19:00:31 -0300

    requisito-11-12-tenta-2

--- desafio12.sql
+++ desafio12.sql
@@ -4,7 +4,7 @@ e.PHONE_NUMBER 'Telefone funcionário 1',
 CONCAT(ee.FIRST_NAME, ' ', ee.LAST_NAME) 'Nome completo funcionário 2',
 ee.SALARY 'Salário funcionário 2',
 ee.PHONE_NUMBER 'Telefone funcionário 2'
-FROM hr.employees e, hr.employees ee;
-WHERE e.JOB_ID = ee.JJOB_ID AND
+FROM hr.employees e, hr.employees ee
+WHERE e.JOB_ID = ee.JOB_ID AND
 e.EMPLOYEE_ID <> ee.EMPLOYEE_ID
-ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
+ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME), CONCAT(ee.FIRST_NAME, ' ', ee.LAST_NAME);
