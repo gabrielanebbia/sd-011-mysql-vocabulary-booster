cdf6d604a8e4670d679bf5c101368820b3c3bbd4
Rafael Schmitz <schimitaum@gmail.com>
2021-09-07 01:36:48 -0300

    again 2

--- desafio5.sql
+++ desafio5.sql
@@ -1,4 +1,4 @@
-SELECT j.JOB_TITLE, (j.MAX_SALARY-j.MIN_SALARY) AS 'Variação Salarial', 
+SELECT j.JOB_TITLE AS 'Cargo', (j.MAX_SALARY-j.MIN_SALARY) AS 'Variação Salarial', 
 round(j.Min_SALARY/12,2) AS "Média mínima mensal", round(j.MAX_SALARY/12,2) AS "Média máxima mensal"
 FROM jobs AS j
 ORDER BY 2 ASC, 1;
