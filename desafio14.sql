b9be440d60908be5ed40801c8e68318b9988a135
camiloLelis <lafirmafuerte@gmail.com>
2021-09-07 23:57:24 -0300

    requisito-14

---
+++ desafio14.sql
@@ -0,0 +1,7 @@
+(SELECT Country País FROM w3schools.customers)
+UNION  
+(SELECT Country FROM w3schools.suppliers)
+ORDER BY País limit 5;(SELECT Country País FROM w3schools.customers)
+UNION  
+(SELECT Country FROM w3schools.suppliers)
+ORDER BY País limit 5;
