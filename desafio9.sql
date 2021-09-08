1e622211de00b826bded1b3d9660e482af9d4008
Thiago Gomes <thiagog.macedom@gmail.com>
2021-09-08 14:24:36 -0300

    trocando aspas por crase req 9

--- desafio9.sql
+++ desafio9.sql
@@ -3,4 +3,4 @@ COUNT(o.EmployeeID) AS 'Total de pedidos'
 FROM w3schools.employees AS e
 INNER JOIN w3schools.orders AS o ON e.EmployeeID = o.EmployeeID
 GROUP BY e.EmployeeID
-ORDER BY 'Total de pedidos';
+ORDER BY `Total de pedidos`;
