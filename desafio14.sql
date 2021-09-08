16b57f784b43665558624d37ffc68111581edd0f
camiloLelis <lafirmafuerte@gmail.com>
2021-09-08 00:05:25 -0300

    requisito-14-tenta-3

--- desafio14.sql
+++ desafio14.sql
@@ -1,7 +1,5 @@
 (SELECT Country País FROM w3schools.customers)
 UNION  
 (SELECT Country FROM w3schools.suppliers)
-ORDER BY País limit 5;(SELECT Country País FROM w3schools.customers)
-UNION  
-(SELECT Country FROM w3schools.suppliers)
-ORDER BY País limit 5;
+ORDER BY País 
+LIMIT 5;
