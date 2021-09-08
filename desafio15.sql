8454b299263de3278c791af4f8a7d85cb1f19fc0
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-09-07 23:34:12 -0300

    challenge fifteen done

---
+++ desafio15.sql
@@ -0,0 +1,9 @@
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
+BEGIN
+    SELECT round(AVG(e.salary), 2) AS 'Média salarial' 
+    FROM employees AS e
+    INNER JOIN jobs as j ON j.JOB_ID = e.JOB_ID
+    WHERE j.JOB_TITLE = cargo;     
+END $$
+DELIMITER ;
