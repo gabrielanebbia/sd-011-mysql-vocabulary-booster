648d431c0e880613129f52c591be85c647475e11
Lais Simenikim <l.simenikim@gmail.com>
2021-09-07 23:28:06 -0300

    Ajust challenge 15

--- desafio15.sql
+++ desafio15.sql
@@ -3,8 +3,8 @@ CREATE PROCEDURE buscar_media_por_cargo (cargo VARCHAR(40))
 BEGIN
 SELECT ROUND(AVG(E.SALARY), 2) AS 'Média salarial'
 FROM hr.employees AS E
-INNER JOIN hr.jobs AS J ON E.JOB_ID = J.JOB_ID
-GROUP BY E.JOB_ID
-HAVING J.JOB_TITLE = cargo;
+INNER JOIN hr.jobs AS J
+ON E.JOB_ID = J.JOB_ID
+WHERE J.JOB_TITLE = cargo;
 END $$
 DELIMITER ;
