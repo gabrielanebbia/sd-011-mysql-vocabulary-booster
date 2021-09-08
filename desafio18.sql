a40f401226e8a71f19eeb551b88021826963e349
Thiago Gomes <thiagog.macedom@gmail.com>
2021-09-08 14:48:32 -0300

    corrigindo erros de nomeacao req 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,7 @@
-SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome completo',
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
 DATE_FORMAT(jh.START_DATE, "%d/%m/%Y") AS 'Data de início',
 DATE_FORMAT(jh.END_DATE, "%d/%m/%Y") AS 'Data de rescisão',
 (ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE) / 365, 2)) AS 'Anos trabalhados'
-FROM employees AS e
-INNER JOIN job_history AS jh ON E.EMPLOYEE_ID = e.EMPLOYEE_ID
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 ORDER BY `Nome completo`, `Anos trabalhados`;
