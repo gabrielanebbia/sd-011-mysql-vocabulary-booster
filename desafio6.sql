20a66e48b5387e85a1098e8d935ad764507adcc2
limaleonardo <lima.leonardo.r@gmail.com>
2021-09-08 18:16:28 -0300

    implementa desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,10 @@
+SELECT concat(em.FIRST_NAME, ' ', em.LAST_NAME) AS 'Nome completo',
+  jb.JOB_TITLE AS Cargo,
+  jh.START_DATE AS 'Data de início do cargo',
+  dp.DEPARTMENT_NAME AS Departamento
+FROM hr.employees AS em
+  INNER JOIN hr.jobs AS jb ON em.JOB_ID = jb.JOB_ID
+  INNER JOIN hr.job_history AS jh ON em.EMPLOYEE_ID = jh.EMPLOYEE_ID
+  INNER JOIN hr.departments AS dp ON jh.DEPARTMENT_ID = dp.DEPARTMENT_ID
+ORDER BY concat(em.FIRST_NAME, ' ', em.LAST_NAME) DESC,
+  Cargo ASC;
