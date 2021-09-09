22362b7db2a0349ea6b064307c57887f46f69473
Rafael Schmitz <schimitaum@gmail.com>
2021-09-09 00:58:13 -0300

    8

---
+++ desafio8.sql
@@ -0,0 +1,12 @@
+SELECT c.ContactName AS 'Nome de contato',
+s.ShipperName AS 'Empresa que fez o envio',
+o.OrderDate AS 'Data do pedido'
+FROM orders AS o
+INNER JOIN customers AS c
+ON o.CustomerID = c.CustomerID
+INNER JOIN shippers AS s
+ON o.ShipperID = s.ShipperID
+WHERE (
+    s.ShipperName = 'Speedy Express' OR s.ShipperName = 'United Package'
+)
+ORDER BY 1, 2, 3;
