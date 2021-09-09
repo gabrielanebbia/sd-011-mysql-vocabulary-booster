f7e30e959727106e454110b99028e0cfc3328d79
igordmf <igordanielmf@gmail.com>
2021-09-09 16:17:26 -0300

    Requisito 18

---
+++ desafio18.sql
@@ -0,0 +1,9 @@
+SELECT
+CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
+DATE_FORMAT(j_h.start_date, '%d/%m/%Y') AS 'Data de início',
+DATE_FORMAT(j_h.end_date, '%d/%m/%Y') AS 'Data de rescisão',
+ROUND((YEAR(j_h.end_date) - YEAR(j_h.start_date)), 2) AS 'Anos trabalhados'
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS j_h
+ON e.employee_id = j_h.employee_id
+ORDER BY CONCAT(e.first_name, ' ', e.last_name);
