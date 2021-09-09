e13770daada3a648b4c0ff8e0bfb325fa4f54a2b
limaleonardo <lima.leonardo.r@gmail.com>
2021-09-09 13:57:55 -0300

    Corrige soluçao dos desafios 6, 11 e 18

--- desafio11.sql
+++ desafio11.sql
@@ -6,5 +6,9 @@ SELECT cu1.ContactName AS 'Nome',
     WHERE cu2.country = cu1.Country
   ) AS 'Número de compatriotas'
 FROM w3schools.customers AS cu1
-GROUP BY cu1.CustomerID
+WHERE (
+    SELECT COUNT(cu2.Country) - 1
+    FROM w3schools.customers AS cu2
+    WHERE cu2.country = cu1.Country
+  ) > 0
 ORDER BY cu1.ContactName ASC;
--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,13 @@
 SELECT concat(em.FIRST_NAME, ' ', em.LAST_NAME) AS 'Nome completo',
-  jb.JOB_TITLE AS Cargo,
+  jb.JOB_TITLE AS 'Cargo',
   jh.START_DATE AS 'Data de início do cargo',
   dp.DEPARTMENT_NAME AS Departamento
-FROM hr.employees AS em
-  RIGHT JOIN hr.job_history AS jh ON jh.EMPLOYEE_ID = em.EMPLOYEE_ID
-  INNER JOIN hr.jobs AS jb ON em.JOB_ID = jb.JOB_ID
-  INNER JOIN hr.departments AS dp ON jh.DEPARTMENT_ID = dp.DEPARTMENT_ID
+FROM hr.job_history AS jh
+  INNER JOIN hr.employees AS em
+  ON jh.EMPLOYEE_ID = em.EMPLOYEE_ID
+  INNER JOIN hr.jobs AS jb
+  ON jh.JOB_ID = jb.JOB_ID
+  INNER JOIN hr.departments AS dp
+  ON jh.DEPARTMENT_ID = dp.DEPARTMENT_ID
 ORDER BY concat(em.FIRST_NAME, ' ', em.LAST_NAME) DESC,
   jb.JOB_TITLE ASC;
