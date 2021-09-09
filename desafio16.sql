dd8e934e55c1d4e10bcf7f31e65a65b1be72707b
camiloLelis <lafirmafuerte@gmail.com>
2021-09-09 16:14:16 -0300

    requisito-16-tenta-2

--- desafio16.sql
+++ desafio16.sql
@@ -4,7 +4,7 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
 RETURNS INT READS SQL DATA
 BEGIN
-	 DECLARE historico INT;
+	DECLARE historico INT;
     SELECT COUNT(*) FROM hr.job_history jh WHERE 
     jh.EMPLOYEE_ID IN (SELECT e.EMPLOYEE_ID FROM hr.employees e WHERE e.EMAIL = email) INTO historico;
     RETURN historico;
