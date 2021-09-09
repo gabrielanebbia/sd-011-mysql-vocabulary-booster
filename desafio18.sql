8acdb120a0ec303d805a0ac1de99e1cd6621713f
igordmf <igordanielmf@gmail.com>
2021-09-09 16:22:43 -0300

    Requisito 18

--- desafio18.sql
+++ desafio18.sql
@@ -6,4 +6,4 @@ ROUND((YEAR(j_h.end_date) - YEAR(j_h.start_date)), 2) AS 'Anos trabalhados'
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS j_h
 ON e.employee_id = j_h.employee_id
-ORDER BY CONCAT(e.first_name, ' ', e.last_name);
+ORDER BY CONCAT(e.first_name, ' ', e.last_name), ROUND((YEAR(j_h.end_date) - YEAR(j_h.start_date)), 2);
