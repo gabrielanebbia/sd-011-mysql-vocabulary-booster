2359b3f3e84c5a2f1306a33f14fc51549a6e7e99
igordmf <igordanielmf@gmail.com>
2021-09-08 22:36:49 -0300

    Requisito 13

--- desafio13.sql
+++ desafio13.sql
@@ -3,4 +3,5 @@ p.productname AS 'Produto',
 p.price AS 'Preço'
 FROM w3schools.products AS p
 INNER JOIN w3schools.order_details AS o_d
-WHERE p.productid = o_d.productid AND o_d.quantity > 80;
+WHERE p.productid = o_d.productid AND o_d.quantity > 80
+ORDER BY p.productname;
