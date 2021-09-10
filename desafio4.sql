40c71bcf504fa5c563f4a74784527c079792aa7e
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2021-09-10 12:27:41 -0300

    Req 5 finished

--- desafio4.sql
+++ desafio4.sql
@@ -1,12 +1,12 @@
 SELECT j.JOB_TITLE AS 'Cargo', ROUND(AVG(e.SALARY), 2) AS 'Média Salarial',
 CASE
 WHEN ROUND(AVG(e.SALARY)) >= 2000 AND ROUND(AVG(e.SALARY)) <= 5000 THEN 'Júnior'
-WHEN AVG(e.SALARY) <= 7500 THEN 'Pleno'
-WHEN AVG(e.SALARY) <= 10500 THEN 'Sênior'
+WHEN ROUND(AVG(e.SALARY)) <= 7500 THEN 'Pleno'
+WHEN ROUND(AVG(e.SALARY)) <= 10500 THEN 'Sênior'
 ELSE 'CEO'	 
-END AS Senioridade
-FROM hr.jobs AS j
-INNER JOIN hr.employees AS e
+END AS 'Senioridade'
+FROM hr.jobs j
+INNER JOIN hr.employees e
 ON j.JOB_ID = e.JOB_ID
 GROUP BY j.JOB_TITLE
-ORDER BY `Média Salarial`, j.JOB_TITLE;
+ORDER BY `Média Salarial`, `Cargo`;
---
+++ desafio5.sql
@@ -0,0 +1,4 @@
+SELECT j.JOB_TITLE as 'Cargo', (MAX_SALARY - MIN_SALARY) as 'Variação Salarial', ROUND(MIN_SALARY / 12, 2) as 'Média Mínima Mensal',
+ROUND(MAX_SALARY / 12, 2) as 'Média Máxima Mensal'
+FROM hr.jobs j
+ORDER BY `Variação Salarial`, `Cargo`;
