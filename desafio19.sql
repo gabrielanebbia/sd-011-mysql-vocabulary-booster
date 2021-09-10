bb98467cddfe563eb4131b963483b967318435ef
camiloLelis <lafirmafuerte@gmail.com>
2021-09-10 14:53:05 -0300

    requisito-19

---
+++ desafio19.sql
@@ -0,0 +1,13 @@
+USE hr
+DELIMITER $$
+
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes int, ano int)
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE valor int;
+	select count(*) from hr.employees e 
+    where MONTH(e.HIRE_DATE) = mes AND YEAR(e.HIRE_DATE) = ano INTO valor; 
+	RETURN valor;
+END $$
+
+DELIMITER ;
