026a95de16230508e6d57ae1191afe406238c3ce
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2021-09-10 16:03:58 -0300

    Req 11 agr vai

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,8 @@
 select customers1.ContactName as 'Nome',
 customers1.Country as 'País',
-count(customers1.Country) - 1 AS 'Número de Compatriotas'
+COUNT(customers1.Country) AS 'Número de Compatriotas'
 FROM w3schools.customers as customers1, w3schools.customers as customers2
-where customers1.Country = customers2.Country
+where customers1.Country = customers2.Country AND customers1.CustomerID <> customers2.CustomerID
 GROUP BY `Nome`, `País`
 HAVING `Número de Compatriotas` <> 0
 ORDER BY `Nome`;
