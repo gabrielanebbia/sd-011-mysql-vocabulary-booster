c9b5a542d06948334a5ffd364f6a8e0afa446b41
Rafael Schmitz <schimitaum@gmail.com>
2021-09-09 22:18:27 -0300

    again

--- desafio10.sql
+++ desafio10.sql
@@ -1,12 +1,10 @@
 SELECT p.ProductName AS 'Produto',
 min(o.Quantity) AS 'Mínima',
 max(o.Quantity) AS 'Máxima',
-avg(o.Quantity) AS 'Média'
+round(avg(o.Quantity),2) AS 'Média'
 FROM products AS p 
 INNER JOIN order_details AS o
 ON o.ProductID = p.ProductID
-WHERE (
-    'Média' > 20
-)
 GROUP BY p.ProductName
-ORDER BY 4 ASC, 1 ASC;
+HAVING ROUND(AVG(o.Quantity), 2) > 20.00
+ORDER BY 4, 1;
