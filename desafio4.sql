d489ee4e3b9e614d2f9be40af202bc03325db0c3
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2021-09-10 12:40:06 -0300

    4 e 5 found and fixed errors

--- desafio4.sql
+++ desafio4.sql
@@ -1,12 +1,12 @@
-SELECT j.JOB_TITLE AS 'Cargo', ROUND(AVG(e.SALARY), 2) AS 'Média Salarial',
+SELECT JOBS.JOB_TITLE AS 'Cargo', ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
 CASE
 WHEN ROUND(AVG(e.SALARY)) >= 2000 AND ROUND(AVG(e.SALARY)) <= 5000 THEN 'Júnior'
 WHEN ROUND(AVG(e.SALARY)) <= 7500 THEN 'Pleno'
 WHEN ROUND(AVG(e.SALARY)) <= 10500 THEN 'Sênior'
 ELSE 'CEO'	 
 END AS 'Senioridade'
-FROM hr.jobs j
-INNER JOIN hr.employees e
-ON j.JOB_ID = e.JOB_ID
-GROUP BY j.JOB_TITLE
+FROM hr.jobs as JOBS
+INNER JOIN hr.employees AS EMPLOYEES
+ON JOBS.JOB_ID = EMPLOYEES.JOB_ID
+GROUP BY `Cargo`
 ORDER BY `Média Salarial`, `Cargo`;
--- desafio5.sql
+++ desafio5.sql
@@ -1,4 +1,4 @@
-SELECT JOB_TITLE as 'Cargo', (MAX_SALARY - MIN_SALARY) as 'Variação Salarial', ROUND((MIN_SALARY / 12), 2) as 'Média Mínima Mensal',
-ROUND((MAX_SALARY / 12), 2) as 'Média Máxima Mensal'
+SELECT JOB_TITLE as 'Cargo', (MAX_SALARY - MIN_SALARY) as 'Variação Salarial', ROUND((MIN_SALARY / 12), 2) as 'Média mínima mensal',
+ROUND((MAX_SALARY / 12), 2) as 'Média máxima mensal'
 FROM hr.jobs 
 ORDER BY `Variação Salarial`, `Cargo`;
