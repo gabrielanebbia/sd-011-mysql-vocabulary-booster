a7059d16e4ee98f725e8da46d6e83aca142f7f29
Rafael Schmitz <schimitaum@gmail.com>
2021-09-10 23:08:44 -0300

    15

---
+++ desafio15.sql
@@ -0,0 +1,13 @@
+USE hr;
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo(IN getTitle VARCHAR(100))
+BEGIN
+SELECT ROUND(AVG(s.SALARY), 2) AS 'Média salarial' 
+FROM employees AS s
+LEFT JOIN jobs AS t
+ON s.JOB_ID = t.JOB_ID
+WHERE t.JOB_TITLE = getTitle
+GROUP BY s.JOB_ID;
+END $$
+DELIMITER ;
+CALL buscar_media_por_cargo('Programmer');
