36954fd5c1d27307df63b3bd7869962869e0d305
Rafael Schmitz <schimitaum@gmail.com>
2021-09-10 23:02:16 -0300

    18

---
+++ desafio18.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT(n.FIRST_NAME, ' ', n.LAST_NAME) AS 'Nome completo',
+DATE_FORMAT(d.START_DATE, '%d/%m/%Y') AS 'Data de início',
+DATE_FORMAT(d.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
+ROUND(DATEDIFF(d.END_DATE, d.START_DATE) / 365, 2) AS 'Anos trabalhados'
+FROM employees n
+INNER JOIN job_history d
+ON n.EMPLOYEE_ID = d.EMPLOYEE_ID
+ORDER BY 1 ASC, 4 ASC;
