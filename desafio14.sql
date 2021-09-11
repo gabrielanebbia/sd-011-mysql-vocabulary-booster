fc7868980e5ea3327a4936baada5ca7c08e54caa
Rafael Schmitz <schimitaum@gmail.com>
2021-09-10 22:57:47 -0300

    14

--- desafio14.sql
+++ desafio14.sql
@@ -1,4 +1,5 @@
--- Considerando o conjunto formado pelas pessoas consumidoras e empresas fornecedoras de produtos, queremos saber quais são os cinco primeiros países distintos, em ordem alfabética, presentes nesse conjunto
--- Exiba os 05 primeiros países distintos dentre as pessoas consumidoras e as empresas fornecedoras, em ordem alfabética.
--- Use o banco w3schools como referência
--- Monte uma query que exiba 01 coluna com o alias "País" contendo o nome do país.
+SELECT Country AS 'País' FROM customers 
+UNION
+SELECT Country AS 'País' FROM suppliers 
+ORDER BY País
+LIMIT 5;
