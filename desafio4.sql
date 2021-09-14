4469f6141feba8b67e7b940e853678b28d0b7ca9
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2021-09-14 10:05:38 -0300

    Fixing 4th

--- desafio4.sql
+++ desafio4.sql
@@ -1,12 +1,12 @@
-SELECT JOBS.job_title AS 'Cargo', ROUND(AVG(EMPLOYEES.salary), 2) AS 'Média salarial',
+SELECT j.JOB_TITLE AS 'Cargo', ROUND(AVG(e.salary), 2) AS 'Média salarial',
 CASE
-WHEN ROUND(AVG(EMPLOYEES.salary), 2) >= 2000 AND ROUND(AVG(EMPLOYEES.salary)) <= 5000 THEN 'Júnior'
-WHEN ROUND(AVG(EMPLOYEES.salary), 2) <= 7500 THEN 'Pleno'
-WHEN ROUND(AVG(EMPLOYEES.salary), 2) <= 10500 THEN 'Sênior'
+WHEN ROUND(AVG(e.salary), 2) >= 2000 AND ROUND(AVG(e.salary)) <= 5000 THEN 'Júnior'
+WHEN ROUND(AVG(e.salary), 2) <= 7500 THEN 'Pleno'
+WHEN ROUND(AVG(e.salary), 2) <= 10500 THEN 'Sênior'
 ELSE 'CEO'	 
-END AS Senioridade
-FROM hr.jobs as JOBS
-INNER JOIN hr.employees AS EMPLOYEES
-ON JOBS.job_id = EMPLOYEES.job_id
+END AS 'Senioridade'
+FROM hr.jobs j
+INNER JOIN hr.employees e
+ON j.JOB_ID = e.JOB_ID
 GROUP BY `Cargo`
 ORDER BY `Média salarial`, `Cargo`;
